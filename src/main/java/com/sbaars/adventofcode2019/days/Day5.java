92f8b2cce970b97047095ce7d8a8e8e7a5b3287d
Simon <simon.mailadres@gmail.com>
2019-12-05 16:29:58 +0100

    Finished day 5

--- src/main/java/com/sbaars/adventofcode2019/days/Day5.java
+++ src/main/java/com/sbaars/adventofcode2019/days/Day5.java
@@ -10,7 +10,7 @@ import com.sbaars.adventofcode2019.util.DoesFileOperations;
 
 public class Day5 implements Day, DoesFileOperations {
 
-	private static int NUM_MODES = 3;
+	private static final int NUM_MODES = 3;
 	private int instructionCounter = 0;
 	private int part;
 
@@ -27,13 +27,14 @@ public class Day5 implements Day, DoesFileOperations {
 	@Override
 	public int part2() throws IOException {
 		part = 2;
+		instructionCounter = 0;
 		return execute();
 	}
 
 	private int execute() throws IOException {
 		int[] program = Arrays.stream(getFileAsString(new File(Day1.class.getClassLoader().getResource("day5.txt").getFile())).split(",")).mapToInt(Integer::parseInt).toArray();;
 		int result;
-		for(; (result = executeInstruction(program, program[instructionCounter])) == -1; instructionCounter+=nParams(program[instructionCounter])+1);
+		while((result = executeInstruction(program, program[instructionCounter])) == -1);
 		return result;
 	}
 
@@ -52,8 +53,12 @@ public class Day5 implements Day, DoesFileOperations {
 	private int execute(int[] program, int[] method, int instruction) {
 		int nParams = nParams(instruction);
 		int[] args = IntStream.range(0, nParams).map(j -> j == 0 ? program[instructionCounter+nParams] : program[instructionCounter+j]).toArray();
-		if(args.length>2) Arrays.stream(new int[]{2, 1}).filter(j -> method[j] == 0).map(j -> j == 2 ? 1 : 2).forEach(j -> args[j] = program[args[j]]);
+		if(args.length>=2) {
+			if(method[2] == 0) args[1] = program[args[1]];
+			if(args.length>=3 && method[1] == 0) args[2] = program[args[2]];
+		}
 		if(instruction == 4 && method[2] == 0) args[0] = program[args[0]];
+		if((instruction == 5 || instruction == 6) && method[1] == 0) args[0] = program[args[0]];
 		return executeInstruction(program, args, instruction);
 	}
 	
@@ -63,13 +68,13 @@ public class Day5 implements Day, DoesFileOperations {
 			case 2: program[args[0]] = args[1] * args[2]; break;
 			case 3: program[args[0]] = part == 1 ? 1 : 5; break;
 			case 4: if(args[0]!=0) return args[0]; break;
-			case 5: if(args[1] != 0) instructionCounter = args[0]; break;
-			case 6: if(args[1] == 0) instructionCounter = args[0]; break;
+			case 5: if(args[1] != 0) { instructionCounter = args[0]; return -1; } break;
+			case 6: if(args[1] == 0) { instructionCounter = args[0]; return -1; } break;
 			case 7: program[args[0]] = args[1] < args[2] ? 1 : 0; break;
 			case 8: program[args[0]] = args[1] == args[2] ? 1 : 0; break;
 			default: throw new IllegalStateException("Something went wrong!");
 		}
-		
+		instructionCounter+=nParams(instruction) + 1;
 		return -1;
 	}
 	
