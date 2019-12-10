878643446d939d5d0635dfebed81a0bb8470b06e
Simon <simon.mailadres@gmail.com>
2019-12-10 13:29:22 +0100

    Final refactoring

--- src/main/java/com/sbaars/adventofcode2019/days/Day10.java
+++ src/main/java/com/sbaars/adventofcode2019/days/Day10.java
@@ -8,6 +8,8 @@ import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import javax.swing.text.Position;
+
 import com.sbaars.adventofcode2019.common.Day;
 import com.sbaars.adventofcode2019.util.DoesFileOperations;
 
@@ -33,41 +35,24 @@ public class Day10 implements Day, DoesFileOperations {
 		return Math.toIntExact(Arrays.stream(nVisible).max().getAsLong());
 	}
 	
-	private long countNVisible(Point asteroid) {
-		return asteroids.stream().map(e -> new Asteroid(calcRotationAngleInDegrees(asteroid, e), asteroid.distance(e), e)).mapToDouble(e -> e.rotation).distinct().count();
-	}
-	
 	@Override
 	public int part2() throws IOException {
-		List<Asteroid> asteroidList = asteroids.stream().map(e -> new Asteroid(calcRotationAngleInDegrees(baseLocation, e), baseLocation.distance(e), e)).collect(Collectors.toList());
-		Asteroid prevDestroyed = new Asteroid(Double.MIN_VALUE, 0, new Point(0,0));
+		List<Asteroid> asteroidList = asteroids.stream().map(e -> new Asteroid(baseLocation, e)).collect(Collectors.toList());
+		Asteroid prevDestroyed = new Asteroid();
 		for(int destroyed = 1; destroyed<200; destroyed++) {
 			Asteroid prev = prevDestroyed;
 			OptionalDouble nextRot = asteroidList.stream().mapToDouble(e -> e.rotation).filter(e -> e > prev.rotation).min();
 			if(nextRot.isPresent()) {
 				double nextRotation = nextRot.getAsDouble();
-				Asteroid a = asteroidList.stream().filter(e -> e.rotation == nextRotation).reduce((a1, a2) -> a1.distance < a2.distance ? a1 : a2).get();
-				asteroidList.remove(a);
-				prevDestroyed = a;
-			} else {
-				prevDestroyed = new Asteroid(Double.MIN_VALUE, 0, new Point(0,0));
-				destroyed--;
+				prevDestroyed = asteroidList.stream().filter(e -> e.rotation == nextRotation).reduce((a1, a2) -> a1.distance < a2.distance ? a1 : a2).get();
+				asteroidList.remove(prevDestroyed);
 			}
 		}
 		return prevDestroyed.position.x*100+prevDestroyed.position.y;
 	}
 	
-	public static double calcRotationAngleInDegrees(Point centerPt, Point targetPt)
-	{
-	    double theta = Math.atan2(targetPt.y - centerPt.y, targetPt.x - centerPt.x);
-	    theta += Math.PI/2.0;
-	    
-	    double angle = Math.toDegrees(theta);
-	    if (angle < 0) {
-	        angle += 360;
-	    }
-
-	    return angle;
+	private long countNVisible(Point asteroid) {
+		return asteroids.stream().map(e -> new Asteroid(asteroid, e)).mapToDouble(e -> e.rotation).distinct().count();
 	}
 	
 	class Asteroid {
@@ -75,11 +60,20 @@ public class Day10 implements Day, DoesFileOperations {
 		double distance;
 		Point position;
 		
-		public Asteroid(double rotation, double distance, Point position) {
-			super();
-			this.rotation = rotation;
-			this.distance = distance;
-			this.position = position;
+		public Asteroid(Point center, Point me) {
+			this.rotation = calcRotationAngleInDegrees(center, me);
+			this.distance = me.distance(center);
+			this.position = me;
+		}
+		
+		public Asteroid() {
+			this.rotation = Double.MIN_VALUE;
+		}
+		
+		private double calcRotationAngleInDegrees(Point centerPt, Point targetPt) {
+		    double theta = Math.atan2(targetPt.y - centerPt.y, targetPt.x - centerPt.x) + Math.PI/2.0;
+		    double angle = Math.toDegrees(theta);
+		    return angle < 0 ? angle + 360 : angle;
 		}
 
 		@Override
