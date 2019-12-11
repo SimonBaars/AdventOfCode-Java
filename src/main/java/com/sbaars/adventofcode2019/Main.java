e87eb70ea26cfc290ace91e528b59cdaf02c9879
Simon <simon.mailadres@gmail.com>
2019-12-11 20:21:44 +0100

    Main method to run all

---
+++ src/main/java/com/sbaars/adventofcode2019/Main.java
@@ -0,0 +1,17 @@
+package com.sbaars.adventofcode2019;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import com.sbaars.adventofcode2019.common.Day;
+
+public class Main {
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, IOException, InvocationTargetException, NoSuchMethodException {
+		for(int day = 1; day<=11; day++) {
+			System.out.println("Day "+day+":");
+			Day instance = (Day)Class.forName("com.sbaars.adventofcode2019.days.Day"+day).getDeclaredConstructor().newInstance();
+			instance.printParts();
+			System.out.println();
+		}
+	}
+}
