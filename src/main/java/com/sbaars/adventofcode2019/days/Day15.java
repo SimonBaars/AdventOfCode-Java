0ad2b1be7eee62585fb9741e51a2411c57cbfba1
Simon <simon.mailadres@gmail.com>
2019-12-15 18:04:26 +0100

    Day 15: Stub

--- src/main/java/com/sbaars/adventofcode2019/days/Day15.java
+++ src/main/java/com/sbaars/adventofcode2019/days/Day15.java
@@ -1,8 +1,14 @@
 package com.sbaars.adventofcode2019.days;
 
+import java.awt.Point;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import com.sbaars.adventofcode2019.common.Day;
+import com.sbaars.adventofcode2019.common.Direction;
+import com.sbaars.adventofcode2019.intcode.IntcodeComputer;
 
 public class Day15 implements Day {
 
@@ -12,7 +18,16 @@ public class Day15 implements Day {
 
 	@Override
 	public Object part1() throws IOException {
-		return 0;
+		IntcodeComputer ic = new IntcodeComputer(15);
+		List<Point> walls = new ArrayList<>();
+		Point pos = new Point(50,50);
+		int[][] grid = new int[100][100];
+		for(int[] row : grid) Arrays.fill(row, -1);
+		grid[pos.y][pos.x] = 0;
+		Direction dir  = Direction.NORTH;
+		while(true) {
+			ic.run();
+		}
 	}
 	
 	@Override
