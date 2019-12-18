80feb7d34d6ffe614cf97d9bb9dcdbee74585364
Simon <simon.mailadres@gmail.com>
2019-12-18 20:17:52 +0100

    Added day 18 to README

--- src/main/java/com/sbaars/adventofcode2019/Main.java
+++ src/main/java/com/sbaars/adventofcode2019/Main.java
@@ -7,7 +7,7 @@ import com.sbaars.adventofcode2019.common.Day;
 
 public class Main {
 	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, IOException, InvocationTargetException, NoSuchMethodException {
-		for(int day = 1; day<=17; day++) {
+		for(int day = 1; day<=18; day++) {
 			System.out.println("Day "+day+":");
 			Day instance = (Day)Class.forName("com.sbaars.adventofcode2019.days.Day"+day).getDeclaredConstructor().newInstance();
 			instance.printParts();
