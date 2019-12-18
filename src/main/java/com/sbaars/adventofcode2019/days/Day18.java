c7f3fb4526e9f9eff3e6ae71dfdf4faff2ef13dc
Simon <simon.mailadres@gmail.com>
2019-12-18 07:49:06 +0100

    Day 18 Part 1 : Recursive

--- src/main/java/com/sbaars/adventofcode2019/days/Day18.java
+++ src/main/java/com/sbaars/adventofcode2019/days/Day18.java
@@ -17,7 +17,7 @@ public class Day18 implements Day {
 	CharGrid2d charGrid;
 	
 	public Day18() throws IOException {
-		grid = Arrays.stream(readDay(19).split(System.lineSeparator())).map(e -> e.toCharArray()).toArray(char[][]::new);
+		grid = Arrays.stream(readDay(18).split(System.lineSeparator())).map(e -> e.toCharArray()).toArray(char[][]::new);
 		charGrid = new CharGrid2d(grid, false);
 	}	
 
@@ -63,23 +63,23 @@ public class Day18 implements Day {
 	
 	public int findSteps(Point me, List<Character> collectedKeys, List<Point> keys) {
 		List<List<Point>> possibleMoves = keys.stream().map(e -> charGrid.findPath(me, e, collectedKeys)).filter(e -> !e.isEmpty()).collect(Collectors.toList());
-		
+		//System.out.println("moves "+possibleMoves.size());
 		//possibleMoves.addAll(doors.stream().filter(e -> collectedKeys.contains(grid[e.y][e.x])).map(e -> charGrid.findPath(meNow, e)).filter(e -> !e.isEmpty()).collect(Collectors.toList()));
 		//List<Point> takenMove = possibleMoves.stream().reduce((a, b) -> a.size()<b.size() ? a : b).get();
+		System.out.println(possibleMoves.size()+", "+collectedKeys.size());
 		List<Integer> nSteps = new ArrayList<>();
 		for(List<Point> takenMove : possibleMoves) {
 		//List<Point> takenMove = possibleMoves.get(new Random().nextInt(possibleMoves.size()));
 			//grid[me.y][me.x] = '.';
 			//me = takenMove.get(takenMove.size()-1);
 			Point newLoc = takenMove.get(takenMove.size()-1);
-			char collected = grid[me.y][me.x];
+			char collected = grid[newLoc.y][newLoc.x];
 			//System.out.println((takenMove.size()-1)+" steps to "+collected);
-			if(collected >= 'a' && collected <= 'z') {
-				collectedKeys.add(Character.toUpperCase(collected));
+			collectedKeys.add(Character.toUpperCase(collected));
 				//doors.stream().filter(e -> grid[e.y][e.x] == Character.toUpperCase(collected)).forEach(e -> grid[e.y][e.x] = '.');
 				//doors.removeIf(e -> grid[e.y][e.x] == Character.toUpperCase(collected));
 				keys.remove(newLoc);
-			} 
+			//System.out.println(collected);
 			//grid[newLoc.y][newLoc.x] = '@';
 			nSteps.add(findSteps(newLoc, new ArrayList<>(collectedKeys), new ArrayList<>(keys))+takenMove.size()-1);
 		}
