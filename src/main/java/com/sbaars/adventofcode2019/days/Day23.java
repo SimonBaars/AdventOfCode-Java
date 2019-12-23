59cdda9124fd5c014449893fd24a4d70a7c88f90
Simon <simon.mailadres@gmail.com>
2019-12-23 07:44:55 +0100

    Refactored day 23 part 1

--- src/main/java/com/sbaars/adventofcode2019/days/Day23.java
+++ src/main/java/com/sbaars/adventofcode2019/days/Day23.java
@@ -1,65 +1,39 @@
 package com.sbaars.adventofcode2019.days;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.stream.IntStream;
 
 import com.sbaars.adventofcode2019.common.Day;
-import com.sbaars.adventofcode2019.intcode.ComputerThread;
 import com.sbaars.adventofcode2019.intcode.IntcodeComputer;
+import com.sbaars.adventofcode2019.intcode.RetentionPolicy;
 
 public class Day23 implements Day {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println(new Day23().part2());
+		System.out.println(new Day23().part1());
 	}
 
 	@Override
 	public Object part1() throws IOException {
-		ComputerThread[] ic = IntStream.range(0, 50).mapToObj(i -> {
-			try {
-				return new ComputerThread(new IntcodeComputer(23, i));
-			} catch (IOException e) {
-				return null;
-			}
-		}).toArray(ComputerThread[]::new);
+		long[] program = readLongArray(23);
+		IntcodeComputer[] ic = IntStream.range(0, 50).mapToObj(i -> new IntcodeComputer(RetentionPolicy.EXIT_ON_EMPTY_INPUT, program, i, -1)).toArray(IntcodeComputer[]::new);
 		
-		
+		long input;
 		while(true) {
 			for(int i = 0; i<ic.length; i++) {
-				while(ic[i].output.size()>2) {
-					int pc = Math.toIntExact(ic[i].output.poll());
-					long x = ic[i].output.poll();
-					long y = ic[i].output.poll();
-					if(pc == 255) {
-						//System.out.println(x+", "+y+", "+pc);
-						//ic[pc].ic.addInput(x);
-						//ic[pc].ic.addInput(y);
-						//System.out.println(new IntcodeComputer(23, 255).run());
-						//Arrays.stream(ic).forEach(e -> e.stop());
-						return y;
-					}
-					ic[pc].ic.addInput(x);
-					ic[pc].ic.addInput(y);
-				}
-				/*while((pc = Math.toIntExact(ic[i].run()))!=-1) {
-					x = ic[i].run();
-					y = ic[i].run();
-					System.out.println(x+", "+y+", "+pc);
+				while((input = ic[i].run())!=IntcodeComputer.STOP_CODE) {
+					int pc = Math.toIntExact(input);
+					long x = ic[i].run(), y = ic[i].run();
 					if(pc == 255) return y;
-					ic[pc].addInput(x);
-					ic[pc].addInput(y);
-				}*/
+					ic[pc].addInput(x, y);
+				}
 			}
-			
-			//x = ic[pc].run();
-			//y = ic[pc].run();
-			//pc = Math.toIntExact(ic[pc].run());
 		}
 	}
 	
 	@Override
 	public Object part2() throws IOException {
+		return 0;/*
 		ComputerThread[] ic = IntStream.range(0, 50).mapToObj(i -> {
 			try {
 				return new ComputerThread(new IntcodeComputer(23, i));
@@ -104,11 +78,11 @@ public class Day23 implements Day {
 					ic[pc].addInput(x);
 					ic[pc].addInput(y);
 				}*/
-			}
+			//}
 			
 			//x = ic[pc].run();
 			//y = ic[pc].run();
 			//pc = Math.toIntExact(ic[pc].run());
-		}
+		//}
 	}
 }
