be80ea7940253d5e783b42ef394b4c8da8f8cd5d
Simon <simon.mailadres@gmail.com>
2019-12-23 08:00:47 +0100

    Day 23 refactoring

--- src/main/java/com/sbaars/adventofcode2019/days/Day23.java
+++ src/main/java/com/sbaars/adventofcode2019/days/Day23.java
@@ -16,24 +16,15 @@ public class Day23 implements Day {
 
 	@Override
 	public Object part1() throws IOException {
-		long[] program = readLongArray(23);
-		IntcodeComputer[] ic = IntStream.range(0, 50).mapToObj(i -> new IntcodeComputer(RetentionPolicy.EXIT_ON_EMPTY_INPUT, program, i, -1)).toArray(IntcodeComputer[]::new);
-		
-		long input;
-		while(true) {
-			for(int i = 0; i<ic.length; i++) {
-				while((input = ic[i].run())!=IntcodeComputer.STOP_CODE) {
-					int pc = Math.toIntExact(input);
-					long x = ic[i].run(), y = ic[i].run();
-					if(pc == 255) return y;
-					ic[pc].addInput(x, y);
-				}
-			}
-		}
+		return getNetworkNumber(true);
 	}
 	
 	@Override
 	public Object part2() throws IOException {
+		return getNetworkNumber(false);
+	}
+
+	private Object getNetworkNumber(boolean returnNatY) throws IOException {
 		long[] program = readLongArray(23);
 		IntcodeComputer[] ic = IntStream.range(0, 50).mapToObj(i -> new IntcodeComputer(RetentionPolicy.EXIT_ON_EMPTY_INPUT, program, i, -1)).toArray(IntcodeComputer[]::new);
 		long[] nat = new long[2];
@@ -42,23 +33,21 @@ public class Day23 implements Day {
 		long input;
 		while(true) {
 			boolean idle = true;
-			forloop: for(int i = 0; i<ic.length; i++) {
+			for(int i = 0; i<ic.length; i++) {
 				if((input = ic[i].run())!=IntcodeComputer.STOP_CODE) {
 					int pc = Math.toIntExact(input);
 					long x = ic[i].run(), y = ic[i].run();
 					if(pc == 255) {
+						if(returnNatY) return y;
 						nat[0] = x;
 						nat[1] = y;
-						continue forloop;
-					}
-					ic[pc].addInput(x, y);
+					} else ic[pc].addInput(x, y);
 					idle = false;
 				}
 			}
 			if(idle) {
 				ic[0].addInput(nat[0], nat[1]);
-				if(sentByNat == nat[1])
-					return nat[1];
+				if(sentByNat == nat[1]) return nat[1];
 				sentByNat = nat[1];
 			}
 		}
