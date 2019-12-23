a6ba01b1130c632d22fe37b7bda2b8c6a029e18a
Simon <simon.mailadres@gmail.com>
2019-12-23 07:55:29 +0100

    Refactored part 2

--- src/main/java/com/sbaars/adventofcode2019/days/Day23.java
+++ src/main/java/com/sbaars/adventofcode2019/days/Day23.java
@@ -1,5 +1,6 @@
 package com.sbaars.adventofcode2019.days;
 
+import java.awt.Point;
 import java.io.IOException;
 import java.util.stream.IntStream;
 
@@ -10,7 +11,7 @@ import com.sbaars.adventofcode2019.intcode.RetentionPolicy;
 public class Day23 implements Day {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println(new Day23().part1());
+		new Day23().printParts();
 	}
 
 	@Override
@@ -33,56 +34,33 @@ public class Day23 implements Day {
 	
 	@Override
 	public Object part2() throws IOException {
-		return 0;/*
-		ComputerThread[] ic = IntStream.range(0, 50).mapToObj(i -> {
-			try {
-				return new ComputerThread(new IntcodeComputer(23, i));
-			} catch (IOException e) {
-				return null;
-			}
-		}).toArray(ComputerThread[]::new);
+		long[] program = readLongArray(23);
+		IntcodeComputer[] ic = IntStream.range(0, 50).mapToObj(i -> new IntcodeComputer(RetentionPolicy.EXIT_ON_EMPTY_INPUT, program, i, -1)).toArray(IntcodeComputer[]::new);
+		long[] nat = new long[2];
+		long sentByNat = -1;
 		
-		IntcodeComputer nat = new IntcodeComputer(23, 255);
-		
-		long prev = -1;
+		long input;
 		while(true) {
-			for(int i = 0; i<ic.length; i++) {
-				while(ic[i].output.size()>2) {
-					int pc = Math.toIntExact(ic[i].output.poll());
-					long x = ic[i].output.poll();
-					long y = ic[i].output.poll();
+			boolean idle = true;
+			forloop: for(int i = 0; i<ic.length; i++) {
+				if((input = ic[i].run())!=IntcodeComputer.STOP_CODE) {
+					int pc = Math.toIntExact(input);
+					long x = ic[i].run(), y = ic[i].run();
 					if(pc == 255) {
-						nat.addInput(x);
-						nat.addInput(y);
-						pc = Math.toIntExact(nat.run());
-						x = nat.run();
-						y = nat.run();
-						System.out.println(x+", "+y+", "+pc);
-						if(y == prev)
-							return y;
-						prev = y;
-						//ic[pc].ic.addInput(x);
-						//ic[pc].ic.addInput(y);
-						//System.out.println(new IntcodeComputer(23, 255).run());
-						//Arrays.stream(ic).forEach(e -> e.stop());
-						//return y;
+						nat[0] = x;
+						nat[1] = y;
+						continue forloop;
 					}
-					ic[pc].ic.addInput(x);
-					ic[pc].ic.addInput(y);
+					ic[pc].addInput(x, y);
+					idle = false;
 				}
-				/*while((pc = Math.toIntExact(ic[i].run()))!=-1) {
-					x = ic[i].run();
-					y = ic[i].run();
-					System.out.println(x+", "+y+", "+pc);
-					if(pc == 255) return y;
-					ic[pc].addInput(x);
-					ic[pc].addInput(y);
-				}*/
-			//}
-			
-			//x = ic[pc].run();
-			//y = ic[pc].run();
-			//pc = Math.toIntExact(ic[pc].run());
-		//}
+			}
+			if(idle) {
+				ic[0].addInput(nat[0], nat[1]);
+				if(sentByNat == nat[1])
+					return nat[1];
+				sentByNat = nat[1];
+			}
+		}
 	}
 }
