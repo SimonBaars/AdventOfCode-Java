7649de650d90701112009da4afb557b8f0a52930
Simon <simon.mailadres@gmail.com>
2019-12-24 08:37:52 +0100

    Day 24 refactor

--- src/main/java/com/sbaars/adventofcode2019/days/Day24.java
+++ src/main/java/com/sbaars/adventofcode2019/days/Day24.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
 import com.sbaars.adventofcode2019.common.Day;
@@ -17,7 +20,17 @@ import lombok.EqualsAndHashCode;
 
 public class Day24 implements Day {
 	
-	Set<Hello> grids = new HashSet<>();
+	private final Set<Grid> grids = new HashSet<>();
+	private Map<Integer, char[][]> layers = new HashMap<>();
+	private final char[][] initialGrid;
+	
+	@EqualsAndHashCode @AllArgsConstructor @Data class Grid{
+		char[][] grid;
+	}
+	
+	public Day24() throws IOException {
+		this.initialGrid = Arrays.stream(readDay(24).split(System.lineSeparator())).map(e -> e.toCharArray()).toArray(char[][]::new);
+	}
 
 	public static void main(String[] args) throws IOException {
 		new Day24().printParts();
@@ -25,72 +38,49 @@ public class Day24 implements Day {
 
 	@Override
 	public Object part1() throws IOException {
-		char[][] grid = Arrays.stream(readDay(24).split(System.lineSeparator())).map(e -> e.toCharArray()).toArray(char[][]::new);
+		char[][] grid = initialGrid;
 		while(true) {
-			//Arrays.stream(grid).forEach(e -> System.out.println(Arrays.toString(e)));
-			if(!grids.add(new Hello(grid))) {
-				return calcRes(grid);
-			}
+			if(!grids.add(new Grid(grid))) return calcRes(grid);
 			char[][] newGrid = copy(grid);
 			for(int y = 0; y<grid.length; y++)
 				for(int x = 0; x<grid[0].length; x++)
-					simulate(grid, newGrid, y, x);
+					simulate(grid, newGrid, new Point(x, y));
 			grid = newGrid;
 		}
 	}
 
-	private void simulate(char[][] grid, char[][] newGrid, int y, int x) {
-		int adj = nAdjecent(grid, new Point(x,y));
-		if(grid[y][x] == '#' && adj != 1) {
-			newGrid[y][x] = '.';
-		} else if(grid[y][x] == '.' && (adj == 1 || adj == 2)) {
-			newGrid[y][x] = '#';
+	private void simulate(char[][] grid, char[][] newGrid, Point p) {
+		int adj = nAdjecent(grid, p);
+		if(grid[p.y][p.x] == '#' && adj != 1) {
+			newGrid[p.y][p.x] = '.';
+		} else if(grid[p.y][p.x] == '.' && (adj == 1 || adj == 2)) {
+			newGrid[p.y][p.x] = '#';
 		}
-	}
-	
-	@EqualsAndHashCode @AllArgsConstructor @Data class Hello{
-		char[][] grid;
 	}
 	
 	private long calcRes(char[][] grid) {
-		//	Arrays.stream(grid).forEach(e -> System.out.println(Arrays.toString(e)));
-		long res = 0;
-		long multiplier = 1;
-		for(int y = 0; y<grid.length; y++) {
-			for(int x = 0; x<grid[0].length; x++) {
-				if(grid[y][x] == '#') {
-					res+=multiplier;	
-				}
-				multiplier*=2;
-			}
-		}
-		return res;
+		return (long)IntStream.range(0, grid.length).boxed().flatMap(y -> IntStream.range(0, grid[y].length).mapToObj(x -> new Point(x,y))).filter(p -> grid[p.y][p.x] == '#').mapToDouble(p -> Math.pow(2, (p.y*grid.length)+p.x)).sum();
 	}
 
 	public char[][] copy(char[][] grid){
 		char[][] g2 = new char[grid.length][];
-		for(int i = 0; i<grid.length; i++) {
-			g2[i] = Arrays.copyOf(grid[i], grid[i].length);
-		}
+		for(int i = 0; i<grid.length; i++) g2[i] = Arrays.copyOf(grid[i], grid[i].length);
 		return g2;
 	}
 	
 	public int nAdjecent(char[][] grid, Point pos) {
-		return num(grid, pos.x, pos.y-1) + num(grid, pos.x, pos.y+1) + num(grid, pos.x-1, pos.y) + num(grid, pos.x+1, pos.y);
+		return num(grid, new Point(pos.x, pos.y-1)) + num(grid, new Point(pos.x, pos.y+1)) + num(grid, new Point(pos.x-1, pos.y)) + num(grid, new Point(pos.x+1, pos.y));
 	}
 	
-	private int num(char[][] grid, int x, int y) {
-		if(x<0 || y<0 || x>=grid.length || y>=grid.length) return 0;
-		return grid[y][x] == '#' ? 1 : 0;
+	private int num(char[][] grid, Point p) {
+		if(p.x<0 || p.y<0 || p.x>=grid.length || p.y>=grid.length) return 0;
+		return grid[p.y][p.x] == '#' ? 1 : 0;
 	}
-	
-	Map<Integer, char[][]> layers = new HashMap<>();
 
 	@Override
 	public Object part2() throws IOException {
 		char[][] grid = Arrays.stream(readDay(24).split(System.lineSeparator())).map(e -> e.toCharArray()).toArray(char[][]::new);
-		for(int i = -200; i<=200; i++)
-			layers.put(i, fill(new char[grid.length][grid[0].length], '.'));
+		for(int i = -200; i<=200; i++) layers.put(i, fill(new char[grid.length][grid[0].length], '.'));
 		layers.put(0, grid);
 		for(int i =0; i<200; i++) {
 			final Map<Integer, char[][]> layers2 = new HashMap<>();
@@ -99,14 +89,10 @@ public class Day24 implements Day {
 				for(int y = 0; y<grid.length; y++)
 					for(int x = 0; x<grid[0].length; x++)
 						if(x != 2 || y!=2)
-						simulate(layer, newGrid, y, x);
+							simulate(layer, newGrid, y, x);
 				layers2.put(layer, newGrid);
 			}
 			layers = layers2;
-		}
-		for(int layer = -200; layer<=200; layer++) {
-			System.out.println("Layer "+layer);
-			Arrays.stream(layers.get(layer)).forEach(e -> System.out.println(Arrays.toString(e)));
 		}
 		return layers.values().stream().mapToInt(e -> count(e, '#')).sum();
 	}
@@ -131,35 +117,20 @@ public class Day24 implements Day {
 	
 	public int nAdjecent(int layer, Point pos) {
 		int res = 0;
+		Point[] surround = new Point[] {new Point(2, 1), new Point(1, 2), new Point(2, 3), new Point(3, 2)};
 		if(layers.containsKey(layer+1)) {
-			if(pos.y == 0) {
-				res+=num(layers.get(layer + 1), 2 , 1);
-			}
-			if(pos.x == 0) {
-				res+=num(layers.get(layer + 1), 1 , 2);
-			}
-			if(pos.y == 4) {
-				res+=num(layers.get(layer + 1), 2 , 3);
-			}
-			if(pos.x == 4) {
-				res+=num(layers.get(layer + 1), 3 , 2);
-			}
+			List<Supplier<Boolean>> conditions = Arrays.asList(() -> pos.y == 0, () -> pos.x == 0, () -> pos.y == 4, () -> pos.x == 4);
+			res += IntStream.range(0, conditions.size()).filter(i -> conditions.get(i).get()).map(i -> num(layers.get(layer + 1), surround[i])).sum();
 		}
 		if(layers.containsKey(layer-1)) {
-			if(pos.equals(new Point(2, 1))) {
-				res+=IntStream.range(0,5).map(i -> num(layers.get(layer-1), i, 0)).sum();
-			}
-			if(pos.equals(new Point(1, 2))) {
-				res+=IntStream.range(0,5).map(i -> num(layers.get(layer-1), 0, i)).sum();
-			}
-			if(pos.equals(new Point(2, 3))) {
-				res+=IntStream.range(0,5).map(i -> num(layers.get(layer-1), i, 4)).sum();
-			}
-			if(pos.equals(new Point(3, 2))) {
-				res+=IntStream.range(0,5).map(i -> num(layers.get(layer-1), 4, i)).sum();
-			}
+			List<Function<Integer, Point>> fs = Arrays.asList(i -> new Point(i, 0), i -> new Point(0, i), i -> new Point(i, 4), i -> new Point(4, i));
+			res += IntStream.range(0, fs.size()).filter(i -> pos.equals(surround[i])).map(i -> nLayer(layer, fs.get(i))).sum();
 		}
 		return res + nAdjecent(layers.get(layer), pos);
+	}
+	
+	public int nLayer(int layer, Function<Integer, Point> f) {
+		return IntStream.range(0,5).map(i -> num(layers.get(layer-1), f.apply(i))).sum();
 	}
 	
 	public char[][] fill(char[][] arr, char el) {
