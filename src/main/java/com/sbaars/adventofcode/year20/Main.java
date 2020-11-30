37253614a1e4b977ecd3e511e5dfa2bd727515ab
Simon Baars <simon.mailadres@gmail.com>
2020-11-30 18:42:47 +0100

    Prepare for 2020 day one

--- src/main/java/com/sbaars/adventofcode/year20/Main.java
+++ src/main/java/com/sbaars/adventofcode/year20/Main.java
@@ -1,4 +1,16 @@
 package com.sbaars.adventofcode.year20;
 
+import com.sbaars.adventofcode.common.Day;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
 public class Main {
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, IOException, InvocationTargetException, NoSuchMethodException {
+        for(int day = 1; day<=1; day++) {
+            System.out.println("Day "+day+":");
+            Day instance = (Day)Class.forName("com.sbaars.adventofcode.year20.days.Day"+day).getDeclaredConstructor().newInstance();
+            instance.printParts();
+            System.out.println();
+        }
+    }
 }
