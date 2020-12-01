6a463cd97cf82a3c917c037ccece24a33d476e3a
Simon <simon.mailadres@gmail.com>
2020-12-01 19:57:46 +0100

    Day 2 stub

---
+++ src/main/java/com/sbaars/adventofcode/year20/days/Day2.java
@@ -0,0 +1,22 @@
+package com.sbaars.adventofcode.year20.days;
+
+import com.sbaars.adventofcode.common.Day;
+
+import java.io.IOException;
+
+public class Day2 implements Day {
+    public static void main(String[] args) throws IOException {
+        new Day2().printParts();
+    }
+
+    @Override
+    public Object part1() throws IOException {
+        String s = day(2);
+        return 0;
+    }
+
+    @Override
+    public Object part2() throws IOException {
+        return 0;
+    }
+}
