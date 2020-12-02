8dcb2073eb43b219a4dd7984c7d8ef659a925ecc
Simon <simon.mailadres@gmail.com>
2020-12-02 08:59:18 +0100

    Haskell converters

---
+++ src/main/java/com/sbaars/adventofcode/haskell/year20/Day2.java
@@ -0,0 +1,27 @@
+package com.sbaars.adventofcode.haskell.year20;
+
+import com.sbaars.adventofcode.year20.Day2020;
+
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+public class Day2 extends Day2020 {
+    public Day2() {
+        super(2);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Day2().printParts();
+    }
+
+    @Override
+    public Object part1() throws IOException {
+        return dayStream().map(com.sbaars.adventofcode.year20.days.Day2::mapPassword)
+                .map(p -> "(("+p.getLower()+", "+p.getHigher()+"), '"+p.getCharacter()+"', \""+p.getPassword()+"\")").collect(Collectors.joining(", ", "[", "]"));
+    }
+
+    @Override
+    public Object part2() throws IOException {
+        return null;
+    }
+}
