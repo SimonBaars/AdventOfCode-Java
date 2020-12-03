b5a79fb4e63da092969d345f62d50b2fcbb356b8
Simon Baars <simon.mailadres@gmail.com>
2020-12-03 06:48:07 +0100

    Day 3 2020 Haskell converter

---
+++ src/main/java/com/sbaars/adventofcode/haskell/year20/Day3.java
@@ -0,0 +1,29 @@
+package com.sbaars.adventofcode.haskell.year20;
+
+import com.sbaars.adventofcode.year20.Day2020;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Day3 extends Day2020 {
+    public Day3() {
+        super(3);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Day3().printParts();
+    }
+
+    @Override
+    public Object part1() throws IOException {
+        return Arrays.stream(dayGrid()).map(Arrays::toString)
+                .map(s -> s.replace('.', '0'))
+                .map(s -> s.replace('#', '1'))
+                .collect(Collectors.joining(", ", "[", "]"));
+    }
+
+    @Override
+    public Object part2() throws IOException {
+        return null;
+    }
+}
