9491347d2fa55d6526762fb8971f83868663fcca
Simon Baars <simon.mailadres@gmail.com>
2020-12-04 10:18:00 +0100

    Prepare day 5

---
+++ src/main/java/com/sbaars/adventofcode/year20/days/Day5.java
@@ -0,0 +1,30 @@
+package com.sbaars.adventofcode.year20.days;
+
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
+
+import com.sbaars.adventofcode.common.ReadsFormattedString;
+import com.sbaars.adventofcode.year20.Day2020;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+public class Day5 extends Day2020 {
+
+    public static void main(String[] args)  {
+        new Day5().printParts();
+    }
+
+    public Day5(){super(5);}
+
+    @Override
+    public Object part1()  {
+        String input = day();
+        return input;
+    }
+
+    @Override
+    public Object part2()  {
+        return 0;
+    }
+}
