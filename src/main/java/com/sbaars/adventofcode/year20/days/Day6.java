3d4cd6ca274aec757fc2f30a2c1ee4192b2cace8
Simon Baars <simon.mailadres@gmail.com>
2020-12-05 21:26:30 +0100

    Prepare day 6

---
+++ src/main/java/com/sbaars/adventofcode/year20/days/Day6.java
@@ -0,0 +1,25 @@
+package com.sbaars.adventofcode.year20.days;
+
+import com.sbaars.adventofcode.year20.Day2020;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Day6 extends Day2020 {
+
+    public static void main(String[] args)  {
+        new Day6().printParts();
+    }
+
+    public Day6(){super(6);}
+
+    @Override
+    public Object part1()  {
+        String input = day();
+        return input;
+    }
+
+    @Override
+    public Object part2()  {
+        return 0;
+    }
+}
