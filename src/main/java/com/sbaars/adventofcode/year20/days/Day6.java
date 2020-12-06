40829f09460d17c8491929ccba89630bab468497
Simon Baars <simon.mailadres@gmail.com>
2020-12-06 06:23:30 +0100

    Day 6

--- src/main/java/com/sbaars/adventofcode/year20/days/Day6.java
+++ src/main/java/com/sbaars/adventofcode/year20/days/Day6.java
@@ -1,7 +1,13 @@
 package com.sbaars.adventofcode.year20.days;
 
+import static com.google.common.primitives.Ints.asList;
+import static java.util.stream.IntStream.range;
+
 import com.sbaars.adventofcode.year20.Day2020;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Day6 extends Day2020 {
@@ -14,12 +20,17 @@ public class Day6 extends Day2020 {
 
     @Override
     public Object part1()  {
-        String input = day();
-        return input;
+        String[] input = day().split("\n\n");
+        return Arrays.stream(input).map(i -> i.replace("\n", "")).mapToLong(i -> i.chars().distinct().count()).sum();
     }
 
     @Override
     public Object part2()  {
-        return 0;
+        return Arrays.stream(day().split("\n\n")).mapToInt(group -> {
+            String[] people = group.split("\n");
+            List<Integer> c = new ArrayList<>(asList(people[0].chars().toArray()));
+            range(1, people.length).forEach(i -> c.retainAll(asList(people[i].chars().toArray())));
+            return c.size();
+        }).sum();
     }
 }
