70d26bd780cf4ae0ad2c1815554ee987e66ff245
Simon Baars <simon.mailadres@gmail.com>
2020-12-06 20:51:55 +0100

    Prepare day 7

---
+++ src/main/java/com/sbaars/adventofcode/year20/days/Day7.java
@@ -0,0 +1,29 @@
+package com.sbaars.adventofcode.year20.days;
+
+import static com.google.common.primitives.Ints.asList;
+import static java.util.stream.IntStream.range;
+
+import com.sbaars.adventofcode.year20.Day2020;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day7 extends Day2020 {
+
+    public static void main(String[] args)  {
+        new Day7().printParts();
+    }
+
+    public Day7(){super(7);}
+
+    @Override
+    public Object part1()  {
+        String input = day();
+        return input;
+    }
+
+    @Override
+    public Object part2()  {
+        return 0;
+    }
+}
