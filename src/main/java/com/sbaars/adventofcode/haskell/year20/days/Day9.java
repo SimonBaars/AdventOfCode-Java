449f7ae2eb89693e7798f2d096a14842177d844c
Simon Baars <simon.mailadres@gmail.com>
2020-12-09 06:58:29 +0100

    Haskell converter day 9

---
+++ src/main/java/com/sbaars/adventofcode/haskell/year20/days/Day9.java
@@ -0,0 +1,23 @@
+package com.sbaars.adventofcode.haskell.year20.days;
+
+import com.sbaars.adventofcode.haskell.year20.HaskellDay2020;
+
+public class Day9 extends HaskellDay2020 {
+    public Day9() {
+        super(9);
+    }
+
+    public static void main(String[] args)  {
+        new Day9().printParts();
+    }
+
+    @Override
+    public Object part1()  {
+        return dayNumberStream().mapToObj(Long::toString).collect(haskellList());
+    }
+
+    @Override
+    public Object part2()  {
+        return null;
+    }
+}
---
+++ src/main/java/com/sbaars/adventofcode/year20/days/Day10.java
@@ -0,0 +1,29 @@
+package com.sbaars.adventofcode.year20.days;
+
+import static java.util.Arrays.copyOfRange;
+import static java.util.Arrays.stream;
+
+import com.sbaars.adventofcode.year20.Day2020;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Day10 extends Day2020 {
+	public Day10()  {
+		super(10);
+	}
+
+	public static void main(String[] args)  {
+		new Day10().printParts();
+	}
+
+	@Override
+	public Object part1()  {
+		String input = day();
+		return input;
+	}
+
+	@Override
+	public Object part2()  {
+		return 0;
+	}
+}
