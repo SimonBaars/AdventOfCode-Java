3b38301ff1fb2b4e3cc72906d3ba2735c0484fa2
Simon Baars <simon.mailadres@gmail.com>
2020-12-10 08:20:04 +0100

    Day 10

---
+++ src/main/java/com/sbaars/adventofcode/year20/days/Day11.java
@@ -0,0 +1,29 @@
+package com.sbaars.adventofcode.year20.days;
+
+import static java.util.Arrays.sort;
+
+import com.sbaars.adventofcode.year19.util.CountMap;
+import com.sbaars.adventofcode.year19.util.LongCountMap;
+import com.sbaars.adventofcode.year20.Day2020;
+import org.apache.commons.lang3.ArrayUtils;
+
+public class Day11 extends Day2020 {
+	public Day11()  {
+		super(11);
+	}
+
+	public static void main(String[] args)  {
+		new Day11().printParts();
+	}
+
+	@Override
+	public Object part1()  {
+		String input = day();
+		return input;
+	}
+
+	@Override
+	public Object part2()  {
+		return 0;
+	}
+}
