0cd1f87d973009115b9c6c7ad9a4aee33948edab
Simon Baars <simon.mailadres@gmail.com>
2020-12-10 21:03:45 +0100

    Refactor day 8 solution

--- src/main/java/com/sbaars/adventofcode/year20/days/Day8.java
+++ src/main/java/com/sbaars/adventofcode/year20/days/Day8.java
@@ -3,6 +3,7 @@ package com.sbaars.adventofcode.year20.days;
 import static java.util.Arrays.stream;
 
 import com.sbaars.adventofcode.year20.Day2020;
+import com.sbaars.adventofcode.year20.gamepad.Gamepad;
 import java.util.HashSet;
 import java.util.Set;
 import lombok.AllArgsConstructor;
@@ -20,73 +21,25 @@ public class Day8 extends Day2020 {
 
     @Override
     public Object part1() {
-        Instruction[] input = dayStream().map(s -> s.replace("+", "")).map(s -> s.split(" ")).map(s -> new Instruction(s[0], Long.parseLong(s[1]))).toArray(Instruction[]::new);
-        long acc = 0L;
+        Gamepad gamepad = new Gamepad(dayStream());
         Set<Integer> visited = new HashSet<>();
-        int op = 0;
-        while (visited.add(op)) {
-            String operation = input[op].operation;
-            long number = input[op].number;
-            switch (operation) {
-                case "acc":
-                    acc += number;
-                    op++;
-                    break;
-                case "jmp":
-                    op += number;
-                    break;
-                case "nop":
-                    op++;
-                    break;
-                default:
-                    throw new IllegalStateException();
-            }
-        }
-        return acc;
-    }
-
-    @Data
-    @AllArgsConstructor
-    public static class Instruction {
-        String operation;
-        long number;
+        while (visited.add(gamepad.executeInstruction()));
+        return gamepad.getAccumulator();
     }
 
     @Override
     public Object part2() {
         for (int i = 0; i < 223; i++) {
-            Instruction[] input = dayStream().map(s -> s.replace("+", "")).map(s -> s.split(" ")).map(s -> new Instruction(s[0], Long.parseLong(s[1]))).toArray(Instruction[]::new);
-            replace(input, "jmp", "nop", i);
-            long acc = 0L;
+            Gamepad gamepad = new Gamepad(dayStream());
+            gamepad.replaceInstruction("jmp", "nop", i);
             Set<Integer> visited = new HashSet<>();
-            int op = 0;
-            while (visited.add(op)) {
-                String operation = input[op].operation;
-                long number = input[op].number;
-                switch (operation) {
-                    case "acc":
-                        acc += number;
-                        op++;
-                        break;
-                    case "jmp":
-                        op += number;
-                        break;
-                    case "nop":
-                        op++;
-                        break;
-                    default:
-                        throw new IllegalStateException();
+            while (visited.add(gamepad.executeInstruction())){
+                if (gamepad.getCurrent() == gamepad.getSize()){
+                    return gamepad.getAccumulator();
                 }
-                if (op == input.length) return acc;
-                if (op > input.length) break;
             }
 
         }
-        return "FAIL";
-    }
-
-    public void replace(Instruction[] input, String instruction, String replacement, int occurrence) {
-        Instruction[] those = stream(input).filter(e -> e.operation.equals(instruction)).toArray(Instruction[]::new);
-        those[occurrence].operation = replacement;
+        throw new IllegalStateException();
     }
 }
---
+++ src/main/java/com/sbaars/adventofcode/year20/gamepad/Gamepad.java
@@ -0,0 +1,55 @@
+package com.sbaars.adventofcode.year20.gamepad;
+
+import static java.util.Arrays.stream;
+
+import java.util.stream.Stream;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+public class Gamepad {
+    private final Instruction[] instructions;
+    private long acc = 0;
+    private int current = 0;
+
+    public Gamepad(Stream<String> input){
+        this.instructions = input.map(s -> s.replace("+", ""))
+                .map(s -> s.split(" "))
+                .map(s -> new Instruction(s[0], Long.parseLong(s[1])))
+                .toArray(Instruction[]::new);
+    }
+
+    public int executeInstruction(){
+        Instruction instruction = instructions[current];
+        switch (instruction.operation) {
+            case "acc": acc += instruction.number; break;
+            case "jmp": current += instruction.number - 1; break;
+            case "nop": break;
+            default: throw new IllegalStateException();
+        }
+        return current++;
+    }
+
+    public void replaceInstruction(String instruction, String replacement, int occurrence) {
+        Instruction[] those = stream(instructions).filter(e -> e.operation.equals(instruction)).toArray(Instruction[]::new);
+        those[occurrence].operation = replacement;
+    }
+
+    public long getAccumulator(){
+        return acc;
+    }
+
+    public int getCurrent(){
+        return current;
+    }
+
+    public int getSize(){
+        return instructions.length;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class Instruction {
+        String operation;
+        long number;
+    }
+}
