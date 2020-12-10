3c26a14ac6c4693909bdf07df18e45040d43df0a
Simon Baars <simon.mailadres@gmail.com>
2020-12-10 21:07:28 +0100

    Day 8 Haskell converter

---
+++ src/main/java/com/sbaars/adventofcode/haskell/year20/days/Day8.java
@@ -0,0 +1,30 @@
+package com.sbaars.adventofcode.haskell.year20.days;
+
+import static java.util.Arrays.stream;
+
+import com.sbaars.adventofcode.haskell.year20.HaskellDay2020;
+import com.sbaars.adventofcode.year20.days.Day7.Item;
+import com.sbaars.adventofcode.year20.days.Day7.Trade;
+import com.sbaars.adventofcode.year20.gamepad.Gamepad;
+
+public class Day8 extends HaskellDay2020 {
+    public Day8() {
+        super(8);
+    }
+
+    public static void main(String[] args)  {
+        new Day8().printParts();
+    }
+
+    @Override
+    public Object part1()  {
+        return stream(new Gamepad(dayStream()).getInstructions())
+                .map(i -> tup(i.getOperation(), i.getNumber()))
+                .collect(haskellList());
+    }
+
+    @Override
+    public Object part2()  {
+        return null;
+    }
+}
