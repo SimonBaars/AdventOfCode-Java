9499d04a8e0ad23970301164ee164bfaa8062f11
Simon <simon.mailadres@gmail.com>
2020-12-13 21:10:16 +0100

    Prepare day 14

---
+++ src/main/java/com/sbaars/adventofcode/year20/days/Day14.java
@@ -0,0 +1,29 @@
+package com.sbaars.adventofcode.year20.days;
+
+import com.sbaars.adventofcode.year20.Day2020;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.util.Arrays.stream;
+import static java.util.stream.IntStream.range;
+
+public class Day14 extends Day2020 {
+    public static void main(String[] args) {
+        new Day14().printParts();
+    }
+
+    public Day14() {
+        super(14);
+    }
+
+    @Override
+    public Object part1() {
+        String input = day();
+        return input;
+    }
+
+    @Override
+    public Object part2() {
+        return 0;
+    }
+}
