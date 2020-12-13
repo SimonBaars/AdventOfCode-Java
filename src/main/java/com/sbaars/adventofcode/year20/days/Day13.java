b6e2327e13d70acbca1d8dae366cf5047cec8db8
Simon Baars <simon.mailadres@gmail.com>
2020-12-13 06:54:45 +0100

    Day 13

--- src/main/java/com/sbaars/adventofcode/year20/days/Day13.java
+++ src/main/java/com/sbaars/adventofcode/year20/days/Day13.java
@@ -2,6 +2,7 @@ package com.sbaars.adventofcode.year20.days;
 
 import static com.sbaars.adventofcode.common.Direction.EAST;
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.range;
 
 import com.sbaars.adventofcode.common.Direction;
 import com.sbaars.adventofcode.year20.Day2020;
@@ -21,12 +22,25 @@ public class Day13 extends Day2020 {
 
     @Override
     public Object part1() {
-        String input = day();
-        return input;
+        int timestamp = 1005526;
+        int[] times = {37,41,587,13,19,23,29,733,17};
+        for(int i = timestamp; true; i++){
+            for(int j : times){
+                if(i%j == 0){
+                    return j * (i-timestamp);
+                }
+            }
+        }
     }
 
     @Override
     public Object part2() {
-        return 0;
+        int[] nums = {37,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,41,0,0,0,0,0,0,0,0,0,587,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,13,19,0,0,0,23,0,0,0,0,0,29,0,733,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,17};
+        for(long match = 100000000000000L; true; match++){
+            long finalMatch = match;
+            if(range(0, nums.length).filter(j -> nums[j] != 0).allMatch(j -> (finalMatch + j) % nums[j] == 0)) {
+                return match;
+            }
+        }
     }
 }
