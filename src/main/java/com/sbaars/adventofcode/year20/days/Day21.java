2308b7069c5b7348cda62abffd6628af62a713f5
Simon Baars <simon.mailadres@gmail.com>
2020-12-20 09:17:21 +0100

    Day 20

---
+++ src/main/java/com/sbaars/adventofcode/year20/days/Day21.java
@@ -0,0 +1,24 @@
+package com.sbaars.adventofcode.year20.days;
+
+import com.sbaars.adventofcode.year20.Day2020;
+
+public class Day21 extends Day2020 {
+    public static void main(String[] args) {
+        new Day21().printParts();
+    }
+
+    public Day21() {
+        super(21);
+    }
+
+    @Override
+    public Object part1() {
+        String input = day();
+        return input;
+    }
+
+    @Override
+    public Object part2() {
+        return 0;
+    }
+}
