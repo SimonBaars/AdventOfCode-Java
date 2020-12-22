c24ecc0c5f1d0c85b185c2f013bb99aed6a0189d
Simon <simon.mailadres@gmail.com>
2020-12-22 08:15:45 +0100

    Day 22

--- src/main/java/com/sbaars/adventofcode/year20/days/Day22.java
+++ src/main/java/com/sbaars/adventofcode/year20/days/Day22.java
@@ -1,6 +1,18 @@
 package com.sbaars.adventofcode.year20.days;
 
+import com.google.common.collect.Comparators;
 import com.sbaars.adventofcode.year20.Day2020;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.util.stream.LongStream;
+
+import static java.lang.Math.toIntExact;
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toCollection;
+import static org.apache.commons.lang3.ArrayUtils.subarray;
 
 public class Day22 extends Day2020 {
     public static void main(String[] args) {
@@ -13,12 +25,69 @@ public class Day22 extends Day2020 {
 
     @Override
     public Object part1() {
-        String input = day();
-        return input;
+        String[] input = day().split("\n\n");
+        Deque<Long> p1 = Arrays.stream(input[0].split("\n")).filter(e -> !e.startsWith("Player")).map(Long::parseLong).collect(toCollection(ArrayDeque::new));
+        Deque<Long> p2 = Arrays.stream(input[1].split("\n")).filter(e -> !e.startsWith("Player")).map(Long::parseLong).collect(toCollection(ArrayDeque::new));
+
+        while(p1.size()>0 && p2.size() > 0){
+            long l1 = p1.pop();
+            long l2 = p2.pop();
+            if(l1 > l2){
+                p1.add(l1);
+                p1.add(l2);
+            } else {
+                p2.add(l2);
+                p2.add(l1);
+            }
+        }
+        Deque<Long> winner = p1.size()>0 ? p1 : p2;
+        return calcScore(winner);
+    }
+
+    private long calcScore(Deque<Long> winner) {
+        return LongStream.rangeClosed(1, winner.size()).boxed().sorted(Comparator.reverseOrder()).mapToLong(l -> winner.pop() * l).sum();
     }
 
     @Override
     public Object part2() {
-        return 0;
+        String[] input = day().split("\n\n");
+        Deque<Long> p1 = Arrays.stream(input[0].split("\n")).filter(e -> !e.startsWith("Player")).map(Long::parseLong).collect(toCollection(ArrayDeque::new));
+        Deque<Long> p2 = Arrays.stream(input[1].split("\n")).filter(e -> !e.startsWith("Player")).map(Long::parseLong).collect(toCollection(ArrayDeque::new));
+
+
+        Deque<Long> winner = playGame(p1, p2) == Player.P1 ? p1 : p2;
+        return calcScore(winner);
     }
+
+    public Player playGame(Deque<Long> p1, Deque<Long> p2){
+        Set<List<Long>> playedGames = new HashSet<>();
+        while(p1.size()>0 && p2.size() > 0){
+            if(!playedGames.add(new ArrayList<>(p1))){
+                return Player.P1;
+            }
+
+            long l1 = p1.pop();
+            long l2 = p2.pop();
+            if(p1.size() < l1 || p2.size() < l2){
+                if(l1 > l2){
+                    p1.add(l1);
+                    p1.add(l2);
+                } else {
+                    p2.add(l2);
+                    p2.add(l1);
+                }
+            } else {
+                if(playGame(new ArrayDeque<>(new ArrayList<>(p1).subList(0, toIntExact(l1))), new ArrayDeque<>(new ArrayList<>(p2).subList(0, toIntExact(l2)))) == Player.P1){
+                    p1.add(l1);
+                    p1.add(l2);
+                } else {
+                    p2.add(l2);
+                    p2.add(l1);
+                }
+            }
+        }
+        return p1.size()>0 ? Player.P1 : Player.P2;
+    }
+
+    enum Player{P1, P2}
 }
