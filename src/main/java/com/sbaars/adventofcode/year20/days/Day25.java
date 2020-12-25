fa6c6d664c17cbe8b049cf1ef26bd810ae01ba8c
Simon Baars <simon.mailadres@gmail.com>
2020-12-25 06:48:53 +0100

    Day 25

--- src/main/java/com/sbaars/adventofcode/year20/days/Day25.java
+++ src/main/java/com/sbaars/adventofcode/year20/days/Day25.java
@@ -6,6 +6,8 @@ public class Day25 extends Day2020 {
     public static void main(String[] args) {
         new Day25().printParts();
     }
+    long cardPublicKey = 1965712L;
+    long doorPublicKey = 19072108L;
 
     public Day25() {
         super(25);
@@ -13,12 +15,25 @@ public class Day25 extends Day2020 {
 
     @Override
     public Object part1() {
-        String input = day();
-        return input;
+        long value = 1;
+        for(int loopSize = 1; true; loopSize++) {
+            value = transform(value, loopSize-1, loopSize, 7);
+            if(value == cardPublicKey) {
+                return transform(1, 0, loopSize, doorPublicKey);
+            }
+        }
+    }
+
+    public long transform (long value, int start, int loopSize, long subjectNumber){
+        for (int i = start; i < loopSize; i++) {
+            value *= subjectNumber;
+            value %= 20201227;
+        }
+        return value;
     }
 
     @Override
     public Object part2() {
-        return 0;
+        return "That's all folks";
     }
 }
