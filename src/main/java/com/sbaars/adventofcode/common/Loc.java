18084b4c5eea0e6f910e18c1f24c49985b62868e
Simon Baars <simon.mailadres@gmail.com>
2021-12-02 07:24:03 +0100

    Use reduce

--- src/main/java/com/sbaars/adventofcode/common/Loc.java
+++ src/main/java/com/sbaars/adventofcode/common/Loc.java
@@ -26,183 +26,128 @@
 package com.sbaars.adventofcode.common;
 
 import com.google.common.base.Objects;
-import java.awt.geom.Point2D;
-import java.beans.Transient;
+import java.awt.*;
 import java.io.Serial;
+import java.io.Serializable;
 
 /**
  * A point representing a location in {@code (x,y)} coordinate space,
  * specified in integer precision.
  *
- * @author      Sami Shaio
- * @since       1.0
+ * @author Sami Shaio
+ * @since 1.0
  */
-public class Loc extends Point2D implements java.io.Serializable {
-    /**
-     * The X coordinate of this {@code Point}.
-     * If no X coordinate is set it will default to 0.
-     *
-     * @serial
-     * @see #getLocation()
-     * @see #move(int, int)
-     * @since 1.0
-     */
-    public long x;
+public class Loc implements Serializable {
+  /**
+   * The X coordinate of this {@code Point}.
+   * If no X coordinate is set it will default to 0.
+   *
+   * @serial
+   * @see #move(int, int)
+   * @since 1.0
+   */
+  public final long x;
 
-    /**
-     * The Y coordinate of this {@code Point}.
-     * If no Y coordinate is set it will default to 0.
-     *
-     * @serial
-     * @see #getLocation()
-     * @see #move(int, int)
-     * @since 1.0
-     */
-    public long y;
+  /**
+   * The Y coordinate of this {@code Point}.
+   * If no Y coordinate is set it will default to 0.
+   *
+   * @serial
+   * @see #move(int, int)
+   * @since 1.0
+   */
+  public final long y;
 
-    /**
-     * Use serialVersionUID from JDK 1.1 for interoperability.
-     */
-    @Serial
-    private static final long serialVersionUID = -5276940640259749850L;
+  /**
+   * Use serialVersionUID from JDK 1.1 for interoperability.
+   */
+  @Serial
+  private static final long serialVersionUID = -5276940640259749850L;
 
-    /**
-     * Constructs and initializes a point at the origin
-     * (0,&nbsp;0) of the coordinate space.
-     * @since       1.1
-     */
-    public Loc() {
-        this(0, 0);
-    }
+  /**
+   * Constructs and initializes a point at the origin
+   * (0,&nbsp;0) of the coordinate space.
+   *
+   * @since 1.1
+   */
+  public Loc() {
+    this(0, 0);
+  }
 
-    /**
-     * Constructs and initializes a point with the same location as
-     * the specified {@code Point} object.
-     * @param       p a point
-     * @since       1.1
-     */
-    public Loc(Loc p) {
-        this(p.x, p.y);
-    }
+  /**
+   * Constructs and initializes a point with the same location as
+   * the specified {@code Point} object.
+   *
+   * @param p a point
+   * @since 1.1
+   */
+  public Loc(Loc p) {
+    this(p.x, p.y);
+  }
 
-    /**
-     * Constructs and initializes a point at the specified
-     * {@code (x,y)} location in the coordinate space.
-     * @param x the X coordinate of the newly constructed {@code Point}
-     * @param y the Y coordinate of the newly constructed {@code Point}
-     * @since 1.0
-     */
-    public Loc(long x, long y) {
-        this.x = x;
-        this.y = y;
-    }
+  /**
+   * Constructs and initializes a point at the specified
+   * {@code (x,y)} location in the coordinate space.
+   *
+   * @param x the X coordinate of the newly constructed {@code Point}
+   * @param y the Y coordinate of the newly constructed {@code Point}
+   * @since 1.0
+   */
+  public Loc(long x, long y) {
+    this.x = x;
+    this.y = y;
+  }
 
-    /**
-     * {@inheritDoc}
-     * @since 1.2
-     */
-    public double getX() {
-        return x;
-    }
+  public Loc(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
 
-    /**
-     * {@inheritDoc}
-     * @since 1.2
-     */
-    public double getY() {
-        return y;
-    }
+  public Loc(Point p) {
+    this.x = p.x;
+    this.y = p.y;
+  }
 
-    /**
-     * Returns the location of this point.
-     * This method is included for completeness, to parallel the
-     * {@code getLocation} method of {@code Component}.
-     * @return      a copy of this point, at the same location
-     * @see         java.awt.Component#getLocation
-     * @see         java.awt.Point#setLocation(java.awt.Point)
-     * @see         java.awt.Point#setLocation(int, int)
-     * @since       1.1
-     */
-    @Transient
-    public Loc getLocation() {
-        return new Loc(x, y);
-    }
+  /**
+   * {@inheritDoc}
+   *
+   * @since 1.2
+   */
+  public double getX() {
+    return x;
+  }
 
-    /**
-     * Sets the location of the point to the specified location.
-     * This method is included for completeness, to parallel the
-     * {@code setLocation} method of {@code Component}.
-     * @param       p  a point, the new location for this point
-     * @see         java.awt.Component#setLocation(java.awt.Point)
-     * @see         java.awt.Point#getLocation
-     * @since       1.1
-     */
-    public void setLocation(Loc p) {
-        setLocation(p.x, p.y);
-    }
+  /**
+   * {@inheritDoc}
+   *
+   * @since 1.2
+   */
+  public double getY() {
+    return y;
+  }
 
-    /**
-     * Changes the point to have the specified location.
-     * <p>
-     * This method is included for completeness, to parallel the
-     * {@code setLocation} method of {@code Component}.
-     * Its behavior is identical with <code>move(int,&nbsp;int)</code>.
-     * @param       x the X coordinate of the new location
-     * @param       y the Y coordinate of the new location
-     * @see         java.awt.Component#setLocation(int, int)
-     * @see         java.awt.Point#getLocation
-     * @see         java.awt.Point#move(int, int)
-     * @since       1.1
-     */
-    public void setLocation(int x, int y) {
-        move(x, y);
-    }
+  /**
+   * Translates this point, at location {@code (x,y)},
+   * by {@code dx} along the {@code x} axis and {@code dy}
+   * along the {@code y} axis so that it now represents the point
+   * {@code (x+dx,y+dy)}.
+   *
+   * @param dx the distance to move this point
+   *           along the X axis
+   * @param dy the distance to move this point
+   *           along the Y axis
+   */
+  public Loc move(int dx, int dy) {
+    return new Loc(x + dx, y + dy);
+  }
 
-    /**
-     * Sets the location of this point to the specified double coordinates.
-     * The double values will be rounded to integer values.
-     * Any number smaller than {@code Integer.MIN_VALUE}
-     * will be reset to {@code MIN_VALUE}, and any number
-     * larger than {@code Integer.MAX_VALUE} will be
-     * reset to {@code MAX_VALUE}.
-     *
-     * @param x the X coordinate of the new location
-     * @param y the Y coordinate of the new location
-     * @see #getLocation
-     */
-    public void setLocation(double x, double y) {
-        this.x = (int) Math.floor(x+0.5);
-        this.y = (int) Math.floor(y+0.5);
-    }
+  public Loc move(Loc l) {
+    return new Loc(x + l.x, y + l.y);
+  }
 
-    /**
-     * Moves this point to the specified location in the
-     * {@code (x,y)} coordinate plane. This method
-     * is identical with <code>setLocation(int,&nbsp;int)</code>.
-     * @param       x the X coordinate of the new location
-     * @param       y the Y coordinate of the new location
-     * @see         java.awt.Component#setLocation(int, int)
-     */
-    public void move(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    /**
-     * Translates this point, at location {@code (x,y)},
-     * by {@code dx} along the {@code x} axis and {@code dy}
-     * along the {@code y} axis so that it now represents the point
-     * {@code (x+dx,y+dy)}.
-     *
-     * @param       dx   the distance to move this point
-     *                            along the X axis
-     * @param       dy    the distance to move this point
-     *                            along the Y axis
-     */
-    public void translate(int dx, int dy) {
-        this.x += dx;
-        this.y += dy;
-    }
+  public Point getPoint() {
+    return new Point(Math.toIntExact(x), Math.toIntExact(y));
+  }
 
   @Override
   public boolean equals(Object o) {
@@ -219,15 +164,15 @@ public class Loc extends Point2D implements java.io.Serializable {
   }
 
   /**
-     * Returns a string representation of this point and its location
-     * in the {@code (x,y)} coordinate space. This method is
-     * intended to be used only for debugging purposes, and the content
-     * and format of the returned string may vary between implementations.
-     * The returned string may be empty but may not be {@code null}.
-     *
-     * @return  a string representation of this point
-     */
-    public String toString() {
-        return getClass().getName() + "[x=" + x + ",y=" + y + "]";
-    }
+   * Returns a string representation of this point and its location
+   * in the {@code (x,y)} coordinate space. This method is
+   * intended to be used only for debugging purposes, and the content
+   * and format of the returned string may vary between implementations.
+   * The returned string may be empty but may not be {@code null}.
+   *
+   * @return a string representation of this point
+   */
+  public String toString() {
+    return getClass().getName() + "[x=" + x + ",y=" + y + "]";
+  }
 }
