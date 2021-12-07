7cf47686bb1ea6e9ffb9c4159daf1bae7e39dc99
Simon Baars <simon.mailadres@gmail.com>
2021-12-07 06:27:41 +0100

    Day 7 refactored

--- src/main/java/com/sbaars/adventofcode/year21/days/Day7.java
+++ src/main/java/com/sbaars/adventofcode/year21/days/Day7.java
@@ -1,10 +1,7 @@
 package com.sbaars.adventofcode.year21.days;
 
-import static java.lang.Math.toIntExact;
-
 import com.sbaars.adventofcode.year21.Day2021;
 import java.io.IOException;
-import java.math.BigInteger;
 import java.util.stream.LongStream;
 
 public class Day7 extends Day2021 {
@@ -15,42 +12,31 @@ public class Day7 extends Day2021 {
 
   public static void main(String[] args) throws IOException {
     new Day7().printParts();
-    System.in.read();
-//    new D?ay7().submitPart1();
-    new Day7().submitPart2();
   }
 
   @Override
   public Object part1() {
-    var in = dayNumbers(",");
-
-    return LongStream.of(in).map(e -> sol(in, e)).min().getAsLong();
+    return input().map(this::sol).min().getAsLong();
   }
 
-  private long sol(long[] nums, long guess){
-    long sol = 0;
-    for(long n : nums){
-      sol += guess > n ? guess-n : n-guess;
-    }
-    return sol;
+  private LongStream input() {
+    return dayNumberStream(",");
   }
 
-  private BigInteger sol2(long[] nums, long guess){
-    BigInteger sol = new BigInteger("0");
-    for(long n : nums){
-      sol = sol.add(guess > n ? fac(toIntExact(guess-n)) : fac(toIntExact(n-guess)));
-    }
-    return sol;
+  private long sol(long guess){
+    return getSteps(guess).sum();
   }
 
-  public BigInteger fac(long n) {
-    return new BigInteger(Long.toString(n*(n+1)/2));
+  private LongStream getSteps(long guess) {
+    return input().map(n -> guess > n ? guess - n : n - guess);
+  }
+
+  private long sol2(long guess){
+    return getSteps(guess).map(n -> n*(n+1)/2).sum();
   }
 
   @Override
   public Object part2() {
-    var in = dayNumbers(",");
-
-    return LongStream.of(in).mapToObj(e -> sol2(in, e)).reduce((a,b) -> a.compareTo(b) == -1 ? a : b).get();
+    return input().map(this::sol2).min().getAsLong();
   }
 }
