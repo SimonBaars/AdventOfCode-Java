b5acfd8dc6882a861fdcade0484db25b9d6c826f
Simon Baars <simon.mailadres@gmail.com>
2021-12-09 16:31:24 +0100

    Prepare next day

--- src/main/java/com/sbaars/adventofcode/year21/days/Day10.java
+++ src/main/java/com/sbaars/adventofcode/year21/days/Day10.java
@@ -1,18 +1,23 @@
 package com.sbaars.adventofcode.year21.days;
 
 import com.sbaars.adventofcode.year21.Day2021;
+import java.io.IOException;
 
 public class Day10 extends Day2021 {
   public Day10() {
     super(10);
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     new Day10().printParts();
+    System.in.read();
+    new Day10().submitPart1();
+    new Day10().submitPart2();
   }
 
   @Override
   public Object part1() {
+    var in = day();
     return "";
   }
 
