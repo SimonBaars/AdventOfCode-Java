97f2b47801dd6d5311b77d30f6f7608a9247b171
Simon Baars <simon.mailadres@gmail.com>
2021-12-11 06:17:35 +0100

    2021 Day 11

--- src/main/java/com/sbaars/adventofcode/year21/days/Day11.java
+++ src/main/java/com/sbaars/adventofcode/year21/days/Day11.java
@@ -1,23 +1,53 @@
 package com.sbaars.adventofcode.year21.days;
 
+import com.sbaars.adventofcode.common.Direction;
+import com.sbaars.adventofcode.common.grid.NumGrid;
 import com.sbaars.adventofcode.year21.Day2021;
+import java.awt.*;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Day11 extends Day2021 {
   public Day11() {
     super(11);
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     new Day11().printParts();
+    System.in.read();
+    new Day11().submitPart1();
+//    new Day11().submitPart2();
   }
 
   @Override
   public Object part1() {
-    return "";
+    var in = new NumGrid(day(), "\n", "");
+    long flashes = 0;
+    for(int i = 0; i<100; i++){
+      flashes+=in.stream().mapToLong(e -> flash(in, e)).sum();
+      in.stream().filter(p -> in.get(p) > 9).forEach(p -> in.set(p, 0));
+    }
+    return flashes;
+  }
+
+  private long flash(NumGrid in, Point e) {
+    long n = 0;
+    in.grid[e.x][e.y]++;
+    if(in.get(e) == 10){
+      n+=Arrays.stream(Direction.values()).map(d -> d.move(e)).filter(p -> in.get(p) != -1).mapToLong(p -> flash(in, p)).sum() + 1;
+    }
+    return n;
   }
 
   @Override
   public Object part2() {
-    return "";
+    var in = new NumGrid(day(), "\n", "");
+    for(int step = 1; true; step++){
+      in.stream().forEach(e -> flash(in, e));
+      in.stream().filter(p -> in.get(p) > 9).forEach(p -> in.set(p, 0));
+      if(in.stream().mapToLong(p -> in.get(p)).allMatch(e -> e == 0L)){
+        return step;
+      }
+    }
   }
 }
