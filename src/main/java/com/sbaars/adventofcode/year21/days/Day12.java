e60892c264e09b8a598f965cb9ecaf836bbd17f5
Simon Baars <simon.mailadres@gmail.com>
2021-12-12 06:30:10 +0100

    Day 12 2021

--- src/main/java/com/sbaars/adventofcode/year21/days/Day12.java
+++ src/main/java/com/sbaars/adventofcode/year21/days/Day12.java
@@ -1,27 +1,69 @@
 package com.sbaars.adventofcode.year21.days;
 
+import static com.google.common.collect.ImmutableListMultimap.toImmutableListMultimap;
+
+import com.google.common.collect.ImmutableListMultimap;
+import com.sbaars.adventofcode.year19.util.CountMap;
 import com.sbaars.adventofcode.year21.Day2021;
-import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
 
 public class Day12 extends Day2021 {
+
+  public static final String START = "start";
+
   public Day12() {
     super(12);
   }
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     new Day12().printParts();
-    System.in.read();
-    new Day11().submitPart1();
-//    new Day11().submitPart2();
   }
 
   @Override
   public Object part1() {
-    return "";
+    return findPos(START, getInput(), Set.of(START));
+  }
+
+  private ImmutableListMultimap<String, String> getInput() {
+    return dayStream().map(e -> e.split("-")).flatMap(e -> Stream.of(new String[]{e[0], e[1]}, new String[]{e[1], e[0]})).collect(toImmutableListMultimap(e -> e[0], e -> e[1]));
+  }
+
+  private long findPos(String s, ImmutableListMultimap<String, String> in, Set<String> visited) {
+    if(s.equals("end")) return 1;
+    long n = 0;
+    List<String> reachable = in.get(s);
+    for(String l : reachable){
+      if(!visited.contains(l)) {
+        Set<String> newSet = new HashSet<>(visited);
+        if(!l.toUpperCase().equals(l)) newSet.add(l);
+        n+=findPos(l, in, newSet);
+      }
+    }
+    return n;
   }
 
   @Override
   public Object part2() {
-    return "";
+    return findPos2(START, getInput(), Map.of(START, 2));
+  }
+
+  private long findPos2(String s, ImmutableListMultimap<String, String> in, Map<String, Integer> visited) {
+    if(s.equals("end")) {
+      return 1;
+    }
+    long n = 0;
+    List<String> reachable = in.get(s);
+    for(String l : reachable){
+      if((!visited.containsKey(l) || visited.get(l) < 2) && visited.values().stream().filter(e -> e==2).count() <= 2) {
+        CountMap<String> newSet = new CountMap<>(visited);
+        if(!l.toUpperCase().equals(l)) newSet.increment(l);
+        n+=findPos2(l, in, newSet);
+      }
+    }
+    return n;
   }
 }
--- src/main/java/com/sbaars/adventofcode/year21/days/Day13.java
+++ src/main/java/com/sbaars/adventofcode/year21/days/Day13.java
@@ -5,6 +5,9 @@ import com.sbaars.adventofcode.year21.Day2021;
 public class Day13 extends Day2021 {
   public Day13() {
     super(13);
+//    System.in.read();
+//    new Day11().submitPart1();
+//    new Day11().submitPart2();
   }
 
   public static void main(String[] args) {
