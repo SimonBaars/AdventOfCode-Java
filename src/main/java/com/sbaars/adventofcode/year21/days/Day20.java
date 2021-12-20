34eaf3bfb61d7075948612404b75769befa057f8
Simon Baars <simon.mailadres@gmail.com>
2021-12-20 07:49:37 +0100

    Refactoring

--- src/main/java/com/sbaars/adventofcode/year21/days/Day20.java
+++ src/main/java/com/sbaars/adventofcode/year21/days/Day20.java
@@ -23,11 +23,10 @@ import java.util.stream.Stream;
 public class Day20 extends Day2021 {
   public Day20() {
     super(20);
-    this.example = 1;
   }
 
   public static void main(String[] args) {
-    System.out.println(new Day20().part1());
+    new Day20().printParts();
 //    new Day20().submitPart1();
 //    new Day20().submitPart2();
   }
@@ -46,16 +45,14 @@ public class Day20 extends Day2021 {
         .map(IntLoc::getPoint)
         .collect(Collectors.toSet()));
     Coordinates outputImage = new Coordinates();
-    System.out.println(inputCoords.toGrid());
     for(int it = 0; it<times; it++){
       NumGrid n = inputCoords.toGrid();
-      for(int i = -1; i<=n.sizeX()+1; i++){
-        for(int j = -1; j<=n.sizeY()+1; j++){
-          Point thisPoint = new Point(j, i);
+      for(int i = -1; i<=n.sizeX(); i++){
+        for(int j = -1; j<=n.sizeY(); j++){
+          Point thisPoint = new Point(i, j);
           String bin = Stream.of(NORTHWEST, NORTH, NORTHEAST, WEST, CENTER, EAST, SOUTHWEST, SOUTH, SOUTHEAST)
-              .map(dir -> n.get(dir.move(thisPoint)) == -1 ? "0" : Long.toString(n.get(dir.move(thisPoint))))
+              .map(dir -> n.at(dir.move(thisPoint)) == -1 ? "0" : Long.toString(n.at(dir.move(thisPoint))))
               .collect(Collectors.joining());
-          System.out.println(bin);
           if(replace[binToDec(bin)] == '#'){
             outputImage.add(thisPoint);
           }
@@ -63,7 +60,6 @@ public class Day20 extends Day2021 {
       }
       inputCoords = outputImage;
       outputImage = new Coordinates();
-      System.out.println(inputCoords.toGrid());
     }
     return inputCoords.size();
   }
