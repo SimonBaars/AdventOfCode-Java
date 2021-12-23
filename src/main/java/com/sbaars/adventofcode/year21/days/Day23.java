b5683e254d0ac557d760bb9386d0c26eefe97e0e
Simon Baars <simon.mailadres@gmail.com>
2021-12-23 19:26:34 +0100

    Add part 2

--- src/main/java/com/sbaars/adventofcode/year21/days/Day23.java
+++ src/main/java/com/sbaars/adventofcode/year21/days/Day23.java
@@ -4,11 +4,13 @@ import static com.google.common.base.Preconditions.checkState;
 
 import com.google.common.base.MoreObjects;
 import com.sbaars.adventofcode.year21.Day2021;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Optional;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -25,39 +27,54 @@ public class Day23 extends Day2021 {
   }
   
   public void verify(){
-    List<State> l = new ArrayList<>(List.of(new State(new int[][]{{0,0},{1,1},{2,2},{3,3}}, new int[]{-1,-1,-1,-1,-1,-1,-1}, 0L)));
-    checkState(l.get(0).win());
-    simulateMoves(l);
+    Queue<State> l = getQueue(new State(new int[][]{{0,0},{1,1},{2,2},{3,3}}, new int[]{-1,-1,-1,-1,-1,-1,-1}));
+    checkState(l.poll().win());
+    simulateMoves(l, new HashSet<>());
     checkState(l.isEmpty());
-    l = new ArrayList<>(List.of(new State(new int[][]{{0,0},{1,1},{2,2},{-1,3}}, new int[]{-1,-1,-1,-1,-1,-1,3}, 0L)));
-    simulateMoves(l);
+    l = getQueue(new State(new int[][]{{0,0},{1,1},{2,2},{-1,3}}, new int[]{-1,-1,-1,-1,-1,-1,3}));
+    simulateMoves(l, new HashSet<>());
     checkState(l.size() == 1);
-    checkState(l.get(0).win());
-    l = new ArrayList<>(List.of(new State(new int[][]{{0,0},{2,1},{1,2},{3,3}}, new int[]{-1,-1,-1,-1,-1,-1,-1}, 0L)));
-    Optional<Long> out = Optional.empty();
-    while(!l.isEmpty()) out = simulateMoves(l);
-    checkState(out.isPresent());
+    checkState(l.poll().win());
+    l = getQueue(new State(new int[][]{{0,0},{2,1},{1,2},{3,3}}, new int[]{-1,-1,-1,-1,-1,-1,-1}));
+    checkState(getMinimum(l, new HashSet<>()) == (100L*4)+(10L*6));
+    l = getQueue(new State(new int[][]{{0,0},{2,2},{1,1},{3,3}}, new int[]{-1,-1,-1,-1,-1,-1,-1}));
+    checkState(getMinimum(l, new HashSet<>()) == (100L*10)+(10L*14));
   }
 
   @Override
   public Object part1() {
-    var rooms = new int[][]{{2, 1}, {3, 0}, {0, 3}, {1, 2}};
-    int[] waiting = new int[7];
-    Arrays.fill(waiting, -1);
-    List<State> states = new ArrayList<>(List.of(new State(rooms, waiting, 0)));
-    long cur = Long.MAX_VALUE;
-    while(!states.isEmpty()){
-      Optional<Long> sim = simulateMoves(states);
-      if(sim.isPresent() && sim.get()<cur) {
-        cur = sim.get();
-        System.out.println(cur);
-      }
-    }
-    return cur;
+    return constructField(new int[][]{{2, 1}, {3, 0}, {0, 3}, {1, 2}});
   }
 
-  private Optional<Long> simulateMoves(List<State> states) {
-    State s = states.remove(0);
+  private long constructField(int[][] rooms) {
+    int[] waiting = new int[7];
+    Arrays.fill(waiting, -1);
+    Set<State> stateSet = new HashSet<>();
+    Queue<State> states = getQueue(new State(rooms, waiting, 0, 0));
+    return getMinimum(states, stateSet);
+  }
+
+  private PriorityQueue<State> getQueue(State s) {
+    var queue = new PriorityQueue<>(1000, Comparator.comparing(State::energySpent));
+    queue.add(s);
+    return queue;
+  }
+
+  private long getMinimum(Queue<State> states, Set<State> stateSet) {
+    while(true){
+      Optional<Long> sim = simulateMoves(states, stateSet);
+      if(sim.isPresent()) {
+        return sim.get();
+      }
+//      if(states.size() % 10000 == 5) System.out.println(states.size()+", "+ states.peek().moves);
+    }
+  }
+
+  private Optional<Long> simulateMoves(Queue<State> states, Set<State> stateSet) {
+    State s = states.poll();
+    if(stateSet.contains(s)) return Optional.empty();
+    stateSet.add(s);
+    if(s.energySpent > 15000) return Optional.empty();
     if(s.win()){
       return Optional.of(s.energySpent);
     }
@@ -78,11 +95,15 @@ public class Day23 extends Day2021 {
     return Optional.empty();
   }
 
-  public record State(int[][] rooms, int[] waiting, long energySpent){
+  public record State(int[][] rooms, int[] waiting, long energySpent, long moves){
+    public State(int[][] rooms, int[] waiting){
+      this(rooms, waiting, 0L, 0L);
+    }
+
     public State copy(long newEnergy){
       int[][] room = Arrays.stream(rooms).map(int[]::clone).toArray(int[][]::new);;
       int[] wait = Arrays.copyOf(waiting, waiting.length);
-      return new State(room, wait, energySpent + newEnergy);
+      return new State(room, wait, energySpent + newEnergy, moves + 1);
     }
 
     public State moveIntoWaiting(int r, int w){
@@ -237,10 +258,23 @@ public class Day23 extends Day2021 {
           .add("energySpent", energySpent)
           .toString();
     }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      State state = (State) o;
+      return Arrays.deepEquals(rooms, state.rooms) && Arrays.equals(waiting, state.waiting);
+    }
+
+    @Override
+    public int hashCode() {
+      return Arrays.deepHashCode(rooms) + Arrays.hashCode(waiting);
+    }
   }
 
   @Override
   public Object part2() {
-    return "";
+    return constructField(new int[][]{{2, 3, 3, 1}, {3, 2, 1, 0}, {0, 1, 0, 3}, {1, 0, 2, 2}});
   }
 }
