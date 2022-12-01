c16030fb3b689399e21837a49b664b97924242d8
SimonBaars <simon.mailadres@gmail.com>
2022-12-01 07:22:42 +0100

    2015 day 2

--- src/main/java/com/sbaars/adventofcode/year15/days/Day2.java
+++ src/main/java/com/sbaars/adventofcode/year15/days/Day2.java
@@ -1,23 +1,74 @@
 package com.sbaars.adventofcode.year15.days;
 
+import com.sbaars.adventofcode.common.Day;
 import com.sbaars.adventofcode.year15.Day2015;
+
+import java.util.function.ToLongFunction;
+
+import static com.sbaars.adventofcode.common.ReadsFormattedString.readString;
 
 public class Day2 extends Day2015 {
   public Day2() {
     super(2);
   }
 
+  public record Dimension (long l, long w, long h) {
+    private long[] smallestSides() {
+      if(l>=w && l>=h) return new long[]{w, h};
+      if(w>=l && w>=h) return new long[]{l, h};
+      return new long[]{l, w};
+    }
+
+    private long smallestArea() {
+      long[] s = smallestSides();
+      return s[0] * s[1];
+    }
+
+    private long area() {
+      return 2*l*w + 2*w*h + 2*h*l;
+    }
+
+    private long wrappingPaper() {
+      return smallestArea() + area();
+    }
+
+    private long sideDistance() {
+      long[] s = smallestSides();
+      return s[0]*2 + s[1]*2;
+    }
+
+    private long volume() {
+      return l*w*h;
+    }
+
+    private long ribbon() {
+      return sideDistance() + volume();
+    }
+  }
+
   public static void main(String[] args) {
-    new Day2().printParts();
+    Day d = new Day2();
+    d.downloadIfNotDownloaded();
+    d.printParts();
+//    d.submitPart1();
+//    d.submitPart2();
   }
 
   @Override
   public Object part1() {
-    return "";
+    return getResult(Dimension::wrappingPaper);
   }
 
   @Override
   public Object part2() {
-    return "";
+    return getResult(Dimension::ribbon);
+  }
+
+  private long getResult(ToLongFunction<Dimension> func) {
+    return dayStream()
+            .map(String::trim)
+            .map(s -> readString(s, "%nx%nx%n", Dimension.class))
+            .mapToLong(func)
+            .sum();
   }
 }
--- src/main/java/com/sbaars/adventofcode/year22/days/Day2.java
+++ src/main/java/com/sbaars/adventofcode/year22/days/Day2.java
@@ -1,5 +1,6 @@
 package com.sbaars.adventofcode.year22.days;
 
+import com.sbaars.adventofcode.common.Day;
 import com.sbaars.adventofcode.year22.Day2022;
 
 public class Day2 extends Day2022 {
@@ -8,7 +9,11 @@ public class Day2 extends Day2022 {
   }
 
   public static void main(String[] args) {
-    new Day2().printParts();
+    Day d = new Day2();
+    d.downloadIfNotDownloaded();
+    d.printParts();
+//    d.submitPart1();
+//    d.submitPart2();
   }
 
   @Override
