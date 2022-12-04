30ed1863b24387e47d5ad8e4025728750b4f818d
SimonBaars <simon.mailadres@gmail.com>
2022-12-04 06:20:01 +0100

    Day 4 refactor

--- src/main/java/com/sbaars/adventofcode/year22/days/Day4.java
+++ src/main/java/com/sbaars/adventofcode/year22/days/Day4.java
@@ -1,9 +1,9 @@
 package com.sbaars.adventofcode.year22.days;
 
-import com.sbaars.adventofcode.common.Day;
 import com.sbaars.adventofcode.year22.Day2022;
 
 import java.io.IOException;
+import java.util.stream.Stream;
 
 import static com.sbaars.adventofcode.common.ReadsFormattedString.readString;
 
@@ -14,32 +14,30 @@ public class Day4 extends Day2022 {
   }
 
   public static void main(String[] args) throws IOException {
-    Day d = new Day4();
-    d.downloadIfNotDownloaded();
-//    d.downloadExample();
-    d.printParts();
-    System.in.read();
-//    d.submitPart1();
-    d.submitPart2();
+    new Day4().printParts();
   }
 
-  public record Assignment(long aStart, long aEnd, long bStart, long bEnd){
-    public boolean contained(){
-      return (aStart>=bStart && aEnd<=bEnd) || (bStart>=aStart && bEnd<=aEnd);
+    public record Assignment(long aStart, long aEnd, long bStart, long bEnd){
+      public boolean contained(){
+        return (aStart>=bStart && aEnd<=bEnd) || (bStart>=aStart && bEnd<=aEnd);
+      }
+
+      public boolean overlap(){
+        return aStart <= bEnd && aEnd >= bStart;
+      }
     }
 
-    public boolean overlap(){
-      return aStart <= bEnd && aEnd >= bStart;
+    @Override
+    public Object part1() {
+      return input().filter(Assignment::contained).count();
     }
-  }
 
-  @Override
-  public Object part1() {
-    return dayStream().map(String::trim).map(s -> readString(s, "%n-%n,%n-%n", Assignment.class)).filter(Assignment::contained).count();
-  }
+    private Stream<Assignment> input() {
+      return dayStream().map(String::trim).map(s -> readString(s, "%n-%n,%n-%n", Assignment.class));
+    }
 
-  @Override
-  public Object part2() {
-    return dayStream().map(String::trim).map(s -> readString(s, "%n-%n,%n-%n", Assignment.class)).filter(Assignment::overlap).count();
-  }
+    @Override
+    public Object part2() {
+      return input().filter(Assignment::overlap).count();
+    }
 }
