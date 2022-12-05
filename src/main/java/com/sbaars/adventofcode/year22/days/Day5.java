5bc13e29480c9722b8e59a49b4bf4b340fb6e61d
SimonBaars <simon.mailadres@gmail.com>
2022-12-05 06:44:22 +0100

    Day 5 2022 More refactoring

--- src/main/java/com/sbaars/adventofcode/year22/days/Day5.java
+++ src/main/java/com/sbaars/adventofcode/year22/days/Day5.java
@@ -31,54 +31,46 @@ public class Day5 extends Day2022 {
   @Override
   public Object part1() {
     List<Deque<Integer>> stacks = input();
-    List<Move> moves = dayStream().map(String::trim).map(s -> readString(s, "move %n from %n to %n", Move.class)).toList();
+    List<Move> moves = getMoves();
     for(Move m : moves) {
       for(int i = 0; i<m.which; i++) {
         int top = stacks.get(toIntExact(m.from-1)).removeLast();
         stacks.get(toIntExact(m.to-1)).addLast(top);
       }
     }
-
     return stacks.stream().map(Deque::peekLast).map(e -> Character.toString((char)(int)e)).collect(Collectors.joining());
   }
-
-  private String toASCII(int value) {
-    int length = 4;
-    StringBuilder builder = new StringBuilder(length);
-    for (int i = length - 1; i >= 0; i--) {
-      builder.append((char) ((value >> (8 * i)) & 0xFF));
-    }
-    return builder.toString();
-  }
-
 
   @Override
   public Object part2() {
     List<Deque<Integer>> stacks = input();
-    List<Move> moves = dayStream().map(String::trim).map(s -> readString(s, "move %n from %n to %n", Move.class)).toList();
+    List<Move> moves = getMoves();
     for(Move m : moves) {
       List<Integer> toBeMoved = new ArrayList<>();
       for(int i = 0; i<m.which; i++) toBeMoved.add(0, stacks.get(toIntExact(m.from-1)).removeLast());
       toBeMoved.forEach(i -> stacks.get(toIntExact(m.to-1)).addLast(i));
     }
-
     return stacks.stream().map(Deque::peekLast).map(e -> Character.toString((char)(int)e)).collect(Collectors.joining());
+  }
+
+  private List<Move> getMoves() {
+    return dayStream().map(String::trim).map(s -> readString(s, "move %n from %n to %n", Move.class)).toList();
   }
 
   private static List<Deque<Integer>> input() {
     List<Deque<Integer>> stacks = new ArrayList<>();
     for(int i = 1; i<=9; i++){
       Deque<Integer> s = new ArrayDeque<>();
-      switch(i){
-        case 1: "NSDCVQT".chars().forEach(s::add); break;
-        case 2: "MFV".chars().forEach(s::add); break;
-        case 3: "FQWDPNHM".chars().forEach(s::add); break;
-        case 4: "DQRTF".chars().forEach(s::add); break;
-        case 5: "RFMNQHVB".chars().forEach(s::add); break;
-        case 6: "CFGNPWQ".chars().forEach(s::add); break;
-        case 7: "WFRLCT".chars().forEach(s::add); break;
-        case 8: "TZNS".chars().forEach(s::add); break;
-        case 9: "MSDJRQHN".chars().forEach(s::add); break;
+      switch (i) {
+        case 1 -> "NSDCVQT".chars().forEach(s::add);
+        case 2 -> "MFV".chars().forEach(s::add);
+        case 3 -> "FQWDPNHM".chars().forEach(s::add);
+        case 4 -> "DQRTF".chars().forEach(s::add);
+        case 5 -> "RFMNQHVB".chars().forEach(s::add);
+        case 6 -> "CFGNPWQ".chars().forEach(s::add);
+        case 7 -> "WFRLCT".chars().forEach(s::add);
+        case 8 -> "TZNS".chars().forEach(s::add);
+        case 9 -> "MSDJRQHN".chars().forEach(s::add);
       }
       stacks.add(s);
     }
