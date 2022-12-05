1e2384a06a1b4c75193794e3bdb23a41307facb4
SimonBaars <simon.mailadres@gmail.com>
2022-12-05 09:31:32 +0100

    Properly use stacks

--- src/main/java/com/sbaars/adventofcode/year22/days/Day5.java
+++ src/main/java/com/sbaars/adventofcode/year22/days/Day5.java
@@ -1,10 +1,12 @@
 package com.sbaars.adventofcode.year22.days;
 
-import com.sbaars.adventofcode.common.Day;
 import com.sbaars.adventofcode.year22.Day2022;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.sbaars.adventofcode.common.ReadsFormattedString.readString;
@@ -17,13 +19,7 @@ public class Day5 extends Day2022 {
   }
 
   public static void main(String[] args) throws IOException {
-    Day d = new Day5();
-    d.downloadIfNotDownloaded();
-//    d.downloadExample();
-    d.printParts();
-    System.in.read();
-//    d.submitPart1();
-    d.submitPart2();
+    new Day5().printParts();
   }
 
   public record Move(long which, long from, long to) {}
@@ -34,11 +30,11 @@ public class Day5 extends Day2022 {
     List<Move> moves = getMoves();
     for(Move m : moves) {
       for(int i = 0; i<m.which; i++) {
-        int top = stacks.get(toIntExact(m.from-1)).removeLast();
-        stacks.get(toIntExact(m.to-1)).addLast(top);
+        int top = stacks.get(toIntExact(m.from-1)).pop();
+        stacks.get(toIntExact(m.to-1)).push(top);
       }
     }
-    return stacks.stream().map(Deque::peekLast).map(e -> Character.toString((char)(int)e)).collect(Collectors.joining());
+    return stacks.stream().map(Deque::peek).map(e -> Character.toString((char)(int)e)).collect(Collectors.joining());
   }
 
   @Override
@@ -47,10 +43,10 @@ public class Day5 extends Day2022 {
     List<Move> moves = getMoves();
     for(Move m : moves) {
       List<Integer> toBeMoved = new ArrayList<>();
-      for(int i = 0; i<m.which; i++) toBeMoved.add(0, stacks.get(toIntExact(m.from-1)).removeLast());
-      toBeMoved.forEach(i -> stacks.get(toIntExact(m.to-1)).addLast(i));
+      for(int i = 0; i<m.which; i++) toBeMoved.add(0, stacks.get(toIntExact(m.from-1)).pop());
+      toBeMoved.forEach(i -> stacks.get(toIntExact(m.to-1)).push(i));
     }
-    return stacks.stream().map(Deque::peekLast).map(e -> Character.toString((char)(int)e)).collect(Collectors.joining());
+    return stacks.stream().map(Deque::peek).map(e -> Character.toString((char)(int)e)).collect(Collectors.joining());
   }
 
   private List<Move> getMoves() {
@@ -62,15 +58,15 @@ public class Day5 extends Day2022 {
     for(int i = 1; i<=9; i++){
       Deque<Integer> s = new ArrayDeque<>();
       switch (i) {
-        case 1 -> "NSDCVQT".chars().forEach(s::add);
-        case 2 -> "MFV".chars().forEach(s::add);
-        case 3 -> "FQWDPNHM".chars().forEach(s::add);
-        case 4 -> "DQRTF".chars().forEach(s::add);
-        case 5 -> "RFMNQHVB".chars().forEach(s::add);
-        case 6 -> "CFGNPWQ".chars().forEach(s::add);
-        case 7 -> "WFRLCT".chars().forEach(s::add);
-        case 8 -> "TZNS".chars().forEach(s::add);
-        case 9 -> "MSDJRQHN".chars().forEach(s::add);
+        case 1 -> "NSDCVQT".chars().forEach(s::push);
+        case 2 -> "MFV".chars().forEach(s::push);
+        case 3 -> "FQWDPNHM".chars().forEach(s::push);
+        case 4 -> "DQRTF".chars().forEach(s::push);
+        case 5 -> "RFMNQHVB".chars().forEach(s::push);
+        case 6 -> "CFGNPWQ".chars().forEach(s::push);
+        case 7 -> "WFRLCT".chars().forEach(s::push);
+        case 8 -> "TZNS".chars().forEach(s::push);
+        case 9 -> "MSDJRQHN".chars().forEach(s::push);
       }
       stacks.add(s);
     }
