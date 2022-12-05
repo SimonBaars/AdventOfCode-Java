9c40b8575e866fb8eae6c30c951c6f42bc63770f
SimonBaars <simon.mailadres@gmail.com>
2022-12-05 06:34:58 +0100

    Day 5 2022 tiny refactor

--- src/main/java/com/sbaars/adventofcode/year22/days/Day5.java
+++ src/main/java/com/sbaars/adventofcode/year22/days/Day5.java
@@ -30,29 +30,12 @@ public class Day5 extends Day2022 {
 
   @Override
   public Object part1() {
-    List<Deque<Integer>> stacks = new ArrayList<>();
-    for(int i = 1; i<=9; i++){
-      Deque<Integer> s = new ArrayDeque<>();
-      switch(i){
-        case 1: "NSDCVQT".chars().forEach(s::add); break;
-        case 2: "MFV".chars().forEach(s::add); break;
-        case 3: "FQWDPNHM".chars().forEach(s::add); break;
-        case 4: "DQRTF".chars().forEach(s::add); break;
-        case 5: "RFMNQHVB".chars().forEach(s::add); break;
-        case 6: "CFGNPWQ".chars().forEach(s::add); break;
-        case 7: "WFRLCT".chars().forEach(s::add); break;
-        case 8: "TZNS".chars().forEach(s::add); break;
-        case 9: "MSDJRQHN".chars().forEach(s::add); break;
-      }
-      stacks.add(s);
-    }
-
+    List<Deque<Integer>> stacks = input();
     List<Move> moves = dayStream().map(String::trim).map(s -> readString(s, "move %n from %n to %n", Move.class)).toList();
     for(Move m : moves) {
       for(int i = 0; i<m.which; i++) {
         int top = stacks.get(toIntExact(m.from-1)).removeLast();
         stacks.get(toIntExact(m.to-1)).addLast(top);
-        System.out.println(((char)top)+" moved");
       }
     }
 
@@ -71,6 +54,18 @@ public class Day5 extends Day2022 {
 
   @Override
   public Object part2() {
+    List<Deque<Integer>> stacks = input();
+    List<Move> moves = dayStream().map(String::trim).map(s -> readString(s, "move %n from %n to %n", Move.class)).toList();
+    for(Move m : moves) {
+      List<Integer> toBeMoved = new ArrayList<>();
+      for(int i = 0; i<m.which; i++) toBeMoved.add(0, stacks.get(toIntExact(m.from-1)).removeLast());
+      toBeMoved.forEach(i -> stacks.get(toIntExact(m.to-1)).addLast(i));
+    }
+
+    return stacks.stream().map(Deque::peekLast).map(e -> Character.toString((char)(int)e)).collect(Collectors.joining());
+  }
+
+  private static List<Deque<Integer>> input() {
     List<Deque<Integer>> stacks = new ArrayList<>();
     for(int i = 1; i<=9; i++){
       Deque<Integer> s = new ArrayDeque<>();
@@ -87,14 +82,6 @@ public class Day5 extends Day2022 {
       }
       stacks.add(s);
     }
-
-    List<Move> moves = dayStream().map(String::trim).map(s -> readString(s, "move %n from %n to %n", Move.class)).toList();
-    for(Move m : moves) {
-      List<Integer> toBeMoved = new ArrayList<>();
-      for(int i = 0; i<m.which; i++) toBeMoved.add(0, stacks.get(toIntExact(m.from-1)).removeLast());
-      toBeMoved.forEach(i -> stacks.get(toIntExact(m.to-1)).addLast(i));
-    }
-
-    return stacks.stream().map(Deque::peekLast).map(e -> Character.toString((char)(int)e)).collect(Collectors.joining());
+    return stacks;
   }
 }
