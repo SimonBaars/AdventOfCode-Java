399a7253dbd90738b19190098b4dc562e87a57e3
SimonBaars <simon.mailadres@gmail.com>
2022-12-06 06:14:17 +0100

    Fix indentation

--- src/main/java/com/sbaars/adventofcode/year22/days/Day6.java
+++ src/main/java/com/sbaars/adventofcode/year22/days/Day6.java
@@ -15,22 +15,22 @@ public class Day6 extends Day2022 {
     new Day6().printParts();
   }
 
-    @Override
-    public Object part1() {
-      return calculateAnswer(4);
-    }
+  @Override
+  public Object part1() {
+    return calculateAnswer(4);
+  }
 
-    private int calculateAnswer(int size) {
-      String s = day();
-      for(int i = 0; i<s.length(); i++){
-        Set<Integer> chars = Set.copyOf(s.substring(i, i+size).chars().boxed().toList());
-        if(chars.size() == size) return i+size;
-      }
-      return 0;
+  private int calculateAnswer(int size) {
+    String s = day();
+    for(int i = 0; i<s.length(); i++){
+      Set<Integer> chars = Set.copyOf(s.substring(i, i+size).chars().boxed().toList());
+      if(chars.size() == size) return i+size;
     }
+    return 0;
+  }
 
-    @Override
-    public Object part2() {
-      return calculateAnswer(14);
-    }
+  @Override
+  public Object part2() {
+    return calculateAnswer(14);
+  }
 }
