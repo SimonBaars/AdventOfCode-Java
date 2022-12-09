bf200c79559fc0a88fe54bfcd503c5b6d4b3e7e6
SimonBaars <simon.mailadres@gmail.com>
2022-12-09 07:04:12 +0100

    Cleanup

--- src/main/java/com/sbaars/adventofcode/year22/days/Day9.java
+++ src/main/java/com/sbaars/adventofcode/year22/days/Day9.java
@@ -1,6 +1,5 @@
 package com.sbaars.adventofcode.year22.days;
 
-import com.sbaars.adventofcode.common.Day;
 import com.sbaars.adventofcode.common.Direction;
 import com.sbaars.adventofcode.year22.Day2022;
 
@@ -18,13 +17,7 @@ public class Day9 extends Day2022 {
   }
 
   public static void main(String[] args) {
-    Day d = new Day9();
-    d.downloadIfNotDownloaded();
-    d.downloadExample();
-    d.printParts();
-//    System.in.read();
-//    d.submitPart1();
-//    d.submitPart2();
+    new Day9().printParts();
   }
 
   public record Move(char dir, long n){}
