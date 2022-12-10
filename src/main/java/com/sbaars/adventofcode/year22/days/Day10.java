38afcbb32e8078c5c4bea4c7af5a019f9bd2eaa2
SimonBaars <simon.mailadres@gmail.com>
2022-12-10 06:46:01 +0100

    Day 10 2022 refactor

--- src/main/java/com/sbaars/adventofcode/year22/days/Day10.java
+++ src/main/java/com/sbaars/adventofcode/year22/days/Day10.java
@@ -1,10 +1,12 @@
 package com.sbaars.adventofcode.year22.days;
 
-import com.sbaars.adventofcode.common.Day;
 import com.sbaars.adventofcode.year22.Day2022;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day10 extends Day2022 {
   public Day10() {
@@ -12,68 +14,44 @@ public class Day10 extends Day2022 {
   }
 
   public static void main(String[] args) {
-    Day d = new Day10();
-    d.downloadIfNotDownloaded();
-    d.downloadExample();
-    d.printParts();
-//    System.in.read();
-//    d.submitPart1();
-//    d.submitPart2();
+    new Day10().printParts();
   }
 
   @Override
   public Object part1() {
-    long cycle = 1;
-    long x  = 1;
-    List<Long> output = new ArrayList<>();
-    for(String op : dayStrings()) {
-      if(op.equals("noop")) {
-        cycle++;
-      } else if(op.startsWith("addx")) {
-        long add = Long.parseLong(op.substring(5));
-        cycle++;
-        if((cycle+20) % 40 == 0) {
-          output.add(cycle*x);
-//          System.out.println(cycle+", "+x);
-        }
-        cycle++;
-        x+=add;
-      }
-      if((cycle+20) % 40 == 0) {
-        output.add(cycle*x);
-//        System.out.p  rintln(cycle+", "+x);
-      }
-      if(cycle>220) break;
-    }
-//    System.out.println(cycle+", "+x+", "+ Arrays.toString(output.toArray()));
-    return output.stream().mapToLong(e -> e).sum();
+    return performOp(this::signalStrength).mapToLong(e -> e).sum();
   }
 
   @Override
   public Object part2() {
+    return performOp(this::getPixel).collect(Collectors.joining());
+  }
+
+  private long signalStrength(long cycle, long x) {
+    return (cycle+20) % 40 == 0 ? cycle*x : 0;
+  }
+
+  private String getPixel(long cycle, long x) {
+    long i = (cycle -1) % 40;
+    return (i == 0 ? "\n" : "") + (List.of(x -1, x, x +1).contains(i) ? "#" : ".");
+  }
+
+  private<T> Stream<T> performOp(BiFunction<Long, Long, T> func) {
     long cycle = 1;
     long x  = 1;
-//    List<Long> output = new ArrayList<>();
-    String pixels = "";
+    List<T> res = new ArrayList<>();
     for(String op : dayStrings()) {
-      if((cycle-1) % 40 == 0) {
-        pixels+="\n";
-      }
-      pixels += List.of(x-1, x, x+1).contains((cycle-1)%40) ? "#" : ".";
+      res.add(func.apply(cycle, x));
       if(op.equals("noop")) {
         cycle++;
       } else if(op.startsWith("addx")) {
         long add = Long.parseLong(op.substring(5));
         cycle++;
-        if((cycle-1) % 40 == 0) {
-          pixels+="\n";
-        }
-        pixels += List.of(x-1, x, x+1).contains((cycle-1)%40) ? "#" : ".";
+        res.add(func.apply(cycle, x));
         cycle++;
         x+=add;
       }
     }
-//    System.out.println(cycle+", "+x+", "+ Arrays.toString(output.toArray()));
-    return pixels;
+    return res.stream();
   }
 }
