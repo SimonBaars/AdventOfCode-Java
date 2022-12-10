90d255837291cacd7826afb637b8b1782d65b4c5
SimonBaars <simon.mailadres@gmail.com>
2022-12-10 06:30:11 +0100

    Day 10 2022

--- src/main/java/com/sbaars/adventofcode/year22/days/Day10.java
+++ src/main/java/com/sbaars/adventofcode/year22/days/Day10.java
@@ -3,6 +3,9 @@ package com.sbaars.adventofcode.year22.days;
 import com.sbaars.adventofcode.common.Day;
 import com.sbaars.adventofcode.year22.Day2022;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Day10 extends Day2022 {
   public Day10() {
     super(10);
@@ -20,11 +23,57 @@ public class Day10 extends Day2022 {
 
   @Override
   public Object part1() {
-    return "";
+    long cycle = 1;
+    long x  = 1;
+    List<Long> output = new ArrayList<>();
+    for(String op : dayStrings()) {
+      if(op.equals("noop")) {
+        cycle++;
+      } else if(op.startsWith("addx")) {
+        long add = Long.parseLong(op.substring(5));
+        cycle++;
+        if((cycle+20) % 40 == 0) {
+          output.add(cycle*x);
+//          System.out.println(cycle+", "+x);
+        }
+        cycle++;
+        x+=add;
+      }
+      if((cycle+20) % 40 == 0) {
+        output.add(cycle*x);
+//        System.out.p  rintln(cycle+", "+x);
+      }
+      if(cycle>220) break;
+    }
+//    System.out.println(cycle+", "+x+", "+ Arrays.toString(output.toArray()));
+    return output.stream().mapToLong(e -> e).sum();
   }
 
   @Override
   public Object part2() {
-    return "";
+    long cycle = 1;
+    long x  = 1;
+//    List<Long> output = new ArrayList<>();
+    String pixels = "";
+    for(String op : dayStrings()) {
+      if((cycle-1) % 40 == 0) {
+        pixels+="\n";
+      }
+      pixels += List.of(x-1, x, x+1).contains((cycle-1)%40) ? "#" : ".";
+      if(op.equals("noop")) {
+        cycle++;
+      } else if(op.startsWith("addx")) {
+        long add = Long.parseLong(op.substring(5));
+        cycle++;
+        if((cycle-1) % 40 == 0) {
+          pixels+="\n";
+        }
+        pixels += List.of(x-1, x, x+1).contains((cycle-1)%40) ? "#" : ".";
+        cycle++;
+        x+=add;
+      }
+    }
+//    System.out.println(cycle+", "+x+", "+ Arrays.toString(output.toArray()));
+    return pixels;
   }
 }
