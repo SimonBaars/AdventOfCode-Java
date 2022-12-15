21c07f74b0a8dc667cc9949b9e2b22609dc89fd3
SimonBaars <simon.mailadres@gmail.com>
2022-12-15 20:41:55 +0100

    Prepare Day 16

--- src/main/java/com/sbaars/adventofcode/year22/days/Day16.java
+++ src/main/java/com/sbaars/adventofcode/year22/days/Day16.java
@@ -1,5 +1,6 @@
 package com.sbaars.adventofcode.year22.days;
 
+import com.sbaars.adventofcode.common.Day;
 import com.sbaars.adventofcode.year22.Day2022;
 
 public class Day16 extends Day2022 {
@@ -9,11 +10,18 @@ public class Day16 extends Day2022 {
   }
 
   public static void main(String[] args) {
-    new Day16().printParts();
+    Day d = new Day16();
+    d.downloadIfNotDownloaded();
+    d.downloadExample();
+    d.printParts();
+//    System.in.read();
+//    d.submitPart1();
+//    d.submitPart2();
   }
 
   @Override
   public Object part1() {
+
     return "";
   }
 
