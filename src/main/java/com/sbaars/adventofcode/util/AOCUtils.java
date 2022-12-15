91b9af95dabcf69b7288f7e1b1bbc342066fd774
SimonBaars <simon.mailadres@gmail.com>
2022-12-15 08:33:39 +0100

    Very slow solution

--- src/main/java/com/sbaars/adventofcode/util/AOCUtils.java
+++ src/main/java/com/sbaars/adventofcode/util/AOCUtils.java
@@ -3,8 +3,9 @@ package com.sbaars.adventofcode.util;
 import com.sbaars.adventofcode.common.Pair;
 
 import java.util.List;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
+import static java.util.stream.IntStream.range;
 
 public class AOCUtils {
     public static void verify(boolean b) {
@@ -18,10 +19,14 @@ public class AOCUtils {
     }
 
     public static<A> Stream<Pair<A, A>> connectedPairs(List<A> l) {
-        return IntStream.range(1, l.size()).mapToObj(i -> Pair.of(l.get(i-1), l.get(i)));
+        return range(1, l.size()).mapToObj(i -> Pair.of(l.get(i-1), l.get(i)));
     }
 
     public static<A> Stream<Pair<A, A>> pairs(List<A> l) {
-        return IntStream.range(1, l.size()/2).map(i -> i + ((i-1)*2)).mapToObj(i -> Pair.of(l.get(i-1), l.get(i)));
+        return range(1, l.size()/2).map(i -> i + ((i-1)*2)).mapToObj(i -> Pair.of(l.get(i-1), l.get(i)));
+    }
+
+    public static<A> Stream<Pair<A, A>> allPairs(List<A> l) {
+        return range(0, l.size()).boxed().flatMap(i -> range(i+1, l.size()).mapToObj(j -> new Pair<A, A>(l.get(i), l.get(j))));
     }
 }
