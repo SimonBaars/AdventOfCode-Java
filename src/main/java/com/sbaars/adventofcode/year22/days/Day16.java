b132fbb16df491fd02006bea0724c3f6174b5b95
SimonBaars <simon.mailadres@gmail.com>
2022-12-16 07:56:58 +0100

    Day 16 2022 part 2 first draft

--- src/main/java/com/sbaars/adventofcode/year22/days/Day16.java
+++ src/main/java/com/sbaars/adventofcode/year22/days/Day16.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static com.sbaars.adventofcode.common.ReadsFormattedString.readString;
+import static com.sbaars.adventofcode.util.AOCUtils.allPairs;
 
 public class Day16 extends Day2022 {
 
@@ -27,6 +28,7 @@ public class Day16 extends Day2022 {
 
   public record Valve(String name, long flow, String others) {}
   public record State(Map<String, Long> open, int index, long totalFlow) {}
+  public record State2(Map<String, Long> open, int myIndex, int elephantIndex, long totalFlow) {}
 
   @Override
   public Object part1() {
@@ -59,6 +61,51 @@ public class Day16 extends Day2022 {
 
   @Override
   public Object part2() {
-    return "";
+    List<Valve> in = dayStream().map(s -> {
+      try {
+        return readString(s, "Valve %s has flow rate=%n; tunnels lead to valves %s", Valve.class);
+      } catch (IllegalStateException e) {
+        return readString(s, "Valve %s has flow rate=%n; tunnel leads to valve %s", Valve.class);
+      }
+    }).toList();
+    Set<String> openable = in.stream().filter(s -> s.flow > 0).map(Valve::name).collect(Collectors.toSet());
+    Map<String, Integer> indices = IntStream.range(0, in.size()).boxed().collect(Collectors.toMap(i -> in.get(i).name, i -> i));
+    Set<State2> states = new HashSet<>();
+    states.add(new State2(new HashMap<>(), 0, 0, 0));
+    for(int minutes = 0; minutes<30; minutes++) {
+      Set<State2> newStates = new HashSet<>();
+      for(State2 s : states) {
+
+        Valve myValve = in.get(s.myIndex);
+        Valve eleValve = in.get(s.elephantIndex);
+        long flow = s.open.values().stream().mapToLong(e -> e).sum() + s.totalFlow;
+        if(s.open.keySet().equals(openable)) { // All valves are open, time to chill
+          newStates.add(new State2(s.open, s.myIndex, s.elephantIndex, flow));
+        }
+        boolean couldOpen = false;
+        if(myValve.flow > 0 && !s.open.containsKey(myValve.name)) {
+          Map<String, Long> newOpen = new HashMap<>(s.open);
+          newOpen.put(myValve.name, myValve.flow);
+          Arrays.stream(eleValve.others.split(", ")).forEach(name -> newStates.add(new State2(newOpen, s.myIndex, indices.get(name), flow)));
+          couldOpen = true;
+        }
+        if(eleValve.flow > 0 && !s.open.containsKey(eleValve.name)) {
+          Map<String, Long> newOpen = new HashMap<>(s.open);
+          newOpen.put(eleValve.name, eleValve.flow);
+          Arrays.stream(myValve.others.split(", ")).forEach(name -> newStates.add(new State2(newOpen, indices.get(name), s.elephantIndex, flow)));
+          couldOpen = true;
+        }
+        if(myValve.flow > 0 && !s.open.containsKey(myValve.name) && eleValve.flow > 0 && !s.open.containsKey(eleValve.name)) {
+          Map<String, Long> newOpen = new HashMap<>(s.open);
+          newOpen.put(myValve.name, myValve.flow);
+          newOpen.put(eleValve.name, eleValve.flow);
+          newStates.add(new State2(newOpen, s.myIndex, s.elephantIndex, flow));
+          couldOpen = true;
+        }
+        if(!couldOpen) allPairs(List.of(myValve.others.split(", ")), List.of(eleValve.others.split(", "))).forEach(p -> newStates.add(new State2(s.open, indices.get(p.a()), indices.get(p.b()), flow)));
+      }
+      states = newStates;
+    }
+    return states.stream().mapToLong(State2::totalFlow).max().getAsLong();
   }
 }
