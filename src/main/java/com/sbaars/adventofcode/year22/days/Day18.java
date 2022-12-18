97919d2bc0a596553e5752a52c9b85fd068c0156
SimonBaars <simon.mailadres@gmail.com>
2022-12-18 07:32:26 +0100

    Use LongCountMap

--- src/main/java/com/sbaars/adventofcode/year22/days/Day18.java
+++ src/main/java/com/sbaars/adventofcode/year22/days/Day18.java
@@ -2,12 +2,15 @@ package com.sbaars.adventofcode.year22.days;
 
 import com.sbaars.adventofcode.common.HexDirection;
 import com.sbaars.adventofcode.common.location.Loc3D;
+import com.sbaars.adventofcode.year19.util.LongCountMap;
 import com.sbaars.adventofcode.year22.Day2022;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 import static com.sbaars.adventofcode.common.ReadsFormattedString.readString;
+import static com.sbaars.adventofcode.year19.util.LongCountMap.toCountMap;
 
 public class Day18 extends Day2022 {
   public Day18() {
@@ -15,13 +18,7 @@ public class Day18 extends Day2022 {
   }
 
   public static void main(String[] args) {
-    Day18 d = new Day18();
-    d.downloadIfNotDownloaded();
-    d.downloadExample();
-    d.printParts();
-//    System.in.read();
-//    d.submitPart1();
-//    d.submitPart2();
+    new Day18().printParts();
   }
 
   @Override
@@ -40,23 +37,26 @@ public class Day18 extends Day2022 {
     List<Loc3D> connecting = locs.stream().flatMap(l -> Arrays.stream(HexDirection.values()).map(d -> d.move(l, 1))).collect(Collectors.toCollection(ArrayList::new));
     List<Loc3D> exterior = connecting.stream().filter(locs::contains).toList();
     connecting.removeAll(exterior);
-    List<Set<Loc3D>> pockets = connecting.stream().map(l -> new HashSet<>(Set.of(l))).collect(Collectors.toCollection(ArrayList::new));
-    int trapped = 0;
-    for(int i = 0; i<1000; i++) {
-      for(int j = 0; j<pockets.size(); j++) {
-        Set<Loc3D> pocket = pockets.get(j);
-        Set<Loc3D> s = new HashSet<>(pocket);
-        pocket.addAll(pocket.stream().flatMap(l -> Arrays.stream(HexDirection.values()).map(d -> d.move(l, 1))).filter(l -> !locs.contains(l)).collect(Collectors.toSet()));
-        if(s.size() == pocket.size()) {
-          trapped++;
-          pockets.remove(j);
-          j--;
-        } else if(pocket.size()>2000) {
-          pockets.remove(j);
-          j--;
+    LongCountMap<Set<Loc3D>> pockets = connecting.stream().map(l -> new HashSet<>(Set.of(l))).collect(toCountMap());
+    var trapped = new AtomicLong();
+    for(int i = 0; i<2000; i++) {
+      LongCountMap<Set<Loc3D>> newPockets = new LongCountMap<>();
+      pockets.forEach((pocket, n) -> {
+        Set<Loc3D> spread = pocket.stream().flatMap(l -> Arrays.stream(HexDirection.values()).map(d -> d.move(l, 1))).filter(l -> !locs.contains(l)).collect(Collectors.toSet());
+        if(spread.size() == pocket.size()) {
+          trapped.addAndGet(n);
+          System.out.println("Pocket of size "+n+" found");
+        } else if(spread.size()<=connecting.size()) {
+          var matching = newPockets.keySet().stream().filter(e -> e.stream().anyMatch(spread::contains)).findAny();
+          matching.ifPresentOrElse(c -> {
+            spread.addAll(c);
+            newPockets.increment(spread, n + newPockets.remove(c));
+          }, () -> newPockets.increment(spread, n));
         }
-      }
+      });
+      pockets = newPockets;
+      System.out.println(i);
     }
-    return (locs.size()*6L) - exterior.size() - trapped;
+    return (locs.size()*6L) - exterior.size() - trapped.get();
   }
 }
