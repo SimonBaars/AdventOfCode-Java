abe36c38b8c95c1c231ab30b81550cabbb606cc6
SimonBaars <simon.mailadres@gmail.com>
2022-12-18 07:30:46 +0100

    toCountMap

--- src/main/java/com/sbaars/adventofcode/year19/util/LongCountMap.java
+++ src/main/java/com/sbaars/adventofcode/year19/util/LongCountMap.java
@@ -2,6 +2,11 @@ package com.sbaars.adventofcode.year19.util;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
@@ -51,10 +56,11 @@ public class LongCountMap<K> extends HashMap<K, Long> {
     amountPerCloneClassSize.entrySet().stream().forEach(e -> this.increment(e.getKey(), e.getValue()));
   }
 
-  public void incrementAll(LongCountMap<K> input) {
+  public LongCountMap<K> incrementAll(LongCountMap<K> input) {
     for (Entry<K, Long> i : input.entrySet()) {
       increment(i.getKey(), i.getValue());
     }
+    return this;
   }
 
   public long sumValues() {
@@ -65,5 +71,13 @@ public class LongCountMap<K> extends HashMap<K, Long> {
     var lcm = new LongCountMap<Long>();
     frequencies.forEach(lcm::increment);
     return lcm;
+  }
+
+  public static<T> Collector<T, LongCountMap<T>, LongCountMap<T>> toCountMap() {
+    final Supplier<LongCountMap<T>> supplier = LongCountMap::new;
+    final BiConsumer<LongCountMap<T>, T> accumulator = LongCountMap::increment;
+    final BinaryOperator<LongCountMap<T>> combiner = LongCountMap::incrementAll;
+    final Function<LongCountMap<T>, LongCountMap<T>> finisher = LongCountMap::new;
+    return Collector.of(supplier, accumulator, combiner, finisher);
   }
 }
