8febe0fb08e0c95e264ae95653dfacecdbd3a366
SimonBaars <simon.mailadres@gmail.com>
2022-12-19 08:23:14 +0100

    Optimize part 1

--- src/main/java/com/sbaars/adventofcode/common/TopElements.java
+++ src/main/java/com/sbaars/adventofcode/common/TopElements.java
@@ -5,19 +5,20 @@ import java.util.*;
 public class TopElements<E> extends PriorityQueue<E> {
     private final int capacity;
     public TopElements(int capacity, Comparator<? super E> comparator) {
-        super(capacity, comparator);
+        super(capacity, comparator.reversed());
         this.capacity = capacity;
     }
 
     public TopElements(int capacity, Collection<? extends E> c, Comparator<? super E> comparator) {
-        super(capacity, comparator);
-        this.capacity = capacity;
+        this(capacity, comparator);
         addAll(c);
     }
 
     @Override
     public boolean add(E e) {
-        return size() <= capacity && super.add(e);
+        super.add(e);
+        if(size() > capacity) super.poll();
+        return size() <= capacity;
     }
 
     @Override
