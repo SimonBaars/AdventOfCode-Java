68db71f204a30959ad3346db08625f3f175fbd7b
SimonBaars <simon.mailadres@gmail.com>
2022-12-20 20:33:18 +0100

    Refactor Day 20 2022

--- src/main/java/com/sbaars/adventofcode/year22/days/Day20.java
+++ src/main/java/com/sbaars/adventofcode/year22/days/Day20.java
@@ -16,23 +16,18 @@ public class Day20 extends Day2022 {
 
   @Override
   public Object part1() {
-    CircularList in = new CircularList(dayNumberStream().toArray());
-    var nodes = new ArrayList<>(in.valueMap);
-    for(int i = 0; i<nodes.size(); i++) {
-      var toMove = nodes.get(i);
-      var moveTo = toMove.move(toMove.value, nodes.size());
-      in.insertAfter(toMove, moveTo);
-    }
-    in.setCurrent(nodes.stream().filter(n -> n.value == 0).findAny().get());
-    long[] nums = in.next(3000);
-    return nums[1000-1] + nums[2000-1] + nums[3000-1];
+    return solve(1, 1);
   }
 
   @Override
   public Object part2() {
-    CircularList in = new CircularList(dayNumberStream().map(l -> l * 811589153L).toArray());
+    return solve(811589153L, 10);
+  }
+
+  public long solve(long multiplier, int times) {
+    CircularList in = new CircularList(dayNumberStream().map(l -> l * multiplier).toArray());
     var nodes = new ArrayList<>(in.valueMap);
-    for(int j = 0; j<10; j++) {
+    for(int j = 0; j<times; j++) {
       for (int i = 0; i < nodes.size(); i++) {
         var toMove = nodes.get(i);
         var moveTo = toMove.move(toMove.value, nodes.size());
