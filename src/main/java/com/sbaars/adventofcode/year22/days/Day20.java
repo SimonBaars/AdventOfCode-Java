354a57e0fc96c0996e4f55f93f5c52be4e779016
SimonBaars <simon.mailadres@gmail.com>
2022-12-20 07:04:00 +0100

    aaaa

--- src/main/java/com/sbaars/adventofcode/year22/days/Day20.java
+++ src/main/java/com/sbaars/adventofcode/year22/days/Day20.java
@@ -1,19 +1,46 @@
 package com.sbaars.adventofcode.year22.days;
 
-import com.sbaars.adventofcode.template.Day1999;
+import com.sbaars.adventofcode.common.CircularList;
+import com.sbaars.adventofcode.year22.Day2022;
 
-public class Day20 extends Day1999 {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Day20 extends Day2022 {
   public Day20() {
     super(20);
   }
 
   public static void main(String[] args) {
-    new Day20().printParts();
+    Day20 d = new Day20();
+    d.downloadIfNotDownloaded();
+    d.downloadExample();
+    d.printParts(1);
+//    System.in.read();
+//    d.submitPart1();
+//    d.submitPart2();
   }
 
   @Override
   public Object part1() {
-    return "";
+//    System.out.println(dayNumberStream().count());
+//    System.out.println(dayNumberStream().distinct().count());
+    CircularList in = new CircularList(dayNumberStream().mapToInt(Math::toIntExact).toArray());
+    var nodes = new ArrayList<>(in.valueMap);
+    for(int i = 0; i<nodes.size(); i++) {
+      var toMove = nodes.get(i);
+      System.out.println(toMove.value+" moves");
+//      var moveTo = in.valueMap.get(Math.floorMod(in.valueMap.indexOf(toMove) + toMove.value, nodes.size()));
+      var moveTo = toMove.move(toMove.value, nodes.size());
+      in.insertAfter(toMove, moveTo);
+      System.out.println(Arrays.toString(in.next(nodes.size())));
+      System.out.println(Arrays.toString(in.prev(nodes.size())));
+    }
+    while(in.current() != 0) {
+      in.next();
+    }
+    int[] nums = in.next(3000);
+    return nums[1000-1] + nums[2000-1] + nums[3000-1];
   }
 
   @Override
