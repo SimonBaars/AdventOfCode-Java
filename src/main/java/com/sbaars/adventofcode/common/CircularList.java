1034364614487141925107fbb4e26af2fed4c39a
SimonBaars <simon.mailadres@gmail.com>
2022-12-20 07:04:00 +0100

    aaaa

--- src/main/java/com/sbaars/adventofcode/common/CircularList.java
+++ src/main/java/com/sbaars/adventofcode/common/CircularList.java
@@ -1,22 +1,22 @@
 package com.sbaars.adventofcode.common;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A circular linked list of fixed length where all operations are O(1)
  */
-public class CircularLinkedList {
-  public final Map<Integer, Node> valueMap = new HashMap<>();
+public class CircularList {
+  public final List<Node> valueMap = new ArrayList<>();
   private Node current;
-  public CircularLinkedList(int[] elements) {
+  public CircularList(int[] elements) {
     Node prev = null;
     Node first = null;
     for (int element : elements) {
       Node n = new Node();
       n.value = element;
       n.prev = prev;
-      valueMap.put(element, n);
+      valueMap.add(n);
       if (prev != null) prev.next = n;
       else first = n;
       prev = n;
@@ -44,26 +44,24 @@ public class CircularLinkedList {
     return arr;
   }
 
+  public int[] prev(int n) {
+    int[] arr = new int[n];
+    Node node = current;
+    for (int i = 0; i < n; i++) {
+      node = node.prev;
+      arr[i] = node.value;
+    }
+    return arr;
+  }
+
   public Node currentNode() {
     return current;
   }
 
-  public void insertAfter(Node s1, Node s2, int dest) {
-    Node d = valueMap.get(dest);
-    Node oldNext = d.next;
-    s1.prev.next = s2.next;
-    s2.next.prev = s1.prev;
-    d.next = s1;
-    s2.next = oldNext;
-    oldNext.prev = s2;
-    s1.prev = d;
-  }
-
   public void insertAfter(Node s1, Node s2) {
-    if(s1 == s2) return;
     s1.prev.next = s1.next;
     s1.next.prev = s1.prev;
-    s2.next.next.prev = s1;
+    s2.next.prev = s1;
     s1.next = s2.next;
     s2.next = s1;
     s1.prev = s2;
@@ -88,5 +86,14 @@ public class CircularLinkedList {
     public int value;
     public Node prev;
     public Node next;
+
+    public Node move(int value, int size) {
+      Node n = this;
+      int movesNeeded = Math.floorMod(value, size);
+      for(int i = 0; i<movesNeeded; i++) {
+        n = n.next;
+      }
+      return n;
+    }
   }
 }
