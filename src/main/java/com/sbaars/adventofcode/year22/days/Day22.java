56229a2596ccdba4b64371ac6fd889355accab0d
SimonBaars <simon.mailadres@gmail.com>
2022-12-22 08:39:42 +0100

    Refactor solution

--- src/main/java/com/sbaars/adventofcode/year22/days/Day22.java
+++ src/main/java/com/sbaars/adventofcode/year22/days/Day22.java
@@ -1,6 +1,7 @@
 package com.sbaars.adventofcode.year22.days;
 
 import com.sbaars.adventofcode.common.Direction;
+import com.sbaars.adventofcode.common.Pair;
 import com.sbaars.adventofcode.common.grid.CharGrid;
 import com.sbaars.adventofcode.common.grid.InfiniteGrid;
 import com.sbaars.adventofcode.common.location.Loc;
@@ -8,8 +9,9 @@ import com.sbaars.adventofcode.common.location.Range;
 import com.sbaars.adventofcode.year22.Day2022;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.sbaars.adventofcode.common.Direction.*;
 
@@ -19,17 +21,12 @@ public class Day22 extends Day2022 {
   }
 
   public static void main(String[] args) throws IOException {
-    Day22 d = new Day22();
-    d.downloadIfNotDownloaded();
-    d.downloadExample();
-    d.printParts();
-//    System.in.read();
-//    d.submitPart1();
-//    d.submitPart2();
+    new Day22().printParts();
   }
 
+  public record Me (Loc l, Direction d) {}
   public record Location(int c, Direction d) {}
-  public record Instruction(Location go, boolean translate, boolean swap) {}
+  public record Instruction(Location go, boolean translate) {}
   Map<Integer, Range> cubes = Map.of(
           5, new Range(50, 0, 99, 49)/*5*/,
           6, new Range(100, 0, 149, 49)/*6*/,
@@ -39,95 +36,75 @@ public class Day22 extends Day2022 {
           1, new Range(0, 150, 49, 199)/*1*/
   );
   Map<Location, Instruction> todo = Map.of(
-          new Location(6, EAST), new Instruction(new Location(3, EAST), true, false), // A
-          new Location(6, SOUTH), new Instruction(new Location(4, EAST), false, true), // B
-          new Location(6, NORTH), new Instruction(new Location(1, SOUTH), false, false), // C
-          new Location(4, WEST), new Instruction(new Location(2, NORTH), false, true), // D
-          new Location(3, SOUTH), new Instruction(new Location(1, EAST), false, true), // E
-          new Location(5, NORTH), new Instruction(new Location(1, WEST), false, true), // F
-          new Location(5, WEST), new Instruction(new Location(2, WEST), true, false)  // G
-  );
+          new Location(6, EAST), new Instruction(new Location(3, EAST), true), // A
+          new Location(6, SOUTH), new Instruction(new Location(4, EAST), false), // B
+          new Location(6, NORTH), new Instruction(new Location(1, SOUTH), false), // C
+          new Location(4, WEST), new Instruction(new Location(2, NORTH), false), // D
+          new Location(3, SOUTH), new Instruction(new Location(1, EAST), false), // E
+          new Location(5, NORTH), new Instruction(new Location(1, WEST), false), // F
+          new Location(5, WEST), new Instruction(new Location(2, WEST), true)  // G
+  ).entrySet().stream()
+          .flatMap(e -> Stream.of(Pair.of(e.getKey(), e.getValue()), Pair.of(e.getValue().go, new Instruction(e.getKey(), e.getValue().translate))))
+          .collect(Collectors.toMap(Pair::a, Pair::b));
 
   @Override
   public Object part1() {
-    String[] in = day().split("\n\n");
-    CharGrid grid = new CharGrid(in[0]);
-    InfiniteGrid g = new InfiniteGrid(grid.grid);
-    Loc start = g.grid.keySet().stream().filter(l -> grid.get(l) == '.' && l.y == 0).findFirst().get();
-    Direction facing = EAST;
-    String instr = in[1].trim();
-    while(!instr.isEmpty()) {
-      if(instr.charAt(0) == 'L' || instr.charAt(0) == 'R') {
-        facing = facing.turn(instr.charAt(0) == 'R');
-        instr = instr.substring(1);
-      } else {
-        int i;
-        for(i=0; i<instr.length() && Character.isDigit(instr.charAt(i)); i++);
-//        System.out.println(instr);
-        int n = Integer.parseInt(instr.substring(0, i));
-        instr = instr.substring(i);
-        for(i = 0; i<n; i++) {
-          Loc newLoc = facing.move(start);
-          var atPos = g.get(newLoc);
-          if(atPos.isPresent() && atPos.get() == '.') {
-            start = newLoc;
-          } else if(!atPos.isPresent()) {
-            int j;
-            Direction opposite = facing.opposite();
-            newLoc = opposite.move(newLoc);
-            for(j = 0; g.get(newLoc).isPresent(); newLoc = opposite.move(newLoc));
-            newLoc = facing.move(newLoc);
-            if(g.get(newLoc).get() == '.') start = newLoc;
-          }
-        }
-      }
-    }
-    return (1000 * (start.y+1)) + (4 * (start.x + 1)) + facing.ordinal() - 1 + (facing == NORTH ? 4 : 0);
+    return solve(false);
   }
 
   @Override
   public Object part2() {
-    var teleport = new HashMap<>(todo);
-    todo.forEach((l, i) -> teleport.put(i.go, new Instruction(l, i.translate, i.swap)));
+    return solve(true);
+  }
+
+  public long solve (boolean cube) {
     String[] in = day().split("\n\n");
     CharGrid grid = new CharGrid(in[0]);
     InfiniteGrid g = new InfiniteGrid(grid.grid);
-    Loc start = g.grid.keySet().stream().filter(l -> grid.get(l) == '.' && l.y == 0).findFirst().get();
-    Direction facing = EAST;
     String instr = in[1].trim();
+    Me me = new Me(g.grid.keySet().stream().filter(l -> grid.get(l) == '.' && l.y == 0).findFirst().get(), EAST);
     while(!instr.isEmpty()) {
       if(instr.charAt(0) == 'L' || instr.charAt(0) == 'R') {
-        facing = facing.turn(instr.charAt(0) == 'R');
+        me = new Me(me.l, me.d.turn(instr.charAt(0) == 'R'));
         instr = instr.substring(1);
       } else {
         int i;
         for(i=0; i<instr.length() && Character.isDigit(instr.charAt(i)); i++);
-//        System.out.println(instr);
         int n = Integer.parseInt(instr.substring(0, i));
         instr = instr.substring(i);
         for(i = 0; i<n; i++) {
-          Loc newLoc = facing.move(start);
+          Loc newLoc = me.d.move(me.l);
           var atPos = g.get(newLoc);
           if(atPos.isPresent() && atPos.get() == '.') {
-            start = newLoc;
+            me = new Me(newLoc, me.d);
           } else if(!atPos.isPresent()) {
-            Loc finalStart = start;
-            int cube = cubes.entrySet().stream().filter(e -> e.getValue().inRange(finalStart)).mapToInt(Map.Entry::getKey).findFirst().getAsInt();
-            var tele = teleport.get(new Location(cube, facing));
-            Range oldCube = cubes.get(cube);
-            Range newCube = cubes.get(tele.go.c);
-            long diff =  facing.diagonal() ? start.x - oldCube.start.x : start.y - oldCube.start.y;
-            long newX = tele.go.d == WEST ? newCube.start.x : tele.go.d == EAST ? newCube.end.x : tele.translate ? newCube.end.x - diff : newCube.start.x + diff;
-            long newY = tele.go.d == NORTH ? newCube.start.y : tele.go.d == SOUTH ? newCube.end.y : tele.translate ? newCube.end.y - diff : newCube.start.y + diff;
-            newLoc = new Loc(newX, newY);
-            if(g.get(newLoc).get() == '.'){
-              start = newLoc;
-              facing = tele.go.d.opposite();
+            Me newMe = cube ? moveCubic(me) : move(g, me);
+            if(g.get(newMe.l).get() == '.') {
+              me = newMe;
             }
           }
         }
       }
     }
-    return (1000 * (start.y+1)) + (4 * (start.x + 1)) + facing.ordinal() - 1 + (facing == NORTH ? 4 : 0);
+    return (1000 * (me.l.y+1)) + (4 * (me.l.x + 1)) + me.d.ordinal() - 1 + (me.d == NORTH ? 4 : 0);
+  }
+
+  public Me move(InfiniteGrid g, Me me) {
+    Direction opposite = me.d.opposite();
+    Loc newLoc = me.l;
+    for(; g.get(newLoc).isPresent(); newLoc = opposite.move(newLoc));
+    newLoc = me.d.move(newLoc);
+    return new Me(newLoc, me.d);
+  }
+
+  public Me moveCubic(Me me) {
+    int cube = cubes.entrySet().stream().filter(e -> e.getValue().inRange(me.l)).mapToInt(Map.Entry::getKey).findFirst().getAsInt();
+    var tele = todo.get(new Location(cube, me.d));
+    Range oldCube = cubes.get(cube);
+    Range newCube = cubes.get(tele.go.c);
+    long diff =  me.d.diagonal() ? me.l.x - oldCube.start.x : me.l.y - oldCube.start.y;
+    long newX = tele.go.d == WEST ? newCube.start.x : tele.go.d == EAST ? newCube.end.x : tele.translate ? newCube.end.x - diff : newCube.start.x + diff;
+    long newY = tele.go.d == NORTH ? newCube.start.y : tele.go.d == SOUTH ? newCube.end.y : tele.translate ? newCube.end.y - diff : newCube.start.y + diff;
+    return new Me(new Loc(newX, newY), tele.go.d.opposite());
   }
 }
