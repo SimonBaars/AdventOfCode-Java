0d90b931d5b76f3add69a3aeba6ea85b40416a9d
SimonBaars <simon.mailadres@gmail.com>
2022-12-22 10:23:24 +0100

    Use only InfiniteGrid

--- src/main/java/com/sbaars/adventofcode/year22/days/Day22.java
+++ src/main/java/com/sbaars/adventofcode/year22/days/Day22.java
@@ -2,20 +2,23 @@ package com.sbaars.adventofcode.year22.days;
 
 import com.sbaars.adventofcode.common.Direction;
 import com.sbaars.adventofcode.common.Pair;
-import com.sbaars.adventofcode.common.grid.CharGrid;
 import com.sbaars.adventofcode.common.grid.InfiniteGrid;
 import com.sbaars.adventofcode.common.location.Loc;
 import com.sbaars.adventofcode.common.location.Range;
 import com.sbaars.adventofcode.year22.Day2022;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static com.sbaars.adventofcode.common.Direction.*;
 
 public class Day22 extends Day2022 {
+  private final int CUBE_SIZE = 50;
+
   public Day22() {
     super(22);
   }
@@ -27,22 +30,15 @@ public class Day22 extends Day2022 {
   public record Me (Loc l, Direction d) {}
   public record Location(int c, Direction d) {}
 
-  Map<Integer, Range> cubes = Map.of(
-          5, new Range(50, 0, 99, 49)/*5*/,
-          6, new Range(100, 0, 149, 49)/*6*/,
-          4, new Range(50, 50, 99, 99)/*4*/,
-          3, new Range(50, 100, 99, 149)/*3*/,
-          2, new Range(0, 100, 49, 149)/*2*/,
-          1, new Range(0, 150, 49, 199)/*1*/
-  );
+  List<Range> cubes = List.of(cube(0, 3), cube(0, 2), cube(1, 2), cube(1, 1), cube(1, 0), cube(2, 0));
   Map<Location, Location> todo = Map.of(
-          new Location(6, EAST), new Location(3, EAST), // A
-          new Location(6, SOUTH), new Location(4, EAST), // B
-          new Location(6, NORTH), new Location(1, SOUTH), // C
-          new Location(4, WEST), new Location(2, NORTH), // D
-          new Location(3, SOUTH), new Location(1, EAST), // E
-          new Location(5, NORTH), new Location(1, WEST), // F
-          new Location(5, WEST), new Location(2, WEST)  // G
+          new Location(5, EAST), new Location(2, EAST), // A
+          new Location(5, SOUTH), new Location(3, EAST), // B
+          new Location(5, NORTH), new Location(0, SOUTH), // C
+          new Location(3, WEST), new Location(1, NORTH), // D
+          new Location(2, SOUTH), new Location(0, EAST), // E
+          new Location(4, NORTH), new Location(0, WEST), // F
+          new Location(4, WEST), new Location(1, WEST)  // G
   ).entrySet().stream()
           .flatMap(e -> Stream.of(Pair.of(e.getKey(), e.getValue()), Pair.of(e.getValue(), e.getKey())))
           .collect(Collectors.toMap(Pair::a, Pair::b));
@@ -59,10 +55,9 @@ public class Day22 extends Day2022 {
 
   public long solve (boolean cube) {
     String[] in = day().split("\n\n");
-    CharGrid grid = new CharGrid(in[0]);
-    InfiniteGrid g = new InfiniteGrid(grid.grid);
+    InfiniteGrid g = new InfiniteGrid(in[0]);
     String instr = in[1].trim();
-    Me me = new Me(g.grid.keySet().stream().filter(l -> grid.get(l) == '.' && l.y == 0).findFirst().get(), EAST);
+    Me me = new Me(g.grid.keySet().stream().filter(l -> g.getChar(l) == '.' && l.y == 0).findFirst().get(), EAST);
     while(!instr.isEmpty()) {
       if(instr.charAt(0) == 'L' || instr.charAt(0) == 'R') {
         me = new Me(me.l, me.d.turn(instr.charAt(0) == 'R'));
@@ -77,7 +72,7 @@ public class Day22 extends Day2022 {
           var atPos = g.get(newLoc);
           if(atPos.isPresent() && atPos.get() == '.') {
             me = new Me(newLoc, me.d);
-          } else if(!atPos.isPresent()) {
+          } else if(atPos.isEmpty()) {
             Me newMe = cube ? moveCubic(me) : move(g, me);
             if(g.get(newMe.l).get() == '.') {
               me = newMe;
@@ -89,7 +84,7 @@ public class Day22 extends Day2022 {
     return (1000 * (me.l.y+1)) + (4 * (me.l.x + 1)) + me.d.ordinal() - 1 + (me.d == NORTH ? 4 : 0);
   }
 
-  public Me move(InfiniteGrid g, Me me) {
+  private Me move(InfiniteGrid g, Me me) {
     Direction opposite = me.d.opposite();
     Loc newLoc = me.l;
     for(; g.get(newLoc).isPresent(); newLoc = opposite.move(newLoc));
@@ -97,8 +92,8 @@ public class Day22 extends Day2022 {
     return new Me(newLoc, me.d);
   }
 
-  public Me moveCubic(Me me) {
-    int cube = cubes.entrySet().stream().filter(e -> e.getValue().inRange(me.l)).mapToInt(Map.Entry::getKey).findFirst().getAsInt();
+  private Me moveCubic(Me me) {
+    int cube = IntStream.range(0, cubes.size()).filter(i -> cubes.get(i).inRange(me.l)).findFirst().getAsInt();
     var tele = todo.get(new Location(cube, me.d));
     Range oldCube = cubes.get(cube);
     Range newCube = cubes.get(tele.c);
@@ -106,5 +101,10 @@ public class Day22 extends Day2022 {
     long newX = tele.d == WEST ? newCube.start.x : tele.d == EAST ? newCube.end.x : tele.d == me.d ? newCube.end.x - diff : newCube.start.x + diff;
     long newY = tele.d == NORTH ? newCube.start.y : tele.d == SOUTH ? newCube.end.y : tele.d == me.d ? newCube.end.y - diff : newCube.start.y + diff;
     return new Me(new Loc(newX, newY), tele.d.opposite());
+  }
+
+  private Range cube(int x, int y) {
+    Loc a = new Loc(x * CUBE_SIZE, y * CUBE_SIZE);
+    return new Range(a, new Loc(a.x+CUBE_SIZE-1, a.y+CUBE_SIZE-1));
   }
 }
