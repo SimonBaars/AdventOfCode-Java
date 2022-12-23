b1f2932daf74699932e976825aed4adbb87682d6
SimonBaars <simon.mailadres@gmail.com>
2022-12-23 09:53:41 +0100

    tiny bit refactoring

--- src/main/java/com/sbaars/adventofcode/year22/days/Day23.java
+++ src/main/java/com/sbaars/adventofcode/year22/days/Day23.java
@@ -1,15 +1,13 @@
 package com.sbaars.adventofcode.year22.days;
 
+import com.sbaars.adventofcode.common.Builder;
 import com.sbaars.adventofcode.common.Direction;
 import com.sbaars.adventofcode.common.Pair;
 import com.sbaars.adventofcode.common.grid.InfiniteGrid;
 import com.sbaars.adventofcode.common.location.Loc;
 import com.sbaars.adventofcode.year22.Day2022;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static com.sbaars.adventofcode.common.Direction.*;
@@ -34,27 +32,26 @@ public class Day23 extends Day2022 {
   }
 
   public long solution(boolean isPart2) {
-    InfiniteGrid g = new InfiniteGrid(dayGrid());
+    Builder<InfiniteGrid> b = new Builder<>(new InfiniteGrid(dayGrid()), InfiniteGrid::new);
     List<Direction> dirs = new ArrayList<>(List.of(NORTH, SOUTH, WEST, EAST));
     for(int i = 0; isPart2 || i<10; i++) {
-      InfiniteGrid newGrid = new InfiniteGrid();
-      InfiniteGrid finalG = g;
-      Map<Loc, Loc> dest = g.grid.keySet().stream()
-              .filter(e -> Arrays.stream(Direction.eightDirections()).anyMatch(d -> finalG.grid.containsKey(d.move(e))))
-              .flatMap(e -> dirs.stream().filter(d -> !finalG.grid.containsKey(d.move(e)) && !finalG.grid.containsKey(d.turn().move(d.move(e)))  && !finalG.grid.containsKey(d.turn(false).move(d.move(e)))).map(d -> new Pair<>(e, d.move(e))).limit(1))
+      InfiniteGrid g = b.get();
+      Map<Loc, Loc> dest = g.stream()
+              .filter(e -> Direction.eight().anyMatch(d -> g.contains(d.move(e))))
+              .flatMap(e -> dirs.stream().filter(d -> !g.contains(d.move(e)) && !g.contains(d.turn().move(d.move(e)))  && !g.contains(d.turn(false).move(d.move(e)))).map(d -> new Pair<>(e, d.move(e))).limit(1))
               .collect(Collectors.toMap(Pair::a, Pair::b));
       dest.entrySet().stream()
               .map(e -> dest.values().stream().filter(l -> l.equals(e.getValue())).limit(2).count() == 1 ? e.getValue() : e.getKey())
-              .forEach(e -> newGrid.set(e, '#'));
+              .forEach(e -> b.getNew().set(e, '#'));
       g.grid.keySet().stream()
               .filter(e -> !dest.containsKey(e))
-              .forEach(e -> newGrid.set(e, '#'));
+              .forEach(e -> b.getNew().set(e, '#'));
       dirs.add(dirs.remove(0));
-      if(newGrid.grid.keySet().equals(g.grid.keySet())){
+      if(b.getNew().grid.keySet().equals(g.grid.keySet())){
         return i+1;
       }
-      g = newGrid;
+      b.refresh();
     }
-    return g.toString().chars().filter(c -> c == ' ').count();
+    return b.get().toString().chars().filter(c -> c == ' ').count();
   }
 }
