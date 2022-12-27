af08997becb60bad90ea099b574cdefba4470aa2
SimonBaars <simon.mailadres@gmail.com>
2022-12-27 12:23:50 +0100

    Refactor Day 6 test using Tree

--- src/main/java/com/sbaars/adventofcode/common/Tree.java
+++ src/main/java/com/sbaars/adventofcode/common/Tree.java
@@ -1,68 +1,130 @@
 package com.sbaars.adventofcode.common;
 
-import com.sbaars.adventofcode.common.grid.InfiniteGrid;
-import com.sbaars.adventofcode.common.location.Loc;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.Supplier;
+import java.util.function.*;
 import java.util.stream.Collector;
+import java.util.stream.Stream;
 
 
 public class Tree<T> {
-    public enum TreeTraversal {CONTINUE, DOWN, UP}
     private final Node<T> rootNode;
 
     public Tree(T rootNodeData) {
-        this.rootNode = new Node<>(rootNodeData);
+        this.rootNode = new Node<>(rootNodeData, 0);
     }
 
-    public Tree(T root, ListMap<T, T> map) {
-        this.rootNode = new Node<>(root, map.get(root), map);
+    public Tree(ListMap<T, T> map) {
+        this.rootNode = new Node<>(map, map.keySet().stream().filter(e -> !map.hasValue(e)).findAny().get(), 0);
     }
 
-
-
-    public static<T> Collector<Loc, Map<Loc, Character>, InfiniteGrid> toTree(Function<?, T> parentMapper, Function<?, T> childMapper) {
-        final Supplier<Map<Loc, Character>> supplier = HashMap::new;
-        final BiConsumer<Map<Loc, Character>, Loc> accumulator = (a, b) -> a.put(b, c);
-        final BinaryOperator<Map<Loc, Character>> combiner = (a, b) -> {a.putAll(b); return a;};
-        final Function<Map<Loc, Character>, InfiniteGrid> finisher = InfiniteGrid::new;
+    public static<T> Collector<Pair<T, T>, ListMap<T, T>, Tree<T>> toTree() {
+        final Supplier<ListMap<T, T>> supplier = ListMap::new;
+        final BiConsumer<ListMap<T, T>, Pair<T, T>> accumulator = (a, b) -> a.addTo(b.a(), b.b());
+        final BinaryOperator<ListMap<T, T>> combiner = ListMap::mergeWith;
+        final Function<ListMap<T, T>, Tree<T>> finisher = Tree::new;
         return Collector.of(supplier, accumulator, combiner, finisher);
     }
 
-    private class Node<T> {
-        final T data;
-        final List<Node<T>> children;
-        final Node<T> parent;
+    public<A> Stream<A> forEach(BiFunction<Node<T>, Integer, A> func) {
+        func.apply(rootNode, 0);
+        List<A> res = new ArrayList<>();
+        List<Node<T>> nodes = List.of(rootNode);
+        for(int i = 0; !nodes.isEmpty(); i++) {
+            for(Node<T> n : nodes) {
+                res.add(func.apply(n, i));
+            }
+            nodes = nodes.stream().map(Node::children).flatMap(List::stream).toList();
+        }
+        return res.stream();
+    }
 
-        public Node(T data, List<Node<T>> children, Node<T> parent) {
+    public Stream<Node<T>> stream() {
+        return toList().stream();
+    }
+
+    public List<Node<T>> toList() {
+        List<Node<T>> res = new ArrayList<>();
+        List<Node<T>> nodes = List.of(rootNode);
+        while(!nodes.isEmpty()) {
+            res.addAll(nodes);
+            nodes = nodes.stream().map(Node::children).flatMap(List::stream).toList();
+        }
+        return res;
+    }
+
+    public Node<T> get(T t) {
+        return stream().filter(n -> n.data.equals(t)).findAny().get();
+    }
+
+    public int distance(Node<T> n1, Node<T> n2) {
+        if(n1.equals(n2)) return 0;
+        List<Node<T>> parents = n1.parents();
+        parents.retainAll(n2.parents());
+        if(parents.isEmpty()) return -1;
+        return (n1.depth-parents.get(0).depth) + (n2.depth-parents.get(0).depth);
+    }
+
+    public class Node<T> {
+        public final T data;
+        public final List<Node<T>> children;
+        public final Node<T> parent;
+        public final int depth;
+        public final int nChildren;
+
+        public Node(T data, List<Node<T>> children, Node<T> parent, int depth) {
             this.data = data;
             this.children = children;
             this.parent = parent;
+            this.nChildren = children.stream().mapToInt(e -> e.nChildren + 1).sum();
+            this.depth = depth;
         }
 
-        public Node(ListMap<T, T> map, Node<T> parent, T data) {
+        public Node(ListMap<T, T> map, Node<T> parent, T data, int depth) {
             this.data = data;
-            this.children = children;
+            this.children = map.get(data).stream().map(d -> new Node<>(map, this, d, depth + 1)).toList();
             this.parent = parent;
+            this.nChildren = children.stream().mapToInt(e -> e.nChildren + 1).sum();
+            this.depth = depth;
         }
 
-        public Node(ListMap<T, T> map, T data) {
-            this(map, null, data);
+        public Node(ListMap<T, T> map, T data, int depth) {
+            this(map, null, data, depth);
         }
 
-        public Node(T data, List<Node<T>> children) {
-            this(data, children, null);
+        public Node(T data, List<Node<T>> children, int depth) {
+            this(data, children, null, depth);
         }
 
-        public Node(T data) {
-            this(data, new ArrayList<>());
+        public Node(T data, int depth) {
+            this(data, new ArrayList<>(), depth);
+        }
+
+        public List<Node<T>> children() {
+            return children;
+        }
+
+        public List<Node<T>> parents() {
+            List<Node<T>> parents = new ArrayList<>();
+            Node<T> n = this;
+            while(n.parent != null) {
+                parents.add(n.parent);
+                n = n.parent;
+            }
+            return parents;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Node<?> node = (Node<?>) o;
+            return data.equals(node.data);
+        }
+
+        @Override
+        public int hashCode() {
+            return data.hashCode();
         }
     }
 }
--- src/main/java/com/sbaars/adventofcode/year19/days/Day6.java
+++ src/main/java/com/sbaars/adventofcode/year19/days/Day6.java
@@ -1,27 +1,17 @@
 package com.sbaars.adventofcode.year19.days;
 
-import com.google.common.collect.ArrayListMultimap;
 import com.sbaars.adventofcode.common.HasRecursion;
+import com.sbaars.adventofcode.common.Pair;
+import com.sbaars.adventofcode.common.Tree;
 import com.sbaars.adventofcode.year19.Day2019;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map.Entry;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 import static com.sbaars.adventofcode.common.Tree.toTree;
 import static com.sbaars.adventofcode.util.DataMapper.readString;
 
 public class Day6 extends Day2019 implements HasRecursion {
-  ArrayListMultimap<String, String> orbits = ArrayListMultimap.create();
-
   public Day6() {
     super(6);
   }
-
-  public record Input(String start, String end) {}
 
   public static void main(String[] args) {
     new Day6().printParts();
@@ -29,53 +19,16 @@ public class Day6 extends Day2019 implements HasRecursion {
 
   @Override
   public Object part1() {
-    String[] nums = dayStream().map(s -> readString(s, "%s)%s", Input.class)).collect(toTree(i -> i.start, i -> i.end));
-    AtomicInteger o = new AtomicInteger();
-    for (Entry<String, Collection<String>> entry : orbits.asMap().entrySet()) {
-      countOrbitsInList(orbits, o, entry.getValue());
-    }
-    return o.get();
-  }
-
-  private void countOrbitsInList(ArrayListMultimap<String, String> orbits, AtomicInteger o, Collection<String> entry) {
-    for (String str : entry) {
-      o.incrementAndGet();
-      if (orbits.containsKey(str)) {
-        countOrbitsInList(orbits, o, orbits.get(str));
-      }
-    }
+    return input().stream().mapToInt(n -> n.depth).sum();
   }
 
   @Override
   public Object part2() {
-    return findRoute("YOU", "SAN");
+    Tree<String> in = input();
+    return in.distance(in.get("YOU"), in.get("SAN")) - 2;
   }
 
-  private int findRoute(String from, String to) {
-    return findRoute(from, to, new ArrayList<>(), 0);
-  }
-
-  private int findRoute(String from, String to, List<String> visited, int depth) {
-    if (visited.contains(from))
-      return 0;
-    visited.add(from);
-    List<String> str = collectAll(from);
-    if (str.contains(to))
-      return depth - 1;
-    for (String s : str) {
-      int findRoute = findRoute(s, to, visited, depth + 1);
-      if (findRoute > 0) return findRoute;
-    }
-    return -1;
-  }
-
-  private List<String> collectAll(String s1) {
-    List<String> s = findOrbit(s1);
-    s.addAll(orbits.get(s1));
-    return s;
-  }
-
-  public List<String> findOrbit(String orbitValue) {
-    return orbits.asMap().entrySet().stream().filter(e -> e.getValue().contains(orbitValue)).map(Entry::getKey).collect(Collectors.toCollection(ArrayList::new));
+  private Tree<String> input() {
+    return dayStream().map(s -> (Pair<String, String>) readString(s, "%s)%s", Pair.class)).collect(toTree());
   }
 }
