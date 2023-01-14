f06b33ad43a4fb3e50bbf81aaf0af97f4c1b6e8b
Simon <simon.mailadres@gmail.com>
2023-01-14 23:40:28 +0100

    2018 day 7 part 1 debugging

--- src/main/java/com/sbaars/adventofcode/year18/days/Day7.java
+++ src/main/java/com/sbaars/adventofcode/year18/days/Day7.java
@@ -1,6 +1,16 @@
 package com.sbaars.adventofcode.year18.days;
 
+import com.sbaars.adventofcode.common.Graph;
+import com.sbaars.adventofcode.common.Pair;
+import com.sbaars.adventofcode.common.Tree;
 import com.sbaars.adventofcode.year18.Day2018;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.sbaars.adventofcode.common.Graph.toGraph;
+import static com.sbaars.adventofcode.util.DataMapper.readString;
 
 public class Day7 extends Day2018 {
 
@@ -14,11 +24,27 @@ public class Day7 extends Day2018 {
 
   @Override
   public Object part1() {
-    return "";
+    var input = input();
+    var done = new HashSet<Graph.Node<Character>>();
+    var current = new HashSet<>(Set.of(input.nodes.values().stream().filter(e -> e.parents().isEmpty()).findAny().get()));
+    StringBuilder output = new StringBuilder();
+    while(!current.isEmpty()) {
+      System.out.println(current.stream().map(n -> n.data()+"").collect(Collectors.joining()));
+      var next = current.stream().sorted().findFirst().get();
+      done.add(next);
+      output.append(next.data());
+      current.remove(next);
+      next.children().stream().filter(c -> done.containsAll(c.parents())).forEach(current::add);
+    }
+    return output.toString();
   }
 
   @Override
   public Object part2() {
     return "";
+  }
+
+  private Graph<Character> input() {
+    return dayStream().map(s -> (Pair<Character, Character>) readString(s, "Step %c must be finished before step %c can begin.", Pair.class)).collect(toGraph());
   }
 }
