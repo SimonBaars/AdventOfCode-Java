31dc528c2ea6c9f386b1704bed93aa87e51e2471
Simon <simon.mailadres@gmail.com>
2023-01-16 23:47:26 +0100

    2018 day 10 tests

---
+++ src/test/java/com/sbaars/adventofcode/year18/days/Day10Test.java
@@ -0,0 +1,30 @@
+package com.sbaars.adventofcode.year18.days;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class Day10Test {
+    Day10 day = new Day10();
+
+    @Test
+    void testPart1() {
+        assertEquals("""
+                
+                 ####   #####      ###  #####   #    #  #    #   ####   ######
+                #    #  #    #      #   #    #  ##   #  #   #   #    #  #    \s
+                #       #    #      #   #    #  ##   #  #  #    #       #    \s
+                #       #    #      #   #    #  # #  #  # #     #       #    \s
+                #       #####       #   #####   # #  #  ##      #       #####\s
+                #       #           #   #  #    #  # #  ##      #       #    \s
+                #       #           #   #   #   #  # #  # #     #       #    \s
+                #       #       #   #   #   #   #   ##  #  #    #       #    \s
+                #    #  #       #   #   #    #  #   ##  #   #   #    #  #    \s
+                 ####   #        ###    #    #  #    #  #    #   ####   #    \s
+                """, day.part1().toString());
+    }
+
+    @Test
+    void testPart2() {
+        assertEquals("10345", day.part2().toString());
+    }
+}
