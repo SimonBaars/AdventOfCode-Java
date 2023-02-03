4109b70e3fb705ece15a8efaaad2de5fbcccf6ee
Simon Baars <simon@Simons-MacBook-Pro.local>
2023-02-03 12:21:01 +0100

    2018 day 13 part 1

--- src/main/java/com/sbaars/adventofcode/year18/days/Day13.java
+++ src/main/java/com/sbaars/adventofcode/year18/days/Day13.java
@@ -1,6 +1,17 @@
 package com.sbaars.adventofcode.year18.days;
 
+import com.sbaars.adventofcode.common.Direction;
+import com.sbaars.adventofcode.common.grid.InfiniteGrid;
+import com.sbaars.adventofcode.common.location.Loc;
+import com.sbaars.adventofcode.common.map.CountMap;
 import com.sbaars.adventofcode.year18.Day2018;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import static com.sbaars.adventofcode.common.Direction.*;
 
 public class Day13 extends Day2018 {
   public Day13() {
@@ -13,7 +24,46 @@ public class Day13 extends Day2018 {
 
   @Override
   public Object part1() {
-    return "";
+    var tracks = new InfiniteGrid(dayGrid());
+    var carts = new InfiniteGrid(tracks);
+    var cartMap = new CountMap<Loc>();
+    tracks.replace('>', '-');
+    tracks.replace('<', '-');
+    tracks.replace('v', '|');
+    tracks.replace('^', '|');
+    carts.removeIf((l, c) -> List.of('|', '-', '+', '/', '\\').contains(c));
+    carts.grid.forEach((l, c) -> cartMap.put(l, 0));
+    while(true) {
+      for (Map.Entry<Loc, Character> e : new HashSet<>(carts.grid.entrySet())) {
+        Direction d = caretToDirection(e.getValue());
+        Loc destination = d.move(e.getKey());
+        char atDestination = tracks.get(destination).orElse(' ');
+        if(carts.get(destination).isPresent()) {
+          return destination.toString().replace(" ", "");
+        } else if (atDestination == '+') {
+          int turn = switch (cartMap.get(e.getKey()) % 3) {
+            case 0 -> 270;
+            case 1 -> 0;
+            case 2 -> 90;
+            default -> throw new IllegalStateException("Invalid state: " + cartMap.get(e.getKey()));
+          };
+          d = d.turnDegrees(turn);
+          cartMap.increment(e.getKey());
+        } else if (atDestination == '\\') {
+          d = d.turn(d == EAST || d == WEST);
+        } else if (atDestination == '/') {
+          d = d.turn(d == NORTH || d == SOUTH);
+        }
+        carts.grid.remove(e.getKey());
+        int state = cartMap.remove(e.getKey());
+        carts.set(destination, d.getCaret());
+        cartMap.put(destination, state);
+      }
+//      var all = new InfiniteGrid(tracks);
+//      all.grid.putAll(carts.grid);
+//      System.out.println(all);
+//      System.out.println("-----------");
+    }
   }
 
   @Override
