3b6bbc5fa801ffe52ff42cc1ed204a33b4274aeb
Simon Baars <simon@Simons-MacBook-Pro.local>
2023-02-03 12:59:14 +0100

    day 13 2018 part 2 progress

--- src/main/java/com/sbaars/adventofcode/year18/days/Day13.java
+++ src/main/java/com/sbaars/adventofcode/year18/days/Day13.java
@@ -1,5 +1,6 @@
 package com.sbaars.adventofcode.year18.days;
 
+import com.sbaars.adventofcode.common.Builder;
 import com.sbaars.adventofcode.common.Direction;
 import com.sbaars.adventofcode.common.grid.InfiniteGrid;
 import com.sbaars.adventofcode.common.location.Loc;
@@ -68,6 +69,59 @@ public class Day13 extends Day2018 {
 
   @Override
   public Object part2() {
-    return "";
+    var tracks = new InfiniteGrid(dayGrid());
+//    var carts = new InfiniteGrid(tracks);
+    var carts = new Builder<InfiniteGrid>(new InfiniteGrid(tracks), InfiniteGrid.class);
+    var cartMap = new CountMap<Loc>();
+    tracks.replace('>', '-');
+    tracks.replace('<', '-');
+    tracks.replace('v', '|');
+    tracks.replace('^', '|');
+    carts.get().removeIf((l, c) -> List.of('|', '-', '+', '/', '\\').contains(c));
+    carts.get().grid.forEach((l, c) -> cartMap.put(l, 0));
+    while(true) {
+      for (Map.Entry<Loc, Character> e : new HashSet<>(carts.grid.entrySet())) {
+        if(carts.get(e.getKey()).isEmpty()) continue;
+        Direction d = caretToDirection(e.getValue());
+        Loc destination = d.move(e.getKey());
+        char atDestination = tracks.get(destination).orElse(' ');
+        if(carts.get(destination).isPresent()) {
+          carts.grid.remove(e.getKey());
+          carts.grid.remove(destination);
+          cartMap.remove(e.getKey());
+          cartMap.remove(destination);
+          continue;
+        } else if (atDestination == '+') {
+          int turn = switch (cartMap.get(e.getKey()) % 3) {
+            case 0 -> 270;
+            case 1 -> 0;
+            case 2 -> 90;
+            default -> throw new IllegalStateException("Invalid state: " + cartMap.get(e.getKey()));
+          };
+          d = d.turnDegrees(turn);
+          cartMap.increment(e.getKey());
+        } else if (atDestination == '\\') {
+          d = d.turn(d == EAST || d == WEST);
+        } else if (atDestination == '/') {
+          d = d.turn(d == NORTH || d == SOUTH);
+        }
+        carts.grid.remove(e.getKey());
+        int state = cartMap.remove(e.getKey());
+        carts.set(destination, d.getCaret());
+        cartMap.put(destination, state);
+      }
+
+      if(cartMap.size() == 1) {
+        var all = new InfiniteGrid(tracks);
+        all.grid.putAll(carts.grid);
+        System.out.println(all);
+        System.out.println("-----------");
+        return carts.stream().findFirst().get().toString().replace(" ", "");
+      }
+//      var all = new InfiniteGrid(tracks);
+//      all.grid.putAll(carts.grid);
+//      System.out.println(all);
+//      System.out.println("-----------");
+    }
   }
 }
