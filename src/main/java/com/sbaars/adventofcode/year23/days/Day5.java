5a048702635fa94242012a4561323b5e8b41872d
Simon Baars <simon.mailadres@gmail.com>
2023-12-06 00:21:10 +0530

    Day 5: do the data mapping

--- src/main/java/com/sbaars/adventofcode/year23/days/Day5.java
+++ src/main/java/com/sbaars/adventofcode/year23/days/Day5.java
@@ -2,6 +2,12 @@ package com.sbaars.adventofcode.year23.days;
 
 import com.sbaars.adventofcode.year23.Day2023;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.sbaars.adventofcode.util.DataMapper.readString;
+
 public class Day5 extends Day2023 {
 
   public Day5() {
@@ -12,9 +18,16 @@ public class Day5 extends Day2023 {
     new Day5().printParts();
   }
 
+  public record Almanac(List<Integer> seeds, List<Mapping> maps) {}
+  public record Mapping(String source, String dest, List<SeedNumbers> nums) {}
+  public record SeedNumbers(long destinationStart, long sourceStart, long size) {}
+
   @Override
   public Object part1() {
-    return "";
+    String[] day = day().split("\n\n");
+    List<Integer> seeds = readString(day[0], "seeds: %ln", " ", ArrayList.class);
+    List<Mapping> mappings = Arrays.stream(Arrays.copyOfRange(day, 1, day.length)).map(s -> readString(s, "%s-to-%s map:\n%l(%n %n %n)", "\n", Mapping.class, SeedNumbers.class)).toList();
+    return new Almanac(seeds, mappings);
   }
 
   @Override
