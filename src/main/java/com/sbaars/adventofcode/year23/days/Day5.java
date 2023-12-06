cd0db8b3fcfbd0b8e1324630b812f1dc85223cc4
Simon Baars <simon.mailadres@gmail.com>
2023-12-06 23:10:33 +0530

    easier to bruteforce day 5 solution

--- src/main/java/com/sbaars/adventofcode/year23/days/Day5.java
+++ src/main/java/com/sbaars/adventofcode/year23/days/Day5.java
@@ -28,25 +28,18 @@ public class Day5 extends Day2023 {
   public Object part1() {
     String[] day = day().split("\n\n");
     List<Long> seeds = getSeeds(day);
-    return findLocations(day, seeds);
+    return seeds.stream().mapToLong(seed -> findLocations(day, seed)).min().getAsLong();
   }
 
-  private ArrayList getSeeds(String[] day) {
+  private ArrayList<Long> getSeeds(String[] day) {
     return readString(day[0], "seeds: %ln", " ", ArrayList.class);
   }
 
-  private long findLocations(String[] day, List<Long> seeds) {
+  private long findLocations(String[] day, long seed) {
     List<Mapping> mappings = mappings(day);
     return mappings
             .stream()
-            .reduce(seeds, (acc, m) -> acc.stream().map(seed -> m.nums.stream().map(n -> new Loc(n.sourceStart, n.sourceStart + n.size - 1).contains(seed) ? seed + (n.destinationStart - n.sourceStart) : -1).filter(n -> n != -1).findAny().orElse(seed)).toList(), (a, b) -> {
-              a.addAll(b);
-              return a;
-            })
-            .stream()
-            .mapToLong(e -> e)
-            .min()
-            .getAsLong();
+            .reduce(seed, (s, m) -> m.nums.stream().map(n -> new Loc(n.sourceStart, n.sourceStart + n.size - 1).contains(s) ? s + (n.destinationStart - n.sourceStart) : -1).filter(n -> n != -1).findAny().orElse(s), Long::sum);
   }
 
   private static List<Mapping> mappings(String[] day) {
@@ -57,6 +50,6 @@ public class Day5 extends Day2023 {
   public Object part2() {
     String[] day = day().split("\n\n");
     List<Long> seeds = getSeeds(day);
-    return findLocations(day, pairs(seeds).flatMap(p -> LongStream.range(p.a(), p.a() + p.b()).boxed()).toList());
+    return pairs(seeds).flatMap(p -> LongStream.range(p.a(), p.a() + p.b()).boxed()).mapToLong(s -> findLocations(day, s)).min().getAsLong();
   }
 }
