a635d24a26794be0605d69fb6f09d0f9dc44e1c9
Simon Baars <simon.mailadres@gmail.com>
2023-12-06 17:00:17 +0530

    Day 5 part 1

--- src/main/java/com/sbaars/adventofcode/year23/days/Day5.java
+++ src/main/java/com/sbaars/adventofcode/year23/days/Day5.java
@@ -1,5 +1,6 @@
 package com.sbaars.adventofcode.year23.days;
 
+import com.sbaars.adventofcode.common.location.Loc;
 import com.sbaars.adventofcode.year23.Day2023;
 
 import java.util.ArrayList;
@@ -18,16 +19,21 @@ public class Day5 extends Day2023 {
     new Day5().printParts();
   }
 
-  public record Almanac(List<Integer> seeds, List<Mapping> maps) {}
   public record Mapping(String source, String dest, List<SeedNumbers> nums) {}
   public record SeedNumbers(long destinationStart, long sourceStart, long size) {}
 
   @Override
   public Object part1() {
     String[] day = day().split("\n\n");
-    List<Integer> seeds = readString(day[0], "seeds: %ln", " ", ArrayList.class);
+    List<Long> seeds = readString(day[0], "seeds: %ln", " ", ArrayList.class);
     List<Mapping> mappings = Arrays.stream(Arrays.copyOfRange(day, 1, day.length)).map(s -> readString(s, "%s-to-%s map:\n%l(%n %n %n)", "\n", Mapping.class, SeedNumbers.class)).toList();
-    return new Almanac(seeds, mappings);
+    return mappings
+            .stream()
+            .reduce(seeds, (acc, m) -> acc.stream().map(seed -> m.nums.stream().map(n -> new Loc(n.sourceStart, n.sourceStart + n.size - 1).contains(seed) ? seed + (n.destinationStart - n.sourceStart) : -1).filter(n -> n != -1).findAny().orElse(seed)).toList(), (a, b) -> {a.addAll(b); return a;})
+            .stream()
+            .mapToLong(e -> e)
+            .min()
+            .getAsLong();
   }
 
   @Override
