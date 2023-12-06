30f08916aacbca118e812e687c85f6436b42e6a8
Simon Baars <simon.mailadres@gmail.com>
2023-12-06 17:05:36 +0530

    Blowing heap space

--- src/main/java/com/sbaars/adventofcode/year23/days/Day5.java
+++ src/main/java/com/sbaars/adventofcode/year23/days/Day5.java
@@ -6,7 +6,9 @@ import com.sbaars.adventofcode.year23.Day2023;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.LongStream;
 
+import static com.sbaars.adventofcode.util.AOCUtils.pairs;
 import static com.sbaars.adventofcode.util.DataMapper.readString;
 
 public class Day5 extends Day2023 {
@@ -25,11 +27,22 @@ public class Day5 extends Day2023 {
   @Override
   public Object part1() {
     String[] day = day().split("\n\n");
-    List<Long> seeds = readString(day[0], "seeds: %ln", " ", ArrayList.class);
+    List<Long> seeds = getSeeds(day);
+    return findLocations(day, seeds);
+  }
+
+  private static ArrayList getSeeds(String[] day) {
+    return readString(day[0], "seeds: %ln", " ", ArrayList.class);
+  }
+
+  private static long findLocations(String[] day, List<Long> seeds) {
     List<Mapping> mappings = Arrays.stream(Arrays.copyOfRange(day, 1, day.length)).map(s -> readString(s, "%s-to-%s map:\n%l(%n %n %n)", "\n", Mapping.class, SeedNumbers.class)).toList();
     return mappings
             .stream()
-            .reduce(seeds, (acc, m) -> acc.stream().map(seed -> m.nums.stream().map(n -> new Loc(n.sourceStart, n.sourceStart + n.size - 1).contains(seed) ? seed + (n.destinationStart - n.sourceStart) : -1).filter(n -> n != -1).findAny().orElse(seed)).toList(), (a, b) -> {a.addAll(b); return a;})
+            .reduce(seeds, (acc, m) -> acc.stream().map(seed -> m.nums.stream().map(n -> new Loc(n.sourceStart, n.sourceStart + n.size - 1).contains(seed) ? seed + (n.destinationStart - n.sourceStart) : -1).filter(n -> n != -1).findAny().orElse(seed)).toList(), (a, b) -> {
+              a.addAll(b);
+              return a;
+            })
             .stream()
             .mapToLong(e -> e)
             .min()
@@ -38,6 +51,9 @@ public class Day5 extends Day2023 {
 
   @Override
   public Object part2() {
-    return "";
+    String[] day = day().split("\n\n");
+    List<Long> seeds = getSeeds(day);
+    seeds = pairs(seeds).flatMap(p -> LongStream.range(p.a(), p.a() + p.b()).boxed()).toList();
+    return findLocations(day, seeds);
   }
 }
