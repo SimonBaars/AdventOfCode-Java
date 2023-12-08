faac7b36f25e1f681a72b431d6d97450d5b5c1fa
Simon Baars <simon.mailadres@gmail.com>
2023-12-08 11:01:33 +0530

    Day 8

--- src/main/java/com/sbaars/adventofcode/year19/days/Day12.java
+++ src/main/java/com/sbaars/adventofcode/year19/days/Day12.java
@@ -27,11 +27,11 @@ public class Day12 extends Day2019 {
     return a;
   }
 
-  private static long lcm(long a, long b) {
+  public static long lcm(long a, long b) {
     return a * (b / gcd(a, b));
   }
 
-  private static long lcm(long[] input) {
+  public static long lcm(long[] input) {
     long result = input[0];
     for (int i = 1; i < input.length; i++) result = lcm(result, input[i]);
     return result;
--- src/main/java/com/sbaars/adventofcode/year23/days/Day8.java
+++ src/main/java/com/sbaars/adventofcode/year23/days/Day8.java
@@ -2,6 +2,13 @@ package com.sbaars.adventofcode.year23.days;
 
 import com.sbaars.adventofcode.year23.Day2023;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.sbaars.adventofcode.util.DataMapper.readString;
+import static com.sbaars.adventofcode.year19.days.Day12.lcm;
+
 public class Day8 extends Day2023 {
 
   public Day8() {
@@ -12,13 +19,36 @@ public class Day8 extends Day2023 {
     new Day8().printParts();
   }
 
+  public record Instruction(String from, String left, String right) {}
+
   @Override
   public Object part1() {
-    return "";
+    String[] input = day().split("\n\n");
+    Map<String, Instruction> inst = Arrays.stream(input[1].split("\n")).map(s -> readString(s, "%s = (%s, %s)", Instruction.class)).collect(Collectors.toMap(e -> e.from, e -> e));
+    return walk(input, inst, "AAA", "ZZZ");
   }
 
   @Override
   public Object part2() {
-    return "";
+    String[] input = day().split("\n\n");
+    Map<String, Instruction> inst = Arrays.stream(input[1].split("\n")).map(s -> readString(s, "%s = (%s, %s)", Instruction.class)).collect(Collectors.toMap(e -> e.from, e -> e));
+    return lcm(inst.keySet().stream().filter(e -> e.endsWith("A")).mapToLong(e -> walk(input, inst, e, "")).toArray());
+  }
+
+  private static int walk(String[] input, Map<String, Instruction> inst, String start, String end) {
+    int i = 0;
+    String current = start;
+    while(true) {
+      char dir = input[0].charAt(i % input[0].length());
+      if(dir == 'L') {
+        current = inst.get(current).left;
+      } else {
+        current = inst.get(current).right;
+      }
+      i++;
+      if(end.isEmpty() ? current.endsWith("Z") : current.equals(end)) {
+        return i;
+      }
+    }
   }
 }
