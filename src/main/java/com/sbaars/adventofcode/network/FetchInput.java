61d19c20c398b57ab8ffd0392b48c9ef7dd6d773
Simon Baars <simon.mailadres@gmail.com>
2023-12-08 11:01:33 +0530

    Day 8

--- src/main/java/com/sbaars/adventofcode/network/FetchInput.java
+++ src/main/java/com/sbaars/adventofcode/network/FetchInput.java
@@ -29,7 +29,7 @@ public class FetchInput {
   }
 
   public static void main(String[] args) {
-    new FetchInput().retrieveDay("7", "2023");
+    new FetchInput().retrieveDay("8", "2023");
   }
 
   private void retrieveDay(String day, String year) {
