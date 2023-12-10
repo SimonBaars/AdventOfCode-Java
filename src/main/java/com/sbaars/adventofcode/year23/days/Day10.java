1e86bffe22ba89afc3317c0959b6d0c702e60ea9
Simon Baars <simon.mailadres@gmail.com>
2023-12-10 12:55:30 +0530

    Day 10 cleanup

--- src/main/java/com/sbaars/adventofcode/year23/days/Day10.java
+++ src/main/java/com/sbaars/adventofcode/year23/days/Day10.java
@@ -22,12 +22,19 @@ public class Day10 extends Day2023 {
   public Object part1() {
     var grid = new InfiniteGrid(dayGrid());
     Loc start = grid.findAll('S').findAny().get();
-    Direction comeFrom = four().filter(d -> grid.get(d.move(start)).isPresent()).filter(d -> connections(grid.get(d.move(start)).get()).anyMatch(d2 -> d2.equals(d.turnDegrees(180)))).findAny().get();
+    Direction comeFrom = four()
+        .filter(d -> grid.contains(d.move(start)))
+        .filter(d -> connections(grid.get(d.move(start)).get()).anyMatch(d2 -> d2.equals(d.turnDegrees(180))))
+        .findAny()
+        .get();
     Loc currentLoc = comeFrom.move(start);
     int numWalked = 1;
     while (!currentLoc.equals(start)) {
       Direction nextDir = comeFrom;
-      comeFrom = connections(grid.get(currentLoc).get()).filter(d -> !d.equals(nextDir.turnDegrees(180))).findAny().get();
+      comeFrom = connections(grid.get(currentLoc).get())
+          .filter(d -> !d.equals(nextDir.turnDegrees(180)))
+          .findAny()
+          .get();
       currentLoc = comeFrom.move(currentLoc);
       numWalked++;
     }
@@ -48,22 +55,15 @@ public class Day10 extends Day2023 {
 
   @Override
   public Object part2() {
-//    this.example = 13;
     var grid = new InfiniteGrid(dayGrid());
     var visited = new InfiniteGrid(' ', grid.width() * 2, grid.height() * 2);
     Loc start = grid.findAll('S').findAny().get();
-    Direction comeFrom = four().filter(d -> grid.get(d.move(start)).isPresent()).filter(d -> connections(grid.get(d.move(start)).get()).anyMatch(d2 -> d2.equals(d.turnDegrees(180)))).findAny().get();
-    Loc currentLoc = comeFrom.move(start);
-    visited.set(currentLoc.translate(l -> l * 2), 'X');
-    visited.set(comeFrom.turnDegrees(180).move(currentLoc.translate(l -> l * 2)), 'X');
+    Direction comeFrom = four().filter(d -> grid.contains(d.move(start))).filter(d -> connections(grid.get(d.move(start)).get()).anyMatch(d2 -> d2.equals(d.turnDegrees(180)))).findAny().get();
+    Loc currentLoc = moveLoc(start, comeFrom, visited);
     while (!currentLoc.equals(start)) {
       Direction nextDir = comeFrom;
       comeFrom = connections(grid.get(currentLoc).get()).filter(d -> !d.equals(nextDir.turnDegrees(180))).findAny().get();
-      currentLoc = comeFrom.move(currentLoc);
-      Loc zoomed = currentLoc.translate(l -> l * 2);
-      visited.set(zoomed, 'X');
-      Loc prevPos = comeFrom.turnDegrees(180).move(zoomed);
-      visited.set(prevPos, 'X');
+      currentLoc = moveLoc(currentLoc, comeFrom, visited);
     }
 
     var pathGrid = new InfiniteGrid('.', grid.width(), grid.height());
@@ -71,21 +71,15 @@ public class Day10 extends Day2023 {
 
     var borderGrid = pathGrid.withBorder(1, '.');
     var borderVisited = visited.withBorder(2, ' ');
-    System.out.println(borderGrid);
-    System.out.println(borderVisited);
-    System.out.println(new InfiniteGrid(borderVisited.floodFill(new Loc(0, 0), c -> c == ' '), 'X'));
-    System.out.println((-1 % 2));
     return borderGrid.findAll('.').count() - borderVisited.floodFill(new Loc(0, 0), c -> c == ' ').stream().filter(l -> l.x % 2 == 0 && l.y % 2 == 0).count();
-//    System.out.println(new InfiniteGrid(visited.floodFill(new Loc(0, 0), c -> c == ' '), 'X'));
-//    return grid
-//        .findAll('|')
-//        .flatMap(l -> Stream.of(new Loc(l.x * 2 + 1, l.y * 2), new Loc(l.x * 2 - 1, l.y * 2)))
-//        .filter(l -> visited.contains(l) && visited.get(l).get() == ' ')
-//        .map(l -> visited.floodFill(l, c -> c == ' '))
-//        .filter(locs -> !locs.contains(new Loc(0, 0)))
-//        .distinct()
-//        .flatMap(Set::stream)
-//        .filter(l -> l.x % 2 == 0 && l.y % 2 == 0)
-//        .count();
+  }
+
+  private static Loc moveLoc(Loc currentLoc, Direction comeFrom, InfiniteGrid visited) {
+    currentLoc = comeFrom.move(currentLoc);
+    Loc zoomed = currentLoc.translate(l -> l * 2);
+    visited.set(zoomed, 'X');
+    Loc prevPos = comeFrom.turnDegrees(180).move(zoomed);
+    visited.set(prevPos, 'X');
+    return currentLoc;
   }
 }
