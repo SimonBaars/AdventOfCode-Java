a32367a79eef6c73578260fda9c241be5b068a84
Simon Baars <simon.mailadres@gmail.com>
2023-12-11 14:29:08 +0530

    Day 11 refactoring

--- src/main/java/com/sbaars/adventofcode/year23/days/Day11.java
+++ src/main/java/com/sbaars/adventofcode/year23/days/Day11.java
@@ -5,6 +5,7 @@ import com.sbaars.adventofcode.common.grid.InfiniteGrid;
 import com.sbaars.adventofcode.common.location.Loc;
 import com.sbaars.adventofcode.year23.Day2023;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
@@ -36,9 +37,13 @@ public class Day11 extends Day2023 {
 
   private long solve(boolean part1) {
     var grid = new InfiniteGrid(dayGrid());
-    Set<Long> emptyRows = grid.rows().entrySet().stream().filter(e -> e.getValue().stream().allMatch(c -> grid.getOptimistic(c) == '.')).map(Map.Entry::getKey).collect(toSet());
-    Set<Long> emptyCols = grid.columns().entrySet().stream().filter(e -> e.getValue().stream().allMatch(c -> grid.getOptimistic(c) == '.')).map(Map.Entry::getKey).collect(toSet());
+    Set<Long> emptyRows = countEmpty(grid.rows(), grid);
+    Set<Long> emptyCols = countEmpty(grid.columns(), grid);
     return allPairs(grid.findAll('#').toList()).mapToLong(p -> p.a().distance(p.b()) + countEmpty(p, Loc::getX, emptyCols, part1) + countEmpty(p, Loc::getY, emptyRows, part1)).sum();
+  }
+
+  private static Set<Long> countEmpty(Map<Long, List<Loc>> rowsOrCols, InfiniteGrid grid) {
+    return rowsOrCols.entrySet().stream().filter(e -> e.getValue().stream().allMatch(c -> grid.getOptimistic(c) == '.')).map(Map.Entry::getKey).collect(toSet());
   }
 
   private long countEmpty(Pair<Loc, Loc> p, Function<Loc, Long> func, Set<Long> emptyRows, boolean part1) {
