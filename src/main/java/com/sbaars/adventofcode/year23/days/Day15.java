b299ae6675dd43c81bb1da106806e3da99c1e34a
Simon Baars <simon.mailadres@gmail.com>
2023-12-15 11:48:37 +0530

    Day 15

--- src/main/java/com/sbaars/adventofcode/year23/days/Day15.java
+++ src/main/java/com/sbaars/adventofcode/year23/days/Day15.java
@@ -1,6 +1,9 @@
 package com.sbaars.adventofcode.year23.days;
 
+import com.sbaars.adventofcode.common.map.ListMap;
 import com.sbaars.adventofcode.year23.Day2023;
+
+import java.util.stream.IntStream;
 
 public class Day15 extends Day2023 {
   public Day15() {
@@ -13,11 +16,38 @@ public class Day15 extends Day2023 {
 
   @Override
   public Object part1() {
-    return "";
+    return dayStream(",").map(String::strip).mapToLong(this::hash).sum();
+  }
+
+  private int hash(String s) {
+    return s.chars().reduce(0, (acc, c) -> ((acc + c) * 17) % 256);
+  }
+
+  public record Lens(String label, boolean dash, int value) {
   }
 
   @Override
   public Object part2() {
-    return "";
+    ListMap<Integer, Lens> lenses = new ListMap<>();
+    dayStream(",")
+        .map(String::strip)
+        .map(s -> s.contains("-") ? new Lens(s.replace("-", ""), true, 0) : new Lens(s.split("=")[0], false, Integer.parseInt(s.split("=")[1])))
+        .forEach(lens -> registerLenses(lens, lenses));
+    return lenses.entrySet().stream().flatMapToLong(e -> IntStream.range(0, e.getValue().size()).mapToLong(i -> (e.getKey() + 1L) * (i + 1L) * e.getValue().get(i).value)).sum();
+  }
+
+  private void registerLenses(Lens lens, ListMap<Integer, Lens> lenses) {
+    if (lens.dash) {
+      lenses.get(hash(lens.label)).removeIf(l -> l.label.equals(lens.label));
+    } else {
+      lenses.get(hash(lens.label))
+          .stream()
+          .filter(l -> l.label.equals(lens.label))
+          .findFirst()
+          .ifPresentOrElse(
+              l -> lenses.get(hash(lens.label)).set(lenses.get(hash(lens.label)).indexOf(l), lens),
+              () -> lenses.addTo(hash(lens.label), lens)
+          );
+    }
   }
 }
