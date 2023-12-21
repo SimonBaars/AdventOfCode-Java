a1f0aff5e823732bc7d47cd9c42ff1281ec6bf37
Simon Baars <simon.mailadres@gmail.com>
2023-12-21 13:27:53 +0530

    Day 21 solution

--- src/main/java/com/sbaars/adventofcode/year23/days/Day21.java
+++ src/main/java/com/sbaars/adventofcode/year23/days/Day21.java
@@ -1,16 +1,11 @@
 package com.sbaars.adventofcode.year23.days;
 
 import com.sbaars.adventofcode.common.Builder;
-import com.sbaars.adventofcode.common.Pair;
 import com.sbaars.adventofcode.common.grid.InfiniteGrid;
 import com.sbaars.adventofcode.common.location.Loc;
-import com.sbaars.adventofcode.util.Solver;
 import com.sbaars.adventofcode.year23.Day2023;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Stream;
+import java.util.*;
 
 import static com.sbaars.adventofcode.common.Direction.four;
 
@@ -25,7 +20,6 @@ public class Day21 extends Day2023 {
 
   @Override
   public Object part1() {
-//    this.example = 1;
     var grid = new InfiniteGrid(dayGrid());
     Builder<Set<Loc>> places = new Builder<>(HashSet::new);
     places.get().add(grid.findAll('S').findAny().get());
@@ -38,48 +32,54 @@ public class Day21 extends Day2023 {
 
   @Override
   public Object part2() {
-    var grid = new InfiniteGrid(dayGrid());
-    Loc start = grid.findAll('S').findAny().get();
-    grid.repeat(3);
-//    System.out.println(grid);
-//    grid = new InfiniteGrid(grid.grid);
-//    grid.repeat(1);
-//    System.out.println(grid);
-//    Builder<Set<Loc>> places = new Builder<>(HashSet::new);
-//    places.get().add(start);
-//    for (int i = 0; i < 10000; i++) {
-//      places.get().stream().flatMap(l -> four().map(d -> d.move(l))).peek(l -> {
-//        if (!grid.contains(l)) {
-//          System.out.println("DIE");
-//          throw new IllegalStateException();
-//        }
-//      }).filter(l -> grid.getChar(l) != '#').forEach(places.getNew()::add);
-//      places.refresh();
-//      System.out.println(places.get().size());
-//    }
-//    return places.get().size();
-    return Solver.solve(Stream.iterate(new Pair<>(grid, new HashSet<>(List.of(start))), this::doTurn2), p -> p.b().size(), 26501365L);
-  }
+    char[][] grid = dayGrid();
+    var infGrid = new InfiniteGrid(grid);
+    Loc start = infGrid.findAll('S').findAny().get();
 
-  int i = 1;
+    // Core algorithm by abnew123: https://github.com/abnew123/aoc2023/blob/main/src/solutions/Day21.java
+    Map<Loc, Integer> distances = new HashMap<>();
+    Builder<Set<Loc>> places = new Builder<>(HashSet::new);
+    places.get().add(start);
+    distances.put(start, 0);
+    List<Long> totals = new ArrayList<>();
+    List<Long> deltas = new ArrayList<>();
+    List<Long> deltaDeltas = new ArrayList<>();
+    long totalReached = 0;
+    for (int i = 1; ; i++) {
+      int finalIndex = i;
+      places.get().stream().flatMap(l -> four().map(d -> d.move(l))).forEach(l -> {
+        if (distances.get(l) == null) {
+          if (grid[((l.intX() % grid.length) + grid.length) % grid.length][((l.intY() % grid.length) + grid.length) % grid.length] != '#') {
+            places.getNew().add(l);
+            distances.put(l, finalIndex);
+          }
+        }
+      });
+      if (i % 2 == 1) {
+        totalReached += places.getNew().size();
+        if (i % 262 == 65) {
+          totals.add(totalReached);
+          int currTotals = totals.size();
+          if (currTotals > 1) {
+            deltas.add(totals.get(currTotals - 1) - totals.get(currTotals - 2));
+          }
+          int currDeltas = deltas.size();
+          if (currDeltas > 1) {
+            deltaDeltas.add(deltas.get(currDeltas - 1) - deltas.get(currDeltas - 2));
+          }
+          if (deltaDeltas.size() > 1) {
+            long neededLoopCount = 26501365 / 262 - 1;
+            long currentLoopCount = i / 262 - 1;
+            long deltaLoopCount = neededLoopCount - currentLoopCount;
+            long deltaLoopCountTriangular = (neededLoopCount * (neededLoopCount + 1)) / 2 - (currentLoopCount * (currentLoopCount + 1)) / 2;
+            long deltaDelta = deltaDeltas.get(deltaDeltas.size() - 1);
+            long initialDelta = deltas.get(0);
+            return deltaDelta * deltaLoopCountTriangular + initialDelta * deltaLoopCount + totalReached;
+          }
+        }
 
-  public Pair<InfiniteGrid, Set<Loc>> doTurn(Pair<InfiniteGrid, Set<Loc>> places) {
-    var set = new HashSet<Loc>();
-    places.b().stream().flatMap(l -> four().map(d -> d.move(l))).filter(l -> !places.a().contains(l) || places.a().getChar(l) != '#').forEach(set::add);
-    System.out.println(set.size());
-    i++;
-    return new Pair<>(places.a(), set);
-  }
-
-  public Pair<InfiniteGrid, Set<Loc>> doTurn2(Pair<InfiniteGrid, Set<Loc>> places) {
-    var set = new HashSet<Loc>();
-    places.b().stream().flatMap(l -> four().map(d -> d.move(l))).filter(l -> places.a().getChar(l) != '#').forEach(set::add);
-    if (places.b().stream().anyMatch(l -> !places.a().contains(l))) {
-      System.out.println("DIE");
-      throw new IllegalStateException();
+      }
+      places.refresh();
     }
-    System.out.println(set.size());
-    i++;
-    return new Pair<>(places.a(), set);
   }
 }
