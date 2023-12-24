8c93c5b399c9b11785e877b489f68fdf9a8d7491
Simon Baars <simon.mailadres@gmail.com>
2023-12-24 10:34:50 +0530

    Day 24: data parsing

--- src/main/java/com/sbaars/adventofcode/year23/days/Day24.java
+++ src/main/java/com/sbaars/adventofcode/year23/days/Day24.java
@@ -1,6 +1,9 @@
 package com.sbaars.adventofcode.year23.days;
 
+import com.sbaars.adventofcode.common.location.Loc3D;
 import com.sbaars.adventofcode.year23.Day2023;
+
+import static com.sbaars.adventofcode.util.DataMapper.readString;
 
 public class Day24 extends Day2023 {
 
@@ -12,8 +15,15 @@ public class Day24 extends Day2023 {
     new Day24().printParts();
   }
 
+  public record Input(Loc3D pos, Loc3D vel) {
+    public Input(long posX, long posY, long posZ, long velX, long velY, long velZ) {
+      this(new Loc3D(posX, posY, posZ), new Loc3D(velX, velY, velZ));
+    }
+  }
+
   @Override
   public Object part1() {
+    var in = dayStream().map(s -> readString(s, "%n, %n, %n @ %n, %n, %n", Input.class)).toList();
     return "";
   }
 
