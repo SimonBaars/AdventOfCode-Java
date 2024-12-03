bd4ecc8c70bdc665c0397a8fc102c56223a6048a
simon <simon.mailadres@gmail.com>
2024-12-03 06:10:35 +0100

    day 3

--- src/main/java/com/sbaars/adventofcode/year24/days/Day3.java
+++ src/main/java/com/sbaars/adventofcode/year24/days/Day3.java
@@ -2,6 +2,12 @@ package com.sbaars.adventofcode.year24.days;
 
 import com.sbaars.adventofcode.year24.Day2024;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.Integer.parseInt;
+
 public class Day3 extends Day2024 {
   public Day3() {
     super(3);
@@ -13,11 +19,32 @@ public class Day3 extends Day2024 {
 
   @Override
   public Object part1() {
-    return "";
+    return solve(day(), true);
   }
 
   @Override
   public Object part2() {
-    return "";
+    return solve(day(), false);
+  }
+
+  private int solve(String input, boolean ignoreConditions) {
+    Pattern pattern = Pattern.compile("(mul\\((\\d+),(\\d+)\\))|(do\\(\\))|(don't\\(\\))");
+    Matcher matcher = pattern.matcher(input);
+    AtomicBoolean isEnabled = new AtomicBoolean(true);
+
+    return matcher.results()
+        .mapToInt(result -> {
+          if (result.group(1) != null && (ignoreConditions || isEnabled.get())) {
+            return parseInt(result.group(2)) * parseInt(result.group(3));
+          } else if (!ignoreConditions) {
+            if (result.group(4) != null) {
+              isEnabled.set(true);
+            } else if (result.group(5) != null) {
+              isEnabled.set(false);
+            }
+          }
+          return 0;
+        })
+        .sum();
   }
 }
