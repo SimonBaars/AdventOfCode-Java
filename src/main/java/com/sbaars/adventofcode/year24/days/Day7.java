d73e6eecef5ae58a6b844af532f8582aa022f5b7
simon <simon.mailadres@gmail.com>
2024-12-07 06:15:48 +0100

    day 7

--- src/main/java/com/sbaars/adventofcode/year24/days/Day7.java
+++ src/main/java/com/sbaars/adventofcode/year24/days/Day7.java
@@ -2,6 +2,11 @@ package com.sbaars.adventofcode.year24.days;
 
 import com.sbaars.adventofcode.year24.Day2024;
 
+import static java.lang.Long.parseLong;
+
+import java.util.List;
+import static com.sbaars.adventofcode.util.DataMapper.readString;
+
 public class Day7 extends Day2024 {
 
   public Day7() {
@@ -14,11 +19,39 @@ public class Day7 extends Day2024 {
 
   @Override
   public Object part1() {
-    return "";
+    return calcTotalCalibRes(false);
   }
 
   @Override
   public Object part2() {
-    return "";
+    return calcTotalCalibRes(true);
+  }
+
+  public record Line(long tgtVal, List<Long> nums) {}
+
+  private long calcTotalCalibRes(boolean inclConcat) {
+    return dayStream()
+      .map(s -> readString(s, "%n: %ln", " ", Line.class))
+      .filter(l -> canBeTrue(l.tgtVal, l.nums, 0, l.nums.get(0), inclConcat))
+      .mapToLong(l -> l.tgtVal)
+      .sum();
+  }
+
+  private boolean canBeTrue(long tgtVal, List<Long> nums, int i, long currVal, boolean inclConcat) {
+    i++;
+    if (i >= nums.size()) {
+      return currVal == tgtVal;
+    }
+
+    long nextNum = nums.get(i);
+    boolean res = canBeTrue(tgtVal, nums, i, currVal + nextNum, inclConcat) ||
+                  canBeTrue(tgtVal, nums, i, currVal * nextNum, inclConcat);
+
+    if (inclConcat) {
+      long concatVal = parseLong(currVal + "" + nextNum);
+      res = res || canBeTrue(tgtVal, nums, i, concatVal, inclConcat);
+    }
+
+    return res;
   }
 }
