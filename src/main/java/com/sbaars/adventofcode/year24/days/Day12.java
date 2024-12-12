5cbd415f741059c52b1ce7c37ba453e52f4b7fbe
simon <simon.mailadres@gmail.com>
2024-12-12 09:19:40 +0100

    merge solutions

--- src/main/java/com/sbaars/adventofcode/year24/days/Day12.java
+++ src/main/java/com/sbaars/adventofcode/year24/days/Day12.java
@@ -1,26 +1,21 @@
 package com.sbaars.adventofcode.year24.days;
 
 import com.sbaars.adventofcode.common.Direction;
-import com.sbaars.adventofcode.common.Pair;
 import com.sbaars.adventofcode.common.grid.InfiniteGrid;
 import com.sbaars.adventofcode.common.location.Loc;
-import com.sbaars.adventofcode.common.location.MutableLoc;
 import com.sbaars.adventofcode.year24.Day2024;
 
 import java.util.Set;
-import java.util.Stack;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.BiFunction;
 import java.util.stream.Stream;
+
+import static java.util.stream.Stream.concat;
+
 import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Objects;
-
-import static com.sbaars.adventofcode.common.Direction.four;
-import static com.sbaars.adventofcode.common.Direction.fourDirections;
 import static com.sbaars.adventofcode.util.AoCUtils.al;
 import static com.sbaars.adventofcode.util.AoCUtils.appendWhile;
-import static com.sbaars.adventofcode.common.Pair.pair;
 
 public class Day12 extends Day2024 {
   public Day12() {
@@ -33,35 +28,18 @@ public class Day12 extends Day2024 {
 
   @Override
   public Object part1() {
-    Set<Loc> visited = new HashSet<>();
-    var stack = new LinkedList<Loc>();
-    var g = new InfiniteGrid(dayGrid());
-
-    return g.streamChars()
-            .filter((l, c) -> !visited.contains(l))
-            .mapToLong((loc, c) -> {
-              AtomicLong area = al(), perimeter = al();
-              stack.push(loc);
-
-              while (!stack.isEmpty()) {
-                Loc curr = stack.pop();
-                if (visited.add(curr)) {
-                  area.incrementAndGet();
-                  g.walkAround(curr).forEach((l2, d) -> {
-                    if (g.getChar(l2) == c) {
-                      stack.push(l2);
-                    } else {
-                      perimeter.incrementAndGet();
-                    }
-                  });
-                }
-              }
-              return area.get() * perimeter.get();
-            }).sum();
+    return solve((g, data) -> Stream.of());
   }
 
   @Override
   public Object part2() {
+    return solve((g, data) -> concat(explorePerimeter(g, data.edge.a(), data.d, g.getChar(data.edge.a()), true), explorePerimeter(g, data.edge.a(), data.d, g.getChar(data.edge.a()), false)));
+  }
+
+  record Edge(Loc a, Loc b) {}
+  public record Data(Edge edge, Direction d) {}
+
+  public long solve(BiFunction<InfiniteGrid, Data, Stream<Edge>> lambda) {
     Set<Loc> visited = new HashSet<>();
     var stack = new LinkedList<Loc>();
     var g = new InfiniteGrid(dayGrid());
@@ -84,8 +62,7 @@ public class Day12 extends Day2024 {
                 var edge = new Edge(curr, next);
                 if (visited2.add(edge)) {
                   perimeter.incrementAndGet();
-                  explorePerimeter(g, edge.a(), d, c, true).forEach(visited2::add);
-                  explorePerimeter(g, edge.a(), d, c, false).forEach(visited2::add);
+                  lambda.apply(g, new Data(edge, d)).forEach(visited2::add);
                 }
               }
             });
@@ -93,11 +70,7 @@ public class Day12 extends Day2024 {
         }
         return area.get() * perimeter.get();
       }).sum();
-    // solve((g, data) -> {
   }
-
-  record Edge(Loc a, Loc b) {}
-  public record Data(Set<Edge> visited2, Edge edge, Direction d) {}
 
   private Stream<Edge> explorePerimeter(InfiniteGrid g, Loc curr, Direction d, char c, boolean turnRight) {
       Direction d2 = d.turn(turnRight);
