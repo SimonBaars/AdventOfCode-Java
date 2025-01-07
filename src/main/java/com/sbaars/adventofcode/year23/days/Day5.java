feefc14e43d4090c4f30a9cecc8ca6d8b8e88cf5
simon <simon.mailadres@gmail.com>
2025-01-07 21:48:50 +0100

    Improve performance 2023 day 5

--- src/main/java/com/sbaars/adventofcode/year23/days/Day5.java
+++ src/main/java/com/sbaars/adventofcode/year23/days/Day5.java
@@ -6,9 +6,7 @@ import com.sbaars.adventofcode.year23.Day2023;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.LongStream;
 
-import static com.sbaars.adventofcode.util.AoCUtils.pairs;
 import static com.sbaars.adventofcode.util.DataMapper.readString;
 
 public class Day5 extends Day2023 {
@@ -25,6 +23,49 @@ public class Day5 extends Day2023 {
   }
 
   public record SeedNumbers(long destinationStart, long sourceStart, long size) {
+  }
+
+  public record Range(long start, long end) {
+    public boolean overlaps(Range other) {
+      return start <= other.end && end >= other.start;
+    }
+
+    public Range intersection(Range other) {
+      return new Range(Math.max(start, other.start), Math.min(end, other.end));
+    }
+  }
+
+  private List<Range> processRanges(List<Range> inputRanges, List<SeedNumbers> mappings) {
+    List<Range> result = new ArrayList<>();
+    for (Range input : inputRanges) {
+      List<Range> unmapped = new ArrayList<>();
+      unmapped.add(input);
+      
+      for (SeedNumbers mapping : mappings) {
+        Range mapRange = new Range(mapping.sourceStart(), mapping.sourceStart() + mapping.size() - 1);
+        List<Range> nextUnmapped = new ArrayList<>();
+        
+        for (Range current : unmapped) {
+          if (current.overlaps(mapRange)) {
+            Range overlap = current.intersection(mapRange);
+            long offset = mapping.destinationStart() - mapping.sourceStart();
+            result.add(new Range(overlap.start() + offset, overlap.end() + offset));
+            
+            if (current.start() < overlap.start()) {
+              nextUnmapped.add(new Range(current.start(), overlap.start() - 1));
+            }
+            if (current.end() > overlap.end()) {
+              nextUnmapped.add(new Range(overlap.end() + 1, current.end()));
+            }
+          } else {
+            nextUnmapped.add(current);
+          }
+        }
+        unmapped = nextUnmapped;
+      }
+      result.addAll(unmapped);
+    }
+    return result;
   }
 
   @Override
@@ -54,6 +95,16 @@ public class Day5 extends Day2023 {
     String[] day = day().split("\n\n");
     List<Long> seeds = getSeeds(day);
     List<Mapping> mappings = mappings(day);
-    return pairs(seeds).flatMap(p -> LongStream.range(p.a(), p.a() + p.b()).boxed()).parallel().mapToLong(s -> findLocations(day, mappings, s)).min().getAsLong();
+    
+    List<Range> ranges = new ArrayList<>();
+    for (int i = 0; i < seeds.size(); i += 2) {
+      ranges.add(new Range(seeds.get(i), seeds.get(i) + seeds.get(i + 1) - 1));
+    }
+    
+    for (Mapping mapping : mappings) {
+      ranges = processRanges(ranges, mapping.nums());
+    }
+    
+    return ranges.stream().mapToLong(Range::start).min().getAsLong();
   }
 }
