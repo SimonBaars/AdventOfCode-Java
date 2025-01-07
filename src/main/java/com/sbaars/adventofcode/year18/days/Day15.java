9b22dcfca796956e2cc06c3f070b8bbe0ccb45ca
simon <simon.mailadres@gmail.com>
2025-01-07 23:09:54 +0100

    Implement day 15 part 2

--- src/main/java/com/sbaars/adventofcode/year18/days/Day15.java
+++ src/main/java/com/sbaars/adventofcode/year18/days/Day15.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Day15 extends Day2018 {
-  private static final int ATTACK_POWER = 3;
+  private static final int GOBLIN_ATTACK_POWER = 3;
   private static final int INITIAL_HP = 200;
 
   private static class Unit {
@@ -14,16 +14,43 @@ public class Day15 extends Day2018 {
     int hp;
     char type; // 'E' for Elf, 'G' for Goblin
     boolean alive;
+    int attackPower;
 
-    Unit(Loc position, char type) {
+    Unit(Loc position, char type, int attackPower) {
       this.position = position;
       this.hp = INITIAL_HP;
       this.type = type;
       this.alive = true;
+      this.attackPower = attackPower;
     }
 
     boolean isEnemy(Unit other) {
       return this.type != other.type;
+    }
+  }
+
+  private static class CombatResult {
+    final int rounds;
+    final int totalHp;
+    final boolean elvesWon;
+    final boolean elfDied;
+
+    CombatResult(int rounds, int totalHp, boolean elvesWon, boolean elfDied) {
+      this.rounds = rounds;
+      this.totalHp = totalHp;
+      this.elvesWon = elvesWon;
+      this.elfDied = elfDied;
+    }
+
+    int getOutcome() {
+      return rounds * totalHp;
+    }
+  }
+
+  private static class MutableBoolean {
+    boolean value;
+    MutableBoolean(boolean value) {
+      this.value = value;
     }
   }
 
@@ -35,12 +62,13 @@ public class Day15 extends Day2018 {
     new Day15().printParts();
   }
 
-  private List<Unit> parseUnits(char[][] grid) {
+  private List<Unit> parseUnits(char[][] grid, int elfAttackPower) {
     List<Unit> units = new ArrayList<>();
     for (int y = 0; y < grid.length; y++) {
       for (int x = 0; x < grid[y].length; x++) {
         if (grid[y][x] == 'E' || grid[y][x] == 'G') {
-          units.add(new Unit(new Loc(x, y), grid[y][x]));
+          units.add(new Unit(new Loc(x, y), grid[y][x], 
+              grid[y][x] == 'E' ? elfAttackPower : GOBLIN_ATTACK_POWER));
         }
       }
     }
@@ -155,11 +183,19 @@ public class Day15 extends Day2018 {
     return Optional.ofNullable(target);
   }
 
-  @Override
-  public Object part1() {
-    char[][] grid = dayGrid();
-    List<Unit> units = parseUnits(grid);
+  private char[][] copyGrid(char[][] original) {
+    char[][] copy = new char[original.length][];
+    for (int i = 0; i < original.length; i++) {
+      copy[i] = Arrays.copyOf(original[i], original[i].length);
+    }
+    return copy;
+  }
+
+  private CombatResult runCombat(char[][] initialGrid, int elfAttackPower) {
+    char[][] grid = copyGrid(initialGrid);
+    List<Unit> units = parseUnits(grid, elfAttackPower);
     int rounds = 0;
+    final MutableBoolean elfDied = new MutableBoolean(false);
     
     while (true) {
       // Sort units in reading order
@@ -193,9 +229,10 @@ public class Day15 extends Day2018 {
 
         // Attack phase
         findTarget(unit, units, grid).ifPresent(target -> {
-          target.hp -= ATTACK_POWER;
+          target.hp -= unit.attackPower;
           if (target.hp <= 0) {
             target.alive = false;
+            if (target.type == 'E') elfDied.value = true;
             grid[target.position.intY()][target.position.intX()] = '.';
           }
         });
@@ -206,7 +243,9 @@ public class Day15 extends Day2018 {
             .filter(u -> u.alive)
             .mapToInt(u -> u.hp)
             .sum();
-        return rounds * totalHp;
+        boolean elvesWon = units.stream()
+            .anyMatch(u -> u.alive && u.type == 'E');
+        return new CombatResult(rounds, totalHp, elvesWon, elfDied.value);
       }
 
       rounds++;
@@ -214,7 +253,22 @@ public class Day15 extends Day2018 {
   }
 
   @Override
+  public Object part1() {
+    CombatResult result = runCombat(dayGrid(), GOBLIN_ATTACK_POWER);
+    return result.getOutcome();
+  }
+
+  @Override
   public Object part2() {
-    return "";
+    char[][] grid = dayGrid();
+    int elfAttackPower = 4;
+    
+    while (true) {
+      CombatResult result = runCombat(grid, elfAttackPower);
+      if (result.elvesWon && !result.elfDied) {
+        return result.getOutcome();
+      }
+      elfAttackPower++;
+    }
   }
 }
