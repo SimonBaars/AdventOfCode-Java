3bdace84550bc533da6ca0779e0f2c423c8c07aa
simon <simon.mailadres@gmail.com>
2025-01-07 18:26:30 +0100

    small refactoring

--- src/main/java/com/sbaars/adventofcode/year23/days/Day20.java
+++ src/main/java/com/sbaars/adventofcode/year23/days/Day20.java
@@ -29,7 +29,7 @@ public class Day20 extends Day2023 {
             default -> Type.BROADCAST;
           },
           in.split(" -> ")[0].replaceAll("[&%]", ""),
-          Arrays.asList(in.split(" -> ")[1].split(", ")));
+          List.of(in.split(" -> ")[1].split(", ")));
     }
   }
 
@@ -38,8 +38,7 @@ public class Day20 extends Day2023 {
 
   @Override
   public Object part1() {
-    var in = dayStrings();
-    var modules = Arrays.stream(in).map(Module::new).collect(toMap(m -> m.source, m -> m));
+    var modules = Arrays.stream(dayStrings()).map(Module::new).collect(toMap(m -> m.source, m -> m));
     var flipflopState = modules.keySet().stream().filter(m -> modules.get(m).t == Type.FLIPFLOP).collect(toMap(k -> k, k -> false));
     var conjunctionState = modules.keySet().stream().filter(m -> modules.get(m).t == Type.CONJUNCTION).collect(toMap(k -> k, k -> modules.entrySet().stream().filter(e -> e.getValue().targets.contains(k)).map(Map.Entry::getKey).collect(toMap(k2 -> k2, k2 -> false))));
     return range(0, 1000).mapToObj(i -> sendPulse(modules, flipflopState, conjunctionState, new HashMap<>(), i)).reduce(new Pair<>(0L, 0L), (a, b) -> new Pair<>(a.a() + b.a(), a.b() + b.b())).map((a, b) -> a * b);
@@ -47,8 +46,7 @@ public class Day20 extends Day2023 {
 
   private Pair<Long, Long> sendPulse(Map<String, Module> modules, Map<String, Boolean> state, Map<String, Map<String, Boolean>> conjunctionState, Map<String, Long> lcms, long i) {
     Pair<Long, Long> out = new Pair<>(0L, 0L);
-    var pulseQueue = new LinkedList<Signal>();
-    pulseQueue.add(new Signal("", "broadcaster", false));
+    var pulseQueue = new LinkedList<>(List.of(new Signal("", "broadcaster", false)));
     while (!pulseQueue.isEmpty()) {
       var pulse = pulseQueue.pop();
       var isHigh = pulse.isHigh();
@@ -59,41 +57,34 @@ public class Day20 extends Day2023 {
           return new Pair<>(lcm(lcms.values().stream().mapToLong(e -> e).toArray()), 0L);
         }
       }
-      if (!modules.containsKey(pulse.to)) {
-        continue;
-      }
+      if (!modules.containsKey(pulse.to)) continue;
       var module = modules.get(pulse.to);
-      if (module.t == Type.BROADCAST) {
-        module.targets.forEach(t -> pulseQueue.add(new Signal(pulse.to, t, isHigh)));
-      } else if (module.t == Type.CONJUNCTION) {
-        conjunctionState.get(pulse.to).put(pulse.from, isHigh);
-        if (conjunctionState.get(pulse.to).values().stream().allMatch(b -> b)) {
-          module.targets.forEach(t -> pulseQueue.add(new Signal(pulse.to, t, false)));
-        } else {
-          module.targets.forEach(t -> pulseQueue.add(new Signal(pulse.to, t, true)));
+      switch (module.t) {
+        case BROADCAST -> module.targets.forEach(t -> pulseQueue.add(new Signal(pulse.to, t, isHigh)));
+        case CONJUNCTION -> {
+          conjunctionState.get(pulse.to).put(pulse.from, isHigh);
+          boolean allHigh = conjunctionState.get(pulse.to).values().stream().allMatch(b -> b);
+          module.targets.forEach(t -> pulseQueue.add(new Signal(pulse.to, t, !allHigh)));
         }
-      } else {
-        if (!isHigh) {
-          boolean newState = !state.get(module.source);
-          state.put(module.source, newState);
-          module.targets.forEach(t -> pulseQueue.add(new Signal(pulse.to, t, newState)));
+        case FLIPFLOP -> {
+          if (!isHigh) {
+            boolean newState = !state.get(module.source);
+            state.put(module.source, newState);
+            module.targets.forEach(t -> pulseQueue.add(new Signal(pulse.to, t, newState)));
+          }
         }
       }
     }
-    if (!lcms.isEmpty()) {
-      return new Pair<>(0L, 0L);
-    }
-    return out;
+    return lcms.isEmpty() ? out : new Pair<>(0L, 0L);
   }
 
   @Override
   public Object part2() {
-    var in = dayStrings();
-    var modules = Arrays.stream(in).map(Module::new).collect(toMap(m -> m.source, m -> m));
-    var rx = modules.keySet().stream().filter(m -> modules.get(m).targets.contains("rx")).findFirst().get();
+    var modules = Arrays.stream(dayStrings()).map(Module::new).collect(toMap(m -> m.source, m -> m));
+    var rx = modules.keySet().stream().filter(m -> modules.get(m).targets.contains("rx")).findFirst().orElseThrow();
     var flipflopState = modules.keySet().stream().filter(m -> modules.get(m).t == Type.FLIPFLOP).collect(toMap(k -> k, k -> false));
     var conjunctionState = modules.keySet().stream().filter(m -> modules.get(m).t == Type.CONJUNCTION).collect(toMap(k -> k, k -> modules.entrySet().stream().filter(e -> e.getValue().targets.contains(k)).map(Map.Entry::getKey).collect(toMap(k2 -> k2, k2 -> false))));
     var lcms = conjunctionState.get(rx).keySet().stream().collect(toMap(e -> e, e -> 0L));
-    return range(1, MAX_VALUE).map(i -> sendPulse(modules, flipflopState, conjunctionState, lcms, i).a()).filter(e -> e != 0L).findFirst().getAsLong();
+    return range(1, MAX_VALUE).map(i -> sendPulse(modules, flipflopState, conjunctionState, lcms, i).a()).filter(e -> e != 0L).findFirst().orElseThrow();
   }
 }
