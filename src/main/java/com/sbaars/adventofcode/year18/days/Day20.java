dc926f693d97bc8d843c3351fa9f2b4daf57c027
simon <simon.mailadres@gmail.com>
2025-01-08 00:08:07 +0100

    d20p1

--- src/main/java/com/sbaars/adventofcode/year18/days/Day20.java
+++ src/main/java/com/sbaars/adventofcode/year18/days/Day20.java
@@ -1,6 +1,9 @@
 package com.sbaars.adventofcode.year18.days;
 
 import com.sbaars.adventofcode.year18.Day2018;
+import com.sbaars.adventofcode.common.Direction;
+import com.sbaars.adventofcode.common.location.Loc;
+import java.util.*;
 
 public class Day20 extends Day2018 {
   public Day20() {
@@ -11,9 +14,69 @@ public class Day20 extends Day2018 {
     new Day20().printParts();
   }
 
+  private void explore(String regex) {
+    Stack<Loc> posStack = new Stack<>();
+    Stack<Integer> distStack = new Stack<>();
+    Map<Loc, Integer> distances = new HashMap<>();
+    Loc current = new Loc(0, 0);
+    distances.put(current, 0);
+    int currentDist = 0;
+
+    for (int i = 0; i < regex.length(); i++) {
+      char c = regex.charAt(i);
+      switch (c) {
+        case 'N':
+          current = current.move(Direction.NORTH);
+          currentDist++;
+          if (!distances.containsKey(current) || distances.get(current) > currentDist) {
+            distances.put(current, currentDist);
+          }
+          break;
+        case 'S':
+          current = current.move(Direction.SOUTH);
+          currentDist++;
+          if (!distances.containsKey(current) || distances.get(current) > currentDist) {
+            distances.put(current, currentDist);
+          }
+          break;
+        case 'E':
+          current = current.move(Direction.EAST);
+          currentDist++;
+          if (!distances.containsKey(current) || distances.get(current) > currentDist) {
+            distances.put(current, currentDist);
+          }
+          break;
+        case 'W':
+          current = current.move(Direction.WEST);
+          currentDist++;
+          if (!distances.containsKey(current) || distances.get(current) > currentDist) {
+            distances.put(current, currentDist);
+          }
+          break;
+        case '(':
+          posStack.push(current);
+          distStack.push(currentDist);
+          break;
+        case '|':
+          current = posStack.peek();
+          currentDist = distStack.peek();
+          break;
+        case ')':
+          current = posStack.pop();
+          currentDist = distStack.pop();
+          break;
+      }
+    }
+    this.distances = distances;
+  }
+
+  private Map<Loc, Integer> distances;
+
   @Override
   public Object part1() {
-    return "";
+    String input = day().trim();
+    explore(input);
+    return distances.values().stream().mapToInt(Integer::intValue).max().orElse(0);
   }
 
   @Override
