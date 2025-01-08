953727af7ac60003ec5ff02539ed7f13c1120440
simon <simon.mailadres@gmail.com>
2025-01-08 19:32:48 +0100

    Day 7: Implement part 1 - IPv7 TLS support

--- src/main/java/com/sbaars/adventofcode/year16/days/Day7.java
+++ src/main/java/com/sbaars/adventofcode/year16/days/Day7.java
@@ -1,6 +1,8 @@
 package com.sbaars.adventofcode.year16.days;
 
 import com.sbaars.adventofcode.year16.Day2016;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Day7 extends Day2016 {
 
@@ -12,9 +14,49 @@ public class Day7 extends Day2016 {
     new Day7().printParts();
   }
 
+  private boolean hasAbba(String s) {
+    for (int i = 0; i < s.length() - 3; i++) {
+      if (s.charAt(i) == s.charAt(i + 3) && 
+          s.charAt(i + 1) == s.charAt(i + 2) && 
+          s.charAt(i) != s.charAt(i + 1)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean supportsTls(String ip) {
+    List<String> outsideBrackets = new ArrayList<>();
+    List<String> insideBrackets = new ArrayList<>();
+    StringBuilder current = new StringBuilder();
+    boolean inBrackets = false;
+
+    for (char c : ip.toCharArray()) {
+      if (c == '[') {
+        outsideBrackets.add(current.toString());
+        current = new StringBuilder();
+        inBrackets = true;
+      } else if (c == ']') {
+        insideBrackets.add(current.toString());
+        current = new StringBuilder();
+        inBrackets = false;
+      } else {
+        current.append(c);
+      }
+    }
+    if (!current.isEmpty()) {
+      outsideBrackets.add(current.toString());
+    }
+
+    return outsideBrackets.stream().anyMatch(this::hasAbba) && 
+           insideBrackets.stream().noneMatch(this::hasAbba);
+  }
+
   @Override
   public Object part1() {
-    return "";
+    return dayStream()
+        .filter(this::supportsTls)
+        .count();
   }
 
   @Override
