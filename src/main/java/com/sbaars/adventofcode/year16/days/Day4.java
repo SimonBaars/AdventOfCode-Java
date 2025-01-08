f9e336520f024c4bce3c5b72c353bd15ecbc54f6
simon <simon.mailadres@gmail.com>
2025-01-08 19:32:37 +0100

    Day 4: Implement part 1 - Room sector IDs

--- src/main/java/com/sbaars/adventofcode/year16/days/Day4.java
+++ src/main/java/com/sbaars/adventofcode/year16/days/Day4.java
@@ -1,6 +1,9 @@
 package com.sbaars.adventofcode.year16.days;
 
 import com.sbaars.adventofcode.year16.Day2016;
+import java.util.Map;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Day4 extends Day2016 {
 
@@ -12,9 +15,40 @@ public class Day4 extends Day2016 {
     new Day4().printParts();
   }
 
+  private boolean isRealRoom(String name, String checksum) {
+    // Remove dashes and count letter frequencies
+    Map<Character, Long> freq = name.replace("-", "").chars()
+        .mapToObj(c -> (char) c)
+        .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+
+    // Get top 5 most common letters, breaking ties alphabetically
+    String calculatedChecksum = freq.entrySet().stream()
+        .sorted((a, b) -> {
+          int freqCompare = b.getValue().compareTo(a.getValue());
+          return freqCompare != 0 ? freqCompare : a.getKey().compareTo(b.getKey());
+        })
+        .limit(5)
+        .map(e -> String.valueOf(e.getKey()))
+        .collect(Collectors.joining());
+
+    return calculatedChecksum.equals(checksum);
+  }
+
   @Override
   public Object part1() {
-    return "";
+    return dayStream()
+        .map(line -> {
+          // Parse room string into components
+          int checksumStart = line.indexOf('[');
+          String checksum = line.substring(checksumStart + 1, line.length() - 1);
+          String[] parts = line.substring(0, checksumStart).split("-");
+          int sectorId = Integer.parseInt(parts[parts.length - 1]);
+          String name = String.join("-", Arrays.copyOf(parts, parts.length - 1));
+
+          return isRealRoom(name, checksum) ? sectorId : 0;
+        })
+        .mapToInt(Integer::intValue)
+        .sum();
   }
 
   @Override
