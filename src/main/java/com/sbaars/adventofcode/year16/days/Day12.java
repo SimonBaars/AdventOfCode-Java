f6186d9986c49c3ff095c90f00e3f7e355e0128c
simon <simon.mailadres@gmail.com>
2025-01-08 19:33:15 +0100

    Day 12: Implement part 1 - Assembunny code

--- src/main/java/com/sbaars/adventofcode/year16/days/Day12.java
+++ src/main/java/com/sbaars/adventofcode/year16/days/Day12.java
@@ -1,6 +1,9 @@
 package com.sbaars.adventofcode.year16.days;
 
 import com.sbaars.adventofcode.year16.Day2016;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Day12 extends Day2016 {
   public Day12() {
@@ -11,9 +14,51 @@ public class Day12 extends Day2016 {
     new Day12().printParts();
   }
 
+  private int getValue(String x, Map<String, Integer> registers) {
+    try {
+      return Integer.parseInt(x);
+    } catch (NumberFormatException e) {
+      return registers.getOrDefault(x, 0);
+    }
+  }
+
+  private int executeProgram(List<String> instructions) {
+    Map<String, Integer> registers = new HashMap<>();
+    int pc = 0;
+
+    while (pc < instructions.size()) {
+      String[] parts = instructions.get(pc).split(" ");
+      String op = parts[0];
+
+      switch (op) {
+        case "cpy" -> {
+          registers.put(parts[2], getValue(parts[1], registers));
+          pc++;
+        }
+        case "inc" -> {
+          registers.put(parts[1], registers.getOrDefault(parts[1], 0) + 1);
+          pc++;
+        }
+        case "dec" -> {
+          registers.put(parts[1], registers.getOrDefault(parts[1], 0) - 1);
+          pc++;
+        }
+        case "jnz" -> {
+          if (getValue(parts[1], registers) != 0) {
+            pc += Integer.parseInt(parts[2]);
+          } else {
+            pc++;
+          }
+        }
+      }
+    }
+
+    return registers.getOrDefault("a", 0);
+  }
+
   @Override
   public Object part1() {
-    return "";
+    return executeProgram(dayStream().toList());
   }
 
   @Override
