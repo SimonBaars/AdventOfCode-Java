a20f9628995e92e400438daa1eac9e44baeef839
simon <simon.mailadres@gmail.com>
2025-01-08 19:32:59 +0100

    Day 9: Implement part 1 - Decompressed length

--- src/main/java/com/sbaars/adventofcode/year16/days/Day9.java
+++ src/main/java/com/sbaars/adventofcode/year16/days/Day9.java
@@ -1,8 +1,12 @@
 package com.sbaars.adventofcode.year16.days;
 
 import com.sbaars.adventofcode.year16.Day2016;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day9 extends Day2016 {
+  private static final Pattern MARKER_PATTERN = Pattern.compile("\\((\\d+)x(\\d+)\\)");
+
   public Day9() {
     super(9);
   }
@@ -11,9 +15,35 @@ public class Day9 extends Day2016 {
     new Day9().printParts();
   }
 
+  private int decompressedLength(String input) {
+    int length = 0;
+    int i = 0;
+
+    while (i < input.length()) {
+      if (input.charAt(i) == '(') {
+        Matcher m = MARKER_PATTERN.matcher(input.substring(i));
+        if (m.find() && m.start() == 0) {
+          int chars = Integer.parseInt(m.group(1));
+          int repeat = Integer.parseInt(m.group(2));
+          i += m.end();
+          length += chars * repeat;
+          i += chars;
+        } else {
+          length++;
+          i++;
+        }
+      } else {
+        length++;
+        i++;
+      }
+    }
+
+    return length;
+  }
+
   @Override
   public Object part1() {
-    return "";
+    return decompressedLength(day().trim());
   }
 
   @Override
