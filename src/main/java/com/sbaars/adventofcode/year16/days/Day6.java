17d7cffc5225546e75a96cbb3b3623fd53027ba3
simon <simon.mailadres@gmail.com>
2025-01-08 19:32:42 +0100

    Day 6: Implement part 1 - Error-corrected message

--- src/main/java/com/sbaars/adventofcode/year16/days/Day6.java
+++ src/main/java/com/sbaars/adventofcode/year16/days/Day6.java
@@ -1,6 +1,11 @@
 package com.sbaars.adventofcode.year16.days;
 
 import com.sbaars.adventofcode.year16.Day2016;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day6 extends Day2016 {
 
@@ -12,9 +17,25 @@ public class Day6 extends Day2016 {
     new Day6().printParts();
   }
 
+  private char getMostFrequentChar(List<String> messages, int position) {
+    return messages.stream()
+        .map(s -> s.charAt(position))
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+        .entrySet().stream()
+        .max(Map.Entry.comparingByValue())
+        .map(Map.Entry::getKey)
+        .orElseThrow();
+  }
+
   @Override
   public Object part1() {
-    return "";
+    List<String> messages = dayStream().toList();
+    int messageLength = messages.get(0).length();
+
+    return IntStream.range(0, messageLength)
+        .mapToObj(pos -> getMostFrequentChar(messages, pos))
+        .map(String::valueOf)
+        .collect(Collectors.joining());
   }
 
   @Override
