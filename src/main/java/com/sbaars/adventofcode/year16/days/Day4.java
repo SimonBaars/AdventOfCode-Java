de8df96a13794a32d20726ee84ebd8c0a2b165e6
simon <simon.mailadres@gmail.com>
2025-01-08 20:15:30 +0100

    Implement Day 4 solution

--- src/main/java/com/sbaars/adventofcode/year16/days/Day4.java
+++ src/main/java/com/sbaars/adventofcode/year16/days/Day4.java
@@ -1,12 +1,10 @@
 package com.sbaars.adventofcode.year16.days;
 
 import com.sbaars.adventofcode.year16.Day2016;
-import java.util.Map;
-import java.util.Arrays;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Day4 extends Day2016 {
-
   public Day4() {
     super(4);
   }
@@ -15,44 +13,65 @@ public class Day4 extends Day2016 {
     new Day4().printParts();
   }
 
-  private boolean isRealRoom(String name, String checksum) {
-    // Remove dashes and count letter frequencies
-    Map<Character, Long> freq = name.replace("-", "").chars()
-        .mapToObj(c -> (char) c)
-        .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+  private record Room(String name, int sectorId, String checksum) {
+    public boolean isReal() {
+      Map<Character, Integer> freq = new HashMap<>();
+      for (char c : name.replaceAll("-", "").toCharArray()) {
+        freq.merge(c, 1, Integer::sum);
+      }
+      
+      String calculatedChecksum = freq.entrySet().stream()
+          .sorted((a, b) -> {
+            int comp = b.getValue().compareTo(a.getValue());
+            return comp != 0 ? comp : a.getKey().compareTo(b.getKey());
+          })
+          .limit(5)
+          .map(e -> String.valueOf(e.getKey()))
+          .collect(Collectors.joining());
+      
+      return calculatedChecksum.equals(checksum);
+    }
+    
+    public String decrypt() {
+      StringBuilder result = new StringBuilder();
+      for (char c : name.toCharArray()) {
+        if (c == '-') {
+          result.append(' ');
+        } else {
+          int shifted = ((c - 'a' + sectorId) % 26) + 'a';
+          result.append((char) shifted);
+        }
+      }
+      return result.toString();
+    }
+  }
 
-    // Get top 5 most common letters, breaking ties alphabetically
-    String calculatedChecksum = freq.entrySet().stream()
-        .sorted((a, b) -> {
-          int freqCompare = b.getValue().compareTo(a.getValue());
-          return freqCompare != 0 ? freqCompare : a.getKey().compareTo(b.getKey());
-        })
-        .limit(5)
-        .map(e -> String.valueOf(e.getKey()))
-        .collect(Collectors.joining());
-
-    return calculatedChecksum.equals(checksum);
+  private Room parseRoom(String line) {
+    String[] parts = line.split("\\[");
+    String checksum = parts[1].substring(0, parts[1].length() - 1);
+    String[] nameParts = parts[0].split("-(?=\\d)");
+    String name = nameParts[0];
+    int sectorId = Integer.parseInt(nameParts[1]);
+    return new Room(name, sectorId, checksum);
   }
 
   @Override
   public Object part1() {
     return dayStream()
-        .map(line -> {
-          // Parse room string into components
-          int checksumStart = line.indexOf('[');
-          String checksum = line.substring(checksumStart + 1, line.length() - 1);
-          String[] parts = line.substring(0, checksumStart).split("-");
-          int sectorId = Integer.parseInt(parts[parts.length - 1]);
-          String name = String.join("-", Arrays.copyOf(parts, parts.length - 1));
-
-          return isRealRoom(name, checksum) ? sectorId : 0;
-        })
-        .mapToInt(Integer::intValue)
+        .map(this::parseRoom)
+        .filter(Room::isReal)
+        .mapToInt(Room::sectorId)
         .sum();
   }
 
   @Override
   public Object part2() {
-    return "";
+    return dayStream()
+        .map(this::parseRoom)
+        .filter(Room::isReal)
+        .filter(room -> room.decrypt().contains("northpole"))
+        .findFirst()
+        .map(Room::sectorId)
+        .orElse(0);
   }
 }
