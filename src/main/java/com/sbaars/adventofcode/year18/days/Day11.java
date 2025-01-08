9261ac4b6daad0a53c7f6ecfc1276c1474668b14
simon <simon.mailadres@gmail.com>
2025-01-08 01:05:16 +0100

    optimize day 11

--- src/main/java/com/sbaars/adventofcode/year18/days/Day11.java
+++ src/main/java/com/sbaars/adventofcode/year18/days/Day11.java
@@ -1,7 +1,6 @@
 package com.sbaars.adventofcode.year18.days;
 
 import com.sbaars.adventofcode.common.Pair;
-import com.sbaars.adventofcode.common.grid.NumGrid;
 import com.sbaars.adventofcode.common.location.Loc3D;
 import com.sbaars.adventofcode.year18.Day2018;
 
@@ -27,30 +26,41 @@ public class Day11 extends Day2018 {
 
   private String findSolution(boolean part2) {
     long in = dayNumbers()[0];
-    NumGrid g = new NumGrid(new long[300][300]);
-    for (int y = 0; y < g.sizeX(); y++) {
-      for (int x = 0; x < g.sizeY(); x++) {
+    int size = 300;
+    long[][] g = new long[size][size];
+    long[][] summedArea = new long[size + 1][size + 1];
+    
+    // Calculate power levels
+    for (int y = 0; y < size; y++) {
+      for (int x = 0; x < size; x++) {
         long rackId = x + 11;
-        g.grid[y][x] = get100Digit(((rackId * (y + 1)) + in) * rackId) - 5;
+        g[y][x] = get100Digit(((rackId * (y + 1)) + in) * rackId) - 5;
       }
     }
+    
+    // Build summed area table
+    for (int y = 1; y <= size; y++) {
+      for (int x = 1; x <= size; x++) {
+        summedArea[y][x] = g[y-1][x-1] + summedArea[y-1][x] + summedArea[y][x-1] - summedArea[y-1][x-1];
+      }
+    }
+    
     Pair<Long, Loc3D> top = new Pair<>(Long.MIN_VALUE, null);
-    int SQUARE_SIZE = 3;
-    for (int size = part2 ? 1 : SQUARE_SIZE; size <= (part2 ? g.sizeX() - 1 : SQUARE_SIZE); size++) {
-      for (int y = 0; y <= g.sizeX() - size; y++) {
-        for (int x = 0; x <= g.sizeY() - size; x++) {
-          long sum = 0;
-          for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-              sum += g.grid[y + j][x + i];
-            }
-          }
+    int maxSize = part2 ? size : 3;
+    int minSize = part2 ? 1 : 3;
+    
+    // Find maximum sum using summed area table
+    for (int s = minSize; s <= maxSize; s++) {
+      for (int y = s; y <= size; y++) {
+        for (int x = s; x <= size; x++) {
+          long sum = summedArea[y][x] - summedArea[y-s][x] - summedArea[y][x-s] + summedArea[y-s][x-s];
           if (sum > top.a()) {
-            top = new Pair<>(sum, new Loc3D(x + 1, y + 1, size));
+            top = new Pair<>(sum, new Loc3D(x-s+1, y-s+1, s));
           }
         }
       }
     }
+    
     String res = top.b().toString().replace(" ", "");
     return part2 ? res : res.substring(0, res.length() - 2);
   }
