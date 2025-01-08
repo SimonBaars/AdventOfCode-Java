da4632a9f1e7f4dd655845be371d79db43acf5f7
simon <simon.mailadres@gmail.com>
2025-01-08 20:12:45 +0100

    Day 2: Add part 2 - Diamond-shaped keypad

--- src/main/java/com/sbaars/adventofcode/year16/days/Day2.java
+++ src/main/java/com/sbaars/adventofcode/year16/days/Day2.java
@@ -3,10 +3,18 @@ package com.sbaars.adventofcode.year16.days;
 import com.sbaars.adventofcode.year16.Day2016;
 
 public class Day2 extends Day2016 {
-  private static final int[][] KEYPAD = {
+  private static final int[][] KEYPAD_1 = {
     {1, 2, 3},
     {4, 5, 6},
     {7, 8, 9}
+  };
+
+  private static final char[][] KEYPAD_2 = {
+    {' ', ' ', '1', ' ', ' '},
+    {' ', '2', '3', '4', ' '},
+    {'5', '6', '7', '8', '9'},
+    {' ', 'A', 'B', 'C', ' '},
+    {' ', ' ', 'D', ' ', ' '}
   };
 
   public Day2() {
@@ -17,28 +25,61 @@ public class Day2 extends Day2016 {
     new Day2().printParts();
   }
 
-  @Override
-  public Object part1() {
-    int x = 1, y = 1; // Starting at 5 (center)
+  private String findCode(int[][] keypad, int startX, int startY) {
     StringBuilder code = new StringBuilder();
-    
+    int x = startX, y = startY;
+
     for (String line : dayStream().toList()) {
       for (char move : line.toCharArray()) {
+        int newX = x, newY = y;
         switch (move) {
-          case 'U' -> y = Math.max(0, y - 1);
-          case 'D' -> y = Math.min(2, y + 1);
-          case 'L' -> x = Math.max(0, x - 1);
-          case 'R' -> x = Math.min(2, x + 1);
+          case 'U' -> newX--;
+          case 'D' -> newX++;
+          case 'L' -> newY--;
+          case 'R' -> newY++;
+        }
+        if (newX >= 0 && newX < keypad.length && newY >= 0 && newY < keypad[0].length && keypad[newX][newY] != 0) {
+          x = newX;
+          y = newY;
         }
       }
-      code.append(KEYPAD[y][x]);
+      code.append(keypad[x][y]);
     }
-    
+
+    return code.toString();
+  }
+
+  private String findCode2(char[][] keypad, int startX, int startY) {
+    StringBuilder code = new StringBuilder();
+    int x = startX, y = startY;
+
+    for (String line : dayStream().toList()) {
+      for (char move : line.toCharArray()) {
+        int newX = x, newY = y;
+        switch (move) {
+          case 'U' -> newX--;
+          case 'D' -> newX++;
+          case 'L' -> newY--;
+          case 'R' -> newY++;
+        }
+        if (newX >= 0 && newX < keypad.length && newY >= 0 && newY < keypad[0].length && keypad[newX][newY] != ' ') {
+          x = newX;
+          y = newY;
+        }
+      }
+      code.append(keypad[x][y]);
+    }
+
     return code.toString();
   }
 
   @Override
+  public Object part1() {
+    return findCode(KEYPAD_1, 1, 1);
+  }
+
+  @Override
   public Object part2() {
-    return "";
+    return findCode2(KEYPAD_2, 2, 0);
   }
 }
