5845938e26adccaee0a8d79d8787a3a3f6a6fbde
simon <simon.mailadres@gmail.com>
2025-01-29 01:14:39 +0100

    Solved 2017 Day 10 Part 1

--- src/main/java/com/sbaars/adventofcode/year17/days/Day10.java
+++ src/main/java/com/sbaars/adventofcode/year17/days/Day10.java
@@ -1,6 +1,8 @@
 package com.sbaars.adventofcode.year17.days;
 
 import com.sbaars.adventofcode.year17.Day2017;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Day10 extends Day2017 {
   public Day10() {
@@ -13,7 +15,27 @@ public class Day10 extends Day2017 {
 
   @Override
   public Object part1() {
-    return "";
+    int[] list = IntStream.range(0, 256).toArray();
+    int[] lengths = Arrays.stream(day().trim().split(",")).mapToInt(Integer::parseInt).toArray();
+    int currentPos = 0;
+    int skipSize = 0;
+    
+    for (int length : lengths) {
+      // Reverse the sublist
+      for (int i = 0; i < length / 2; i++) {
+        int pos1 = (currentPos + i) % list.length;
+        int pos2 = (currentPos + length - 1 - i) % list.length;
+        int temp = list[pos1];
+        list[pos1] = list[pos2];
+        list[pos2] = temp;
+      }
+      
+      // Move current position and increase skip size
+      currentPos = (currentPos + length + skipSize) % list.length;
+      skipSize++;
+    }
+    
+    return list[0] * list[1];
   }
 
   @Override
