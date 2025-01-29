a6310cca260088bc2779413d7f6f78530135d29b
simon <simon.mailadres@gmail.com>
2025-01-29 01:18:20 +0100

    Solved 2017 Day 15 Part 1

--- src/main/java/com/sbaars/adventofcode/year17/days/Day15.java
+++ src/main/java/com/sbaars/adventofcode/year17/days/Day15.java
@@ -1,23 +1,41 @@
 package com.sbaars.adventofcode.year17.days;
 
-import com.sbaars.adventofcode.year17.Day2017;
+import com.sbaars.adventofcode.common.Day;
 
-public class Day15 extends Day2017 {
-  public Day15() {
-    super(15);
-  }
+public class Day15 extends Day {
+    private static final long FACTOR_A = 16807;
+    private static final long FACTOR_B = 48271;
+    private static final long DIVISOR = 2147483647;
+    private static final int MASK = 0xFFFF;  // For getting lowest 16 bits
 
-  public static void main(String[] args) {
-    new Day15().printParts();
-  }
+    public Day15() {
+        super(15, 2017);
+    }
 
-  @Override
-  public Object part1() {
-    return "";
-  }
+    public static void main(String[] args) {
+        new Day15().printParts();
+    }
 
-  @Override
-  public Object part2() {
-    return "";
-  }
+    @Override
+    public Object part1() {
+        long genA = 722;  // puzzle input
+        long genB = 354;  // puzzle input
+        int matches = 0;
+
+        for (int i = 0; i < 40_000_000; i++) {
+            genA = (genA * FACTOR_A) % DIVISOR;
+            genB = (genB * FACTOR_B) % DIVISOR;
+            
+            if ((genA & MASK) == (genB & MASK)) {
+                matches++;
+            }
+        }
+
+        return matches;
+    }
+
+    @Override
+    public Object part2() {
+        return 0;
+    }
 }
