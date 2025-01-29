8971eef9151c3d36dcdaf177747dd82732023ccf
simon <simon.mailadres@gmail.com>
2025-01-29 01:16:50 +0100

    Solved 2017 Day 13 Part 1

--- src/main/java/com/sbaars/adventofcode/year17/days/Day13.java
+++ src/main/java/com/sbaars/adventofcode/year17/days/Day13.java
@@ -1,6 +1,8 @@
 package com.sbaars.adventofcode.year17.days;
 
 import com.sbaars.adventofcode.year17.Day2017;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Day13 extends Day2017 {
   public Day13() {
@@ -13,7 +15,30 @@ public class Day13 extends Day2017 {
 
   @Override
   public Object part1() {
-    return "";
+    Map<Integer, Integer> scanners = new HashMap<>();
+    
+    // Parse input
+    for (String line : dayStrings()) {
+      String[] parts = line.split(": ");
+      scanners.put(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
+    }
+    
+    int severity = 0;
+    int maxDepth = scanners.keySet().stream().mapToInt(Integer::intValue).max().orElse(0);
+    
+    // For each layer
+    for (int depth = 0; depth <= maxDepth; depth++) {
+      if (scanners.containsKey(depth)) {
+        int range = scanners.get(depth);
+        // Scanner position at time = depth is depth % (2 * (range - 1))
+        // If position is 0, we get caught
+        if (depth % (2 * (range - 1)) == 0) {
+          severity += depth * range;
+        }
+      }
+    }
+    
+    return severity;
   }
 
   @Override
