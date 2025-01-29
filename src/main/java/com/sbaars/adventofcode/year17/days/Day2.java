e02317e89b1096fe51ae67eb8aed30179f2adfcc
simon <simon.mailadres@gmail.com>
2025-01-29 01:39:06 +0100

    Implemented 2017 Day 2 Part 2 using streams

--- src/main/java/com/sbaars/adventofcode/year17/days/Day2.java
+++ src/main/java/com/sbaars/adventofcode/year17/days/Day2.java
@@ -2,6 +2,7 @@ package com.sbaars.adventofcode.year17.days;
 
 import com.sbaars.adventofcode.year17.Day2017;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Day2 extends Day2017 {
   public Day2() {
@@ -24,6 +25,16 @@ public class Day2 extends Day2017 {
 
   @Override
   public Object part2() {
-    return "";
+    return dayStream()
+        .map(line -> Arrays.stream(line.split("\t"))
+            .mapToInt(Integer::parseInt)
+            .toArray())
+        .mapToInt(row -> IntStream.range(0, row.length)
+            .flatMap(i -> IntStream.range(i + 1, row.length)
+                .filter(j -> row[i] % row[j] == 0 || row[j] % row[i] == 0)
+                .map(j -> row[i] % row[j] == 0 ? row[i] / row[j] : row[j] / row[i]))
+            .findFirst()
+            .orElse(0))
+        .sum();
   }
 }
