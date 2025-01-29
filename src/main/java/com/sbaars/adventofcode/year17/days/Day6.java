301e2b35edc7ead65a6a2e98f64443c5c1bf36f5
simon <simon.mailadres@gmail.com>
2025-01-29 01:11:52 +0100

    Solved 2017 Day 6 Part 1

--- src/main/java/com/sbaars/adventofcode/year17/days/Day6.java
+++ src/main/java/com/sbaars/adventofcode/year17/days/Day6.java
@@ -1,6 +1,9 @@
 package com.sbaars.adventofcode.year17.days;
 
 import com.sbaars.adventofcode.year17.Day2017;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Day6 extends Day2017 {
 
@@ -14,7 +17,33 @@ public class Day6 extends Day2017 {
 
   @Override
   public Object part1() {
-    return "";
+    int[] banks = Arrays.stream(day().trim().split("\t")).mapToInt(Integer::parseInt).toArray();
+    Set<String> seen = new HashSet<>();
+    int cycles = 0;
+    
+    while (true) {
+      String state = Arrays.toString(banks);
+      if (!seen.add(state)) {
+        return cycles;
+      }
+      
+      // Find bank with most blocks
+      int maxIndex = 0;
+      for (int i = 1; i < banks.length; i++) {
+        if (banks[i] > banks[maxIndex]) {
+          maxIndex = i;
+        }
+      }
+      
+      // Redistribute blocks
+      int blocks = banks[maxIndex];
+      banks[maxIndex] = 0;
+      for (int i = 0; i < blocks; i++) {
+        banks[(maxIndex + 1 + i) % banks.length]++;
+      }
+      
+      cycles++;
+    }
   }
 
   @Override
