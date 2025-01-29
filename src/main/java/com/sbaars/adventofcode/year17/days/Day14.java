777f90074059bb88a395d76d6ab4665be4f26fcc
simon <simon.mailadres@gmail.com>
2025-01-29 01:52:23 +0100

    Implemented 2017 Day 14 Part 2

--- src/main/java/com/sbaars/adventofcode/year17/days/Day14.java
+++ src/main/java/com/sbaars/adventofcode/year17/days/Day14.java
@@ -3,6 +3,8 @@ package com.sbaars.adventofcode.year17.days;
 import com.sbaars.adventofcode.common.Day;
 
 public class Day14 extends Day {
+    private static final int GRID_SIZE = 128;
+    
     public Day14() {
         super(14, 2017);
     }
@@ -10,13 +12,49 @@ public class Day14 extends Day {
     public static void main(String[] args) {
         new Day14().printParts();
     }
+    
+    private boolean[][] createGrid(String input) {
+        boolean[][] grid = new boolean[GRID_SIZE][GRID_SIZE];
+        
+        for (int row = 0; row < GRID_SIZE; row++) {
+            String rowInput = input + "-" + row;
+            String hash = knotHash(rowInput);
+            
+            // Convert hash to binary and fill grid
+            int col = 0;
+            for (char c : hash.toCharArray()) {
+                int value = Character.digit(c, 16);
+                for (int bit = 3; bit >= 0; bit--) {
+                    grid[row][col] = ((value >> bit) & 1) == 1;
+                    col++;
+                }
+            }
+        }
+        
+        return grid;
+    }
+    
+    private void dfs(boolean[][] grid, boolean[][] visited, int row, int col) {
+        if (row < 0 || row >= GRID_SIZE || col < 0 || col >= GRID_SIZE || 
+            !grid[row][col] || visited[row][col]) {
+            return;
+        }
+        
+        visited[row][col] = true;
+        
+        // Visit all adjacent squares
+        dfs(grid, visited, row - 1, col); // up
+        dfs(grid, visited, row + 1, col); // down
+        dfs(grid, visited, row, col - 1); // left
+        dfs(grid, visited, row, col + 1); // right
+    }
 
     @Override
     public Object part1() {
         String input = "hxtvlmkl";
         int usedSquares = 0;
         
-        for (int i = 0; i < 128; i++) {
+        for (int i = 0; i < GRID_SIZE; i++) {
             String rowInput = input + "-" + i;
             String hash = knotHash(rowInput);
             usedSquares += countBits(hash);
@@ -83,6 +121,21 @@ public class Day14 extends Day {
 
     @Override
     public Object part2() {
-        return 0;
+        String input = "hxtvlmkl";
+        boolean[][] grid = createGrid(input);
+        boolean[][] visited = new boolean[GRID_SIZE][GRID_SIZE];
+        int regions = 0;
+        
+        // Count regions using DFS
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int col = 0; col < GRID_SIZE; col++) {
+                if (grid[row][col] && !visited[row][col]) {
+                    dfs(grid, visited, row, col);
+                    regions++;
+                }
+            }
+        }
+        
+        return regions;
     }
 }
