d21d9b58f2a826ef2eb8080222a29c3620417f8e
simon <simon.mailadres@gmail.com>
2025-01-29 02:05:19 +0100

    Fixed 2017 Day 6 implementation - improved efficiency and fixed cycle detection

--- src/main/java/com/sbaars/adventofcode/year17/days/Day6.java
+++ src/main/java/com/sbaars/adventofcode/year17/days/Day6.java
@@ -1,9 +1,7 @@
 package com.sbaars.adventofcode.year17.days;
 
 import com.sbaars.adventofcode.year17.Day2017;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Day6 extends Day2017 {
 
@@ -15,69 +13,66 @@ public class Day6 extends Day2017 {
     new Day6().printParts();
   }
 
+  private String getState(int[] banks) {
+    StringBuilder sb = new StringBuilder();
+    for (int bank : banks) {
+      sb.append(bank).append(',');
+    }
+    return sb.toString();
+  }
+
+  private int[] redistribute(int[] banks) {
+    int[] newBanks = banks.clone();
+    int maxIndex = 0;
+    int maxValue = newBanks[0];
+    
+    // Find bank with most blocks
+    for (int i = 1; i < newBanks.length; i++) {
+      if (newBanks[i] > maxValue) {
+        maxValue = newBanks[i];
+        maxIndex = i;
+      }
+    }
+    
+    // Redistribute blocks
+    newBanks[maxIndex] = 0;
+    int index = (maxIndex + 1) % newBanks.length;
+    while (maxValue > 0) {
+      newBanks[index]++;
+      maxValue--;
+      index = (index + 1) % newBanks.length;
+    }
+    
+    return newBanks;
+  }
+
   @Override
   public Object part1() {
     int[] banks = Arrays.stream(day().trim().split("\t")).mapToInt(Integer::parseInt).toArray();
     Set<String> seen = new HashSet<>();
     int cycles = 0;
     
-    while (true) {
-      String state = Arrays.toString(banks);
-      if (!seen.add(state)) {
-        return cycles;
-      }
-      
-      // Find bank with most blocks
-      int maxIndex = 0;
-      for (int i = 1; i < banks.length; i++) {
-        if (banks[i] > banks[maxIndex]) {
-          maxIndex = i;
-        }
-      }
-      
-      // Redistribute blocks
-      int blocks = banks[maxIndex];
-      banks[maxIndex] = 0;
-      for (int i = 0; i < blocks; i++) {
-        banks[(maxIndex + 1 + i) % banks.length]++;
-      }
-      
+    while (seen.add(getState(banks))) {
+      banks = redistribute(banks);
       cycles++;
     }
+    
+    return cycles;
   }
 
   @Override
   public Object part2() {
     int[] banks = Arrays.stream(day().trim().split("\t")).mapToInt(Integer::parseInt).toArray();
-    Set<String> seen = new HashSet<>();
+    Map<String, Integer> seen = new HashMap<>();
     int cycles = 0;
-    String firstRepeat = null;
     
     while (true) {
-      String state = Arrays.toString(banks);
-      if (state.equals(firstRepeat)) {
-        return cycles;
+      String state = getState(banks);
+      if (seen.containsKey(state)) {
+        return cycles - seen.get(state);
       }
-      if (!seen.add(state)) {
-        firstRepeat = state;
-        cycles = 0;
-      }
-      
-      // Find bank with most blocks
-      int maxIndex = 0;
-      for (int i = 1; i < banks.length; i++) {
-        if (banks[i] > banks[maxIndex]) {
-          maxIndex = i;
-        }
-      }
-      
-      // Redistribute blocks
-      int blocks = banks[maxIndex];
-      banks[maxIndex] = 0;
-      for (int i = 0; i < blocks; i++) {
-        banks[(maxIndex + 1 + i) % banks.length]++;
-      }
-      
+      seen.put(state, cycles);
+      banks = redistribute(banks);
       cycles++;
     }
   }
