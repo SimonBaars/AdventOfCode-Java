89f3f7a306f7fdb92dccc0b540289caef770e58c
simon <simon.mailadres@gmail.com>
2025-01-29 01:41:27 +0100

    Implemented 2017 Day 5 Part 2

--- src/main/java/com/sbaars/adventofcode/year17/days/Day5.java
+++ src/main/java/com/sbaars/adventofcode/year17/days/Day5.java
@@ -30,6 +30,17 @@ public class Day5 extends Day2017 {
 
   @Override
   public Object part2() {
-    return "";
+    int[] jumps = dayIntStream().toArray();
+    int pos = 0;
+    int steps = 0;
+    
+    while (pos >= 0 && pos < jumps.length) {
+      int jump = jumps[pos];
+      jumps[pos] += (jump >= 3) ? -1 : 1;
+      pos += jump;
+      steps++;
+    }
+    
+    return steps;
   }
 }
