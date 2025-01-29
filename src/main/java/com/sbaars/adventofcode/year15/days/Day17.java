87efef452fc47fd9b9d6353e5389883936956392
simon <simon.mailadres@gmail.com>
2025-01-29 03:03:34 +0100

    Solve day 17 2015 part 2: No Such Thing as Too Much

--- src/main/java/com/sbaars/adventofcode/year15/days/Day17.java
+++ src/main/java/com/sbaars/adventofcode/year15/days/Day17.java
@@ -8,6 +8,7 @@ public class Day17 extends Day2015 {
 
     private static final int TARGET_LITERS = 150;
     private final List<Integer> containers = new ArrayList<>();
+    private final Map<Integer, Integer> containerCountMap = new HashMap<>();
 
     public Day17() {
         super(17);
@@ -18,6 +19,7 @@ public class Day17 extends Day2015 {
         Day17 day = new Day17();
         day.printParts();
         new com.sbaars.adventofcode.network.Submit().submit(day.part1(), 2015, 17, 1);
+        new com.sbaars.adventofcode.network.Submit().submit(day.part2(), 2015, 17, 2);
     }
 
     private void parseInput() {
@@ -30,16 +32,22 @@ public class Day17 extends Day2015 {
 
     @Override
     public Object part1() {
-        return findCombinations(0, 0, new boolean[containers.size()]);
+        return findCombinations(0, 0, new boolean[containers.size()], 0);
     }
 
     @Override
     public Object part2() {
-        return 0; // Implement in next part
+        containerCountMap.clear();
+        findCombinations(0, 0, new boolean[containers.size()], 0);
+        int minContainers = containerCountMap.keySet().stream()
+            .min(Integer::compareTo)
+            .orElse(0);
+        return containerCountMap.get(minContainers);
     }
 
-    private int findCombinations(int index, int currentSum, boolean[] used) {
+    private int findCombinations(int index, int currentSum, boolean[] used, int containerCount) {
         if (currentSum == TARGET_LITERS) {
+            containerCountMap.merge(containerCount, 1, Integer::sum);
             return 1;
         }
         if (currentSum > TARGET_LITERS || index >= containers.size()) {
@@ -47,11 +55,11 @@ public class Day17 extends Day2015 {
         }
 
         // Don't use current container
-        int withoutCurrent = findCombinations(index + 1, currentSum, used);
+        int withoutCurrent = findCombinations(index + 1, currentSum, used, containerCount);
 
         // Use current container
         used[index] = true;
-        int withCurrent = findCombinations(index + 1, currentSum + containers.get(index), used);
+        int withCurrent = findCombinations(index + 1, currentSum + containers.get(index), used, containerCount + 1);
         used[index] = false;
 
         return withoutCurrent + withCurrent;
