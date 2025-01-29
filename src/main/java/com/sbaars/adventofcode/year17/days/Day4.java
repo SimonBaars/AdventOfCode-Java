e4c7a8a27736a47bc022e04ff02694a8acefa96c
simon <simon.mailadres@gmail.com>
2025-01-29 01:40:43 +0100

    Implemented 2017 Day 4 Part 2 using streams

--- src/main/java/com/sbaars/adventofcode/year17/days/Day4.java
+++ src/main/java/com/sbaars/adventofcode/year17/days/Day4.java
@@ -3,6 +3,7 @@ package com.sbaars.adventofcode.year17.days;
 import com.sbaars.adventofcode.year17.Day2017;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.stream.Collectors;
 
 public class Day4 extends Day2017 {
 
@@ -12,6 +13,13 @@ public class Day4 extends Day2017 {
 
   public static void main(String[] args) {
     new Day4().printParts();
+  }
+
+  private String sortString(String str) {
+    return str.chars()
+        .sorted()
+        .mapToObj(c -> String.valueOf((char)c))
+        .collect(Collectors.joining());
   }
 
   @Override
@@ -27,6 +35,14 @@ public class Day4 extends Day2017 {
 
   @Override
   public Object part2() {
-    return "";
+    return dayStream()
+        .filter(line -> {
+          String[] words = line.split(" ");
+          HashSet<String> sortedWords = Arrays.stream(words)
+              .map(this::sortString)
+              .collect(Collectors.toCollection(HashSet::new));
+          return sortedWords.size() == words.length;
+        })
+        .count();
   }
 }
