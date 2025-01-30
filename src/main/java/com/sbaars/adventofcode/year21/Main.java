89444831a0051f05012d4034c4fe83c1c1d7d5bd
simon <simon.mailadres@gmail.com>
2025-01-30 03:19:16 +0100

    Add timing info to Main

--- src/main/java/com/sbaars/adventofcode/year21/Main.java
+++ src/main/java/com/sbaars/adventofcode/year21/Main.java
@@ -10,7 +10,17 @@ public class Main {
     for (int day = 1; day <= 25; day++) {
       System.out.println("Day " + day + ":");
       Day instance = (Day) Class.forName("com.sbaars.adventofcode.year21.days.Day" + day).getDeclaredConstructor().newInstance();
-      instance.printParts();
+      
+      long startTime = System.nanoTime();
+      Object part1 = instance.part1();
+      long part1Time = (System.nanoTime() - startTime) / 1_000_000; // Convert to milliseconds
+      
+      startTime = System.nanoTime();
+      Object part2 = instance.part2();
+      long part2Time = (System.nanoTime() - startTime) / 1_000_000; // Convert to milliseconds
+      
+      System.out.println("Part 1: " + part1 + " (" + part1Time + "ms)");
+      System.out.println("Part 2: " + part2 + " (" + part2Time + "ms)");
       System.out.println();
     }
   }
