33274f6a4af9ab09bf0f541b31d4f806e51734df
Simon <simon.mailadres@gmail.com>
2025-12-01 00:23:14 -0600

    Add year 2025 scaffolding

---
+++ src/main/java/com/sbaars/adventofcode/year25/Day2025.java
@@ -0,0 +1,9 @@
+package com.sbaars.adventofcode.year25;
+
+import com.sbaars.adventofcode.common.Day;
+
+public abstract class Day2025 extends Day {
+  protected Day2025(int day) {
+    super(2025, day);
+  }
+}
---
+++ src/main/java/com/sbaars/adventofcode/year25/Main.java
@@ -0,0 +1,27 @@
+package com.sbaars.adventofcode.year25;
+
+import com.sbaars.adventofcode.common.Day;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+  public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, IOException, InvocationTargetException, NoSuchMethodException {
+    for (int day = 1; day <= 25; day++) {
+      System.out.println("Day " + day + ":");
+      Day instance = (Day) Class.forName("com.sbaars.adventofcode.year25.days.Day" + day).getDeclaredConstructor().newInstance();
+      
+      long startTime = System.nanoTime();
+      Object part1 = instance.part1();
+      long part1Time = (System.nanoTime() - startTime) / 1_000_000; // Convert to milliseconds
+      
+      startTime = System.nanoTime();
+      Object part2 = instance.part2();
+      long part2Time = (System.nanoTime() - startTime) / 1_000_000; // Convert to milliseconds
+      
+      System.out.println("Part 1: " + part1 + " (" + part1Time + "ms)");
+      System.out.println("Part 2: " + part2 + " (" + part2Time + "ms)");
+      System.out.println();
+    }
+  }
+}
